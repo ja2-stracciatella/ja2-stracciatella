1422947b840f32483d34f008e3e8d960967d325d
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2006-09-21 16:12:41 +0000

    Call abort() in the CHECK*() macros instead of failing silently and causing havoc later
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@52 5e31c081-6ce3-0310-bb30-f584a8092234

--- sgp/WCheck.h
+++ sgp/WCheck.h
@@ -1,14 +1,15 @@
 #ifndef __WCHECK_
 #define __WCHECK_
 
-#define CHECKF(exp)        if (!(exp)) { return(FALSE); }
-#define CHECKV(exp)        if (!(exp)) { return; }
-#define CHECKN(exp)        if (!(exp)) { return(NULL); }
-#define CHECKBI(exp)       if (!(exp)) { return(-1); }
+#if 1 // XXX TODO put under some flag
+#	define ABORT() abort()
+#else
+#	define ABORT()
+#endif
 
-#define CHECKASSERTF(exp)  if (!(exp)) { ASSERT(0); return(FALSE); }
-#define CHECKASSERTV(exp)  if (!(exp)) { ASSERT(0); return; }
-#define CHECKASSERTN(exp)  if (!(exp)) { ASSERT(0); return(NULL); }
-#define CHECKASSERTBI(exp) if (!(exp)) { ASSERT(0); return(-1); }
+#define CHECKF(exp)  if (!(exp)) { ABORT(); return FALSE; }
+#define CHECKV(exp)  if (!(exp)) { ABORT(); return; }
+#define CHECKN(exp)  if (!(exp)) { ABORT(); return NULL; }
+#define CHECKBI(exp) if (!(exp)) { ABORT(); return -1; }
 
 #endif
