c85f56fb4dc09a0aecf1a0b05339126dd6cf2f79
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2006-09-29 06:01:28 +0000

    Make failed CHECK*() non-fatal for now. Let them print a FIXME instead
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@117 5e31c081-6ce3-0310-bb30-f584a8092234

--- sgp/WCheck.h
+++ sgp/WCheck.h
@@ -1,15 +1,15 @@
 #ifndef __WCHECK_
 #define __WCHECK_
 
-#if 1 // XXX TODO put under some flag
-#	define ABORT() abort()
+#if 0 // XXX TODO put under some flag
+#	define FAIL() abort()
 #else
-#	define ABORT()
+#	define FAIL() FIXME
 #endif
 
-#define CHECKF(exp)  if (!(exp)) { ABORT(); return FALSE; }
-#define CHECKV(exp)  if (!(exp)) { ABORT(); return; }
-#define CHECKN(exp)  if (!(exp)) { ABORT(); return NULL; }
-#define CHECKBI(exp) if (!(exp)) { ABORT(); return -1; }
+#define CHECKF(exp)  if (!(exp)) { FAIL(); return FALSE; }
+#define CHECKV(exp)  if (!(exp)) { FAIL(); return; }
+#define CHECKN(exp)  if (!(exp)) { FAIL(); return NULL; }
+#define CHECKBI(exp) if (!(exp)) { FAIL(); return -1; }
 
 #endif
