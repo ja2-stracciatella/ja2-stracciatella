e17d8caf4c25a092f02a648d4a8010391fcab552
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2006-10-01 21:19:38 +0000

    Implement Blt8BPPDataTo16BPPBufferTransZIncClipZSameZBurnsThrough()
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@151 5e31c081-6ce3-0310-bb30-f584a8092234

--- ja2/Build/TileEngine/RenderWorld.c
+++ ja2/Build/TileEngine/RenderWorld.c
@@ -4972,7 +4972,136 @@ BOOLEAN Blt8BPPDataTo16BPPBufferTransZIncClipZSameZBurnsThrough( UINT16 *pBuffer
 	usZIndex=usZStartIndex;
 
 #if 1 // XXX TODO
-	FIXME // XXX TODO0001
+	UINT32 PxCount;
+
+	while (TopSkip > 0)
+	{
+		for (;;)
+		{
+			PxCount = *SrcPtr++;
+			if (PxCount & 0x80) continue;
+			if (PxCount == 0) break;
+			SrcPtr += PxCount;
+		}
+		TopSkip--;
+	}
+
+	do
+	{
+		usZLevel = usZStartLevel;
+		usZIndex = usZStartIndex;
+		usZColsToGo = usZStartCols;
+		for (LSCount = LeftSkip; LSCount > 0; LSCount -= PxCount)
+		{
+			PxCount = *SrcPtr++;
+			if (PxCount & 0x80)
+			{
+				PxCount &= 0x7F;
+				if (PxCount > LSCount)
+				{
+					PxCount -= LSCount;
+					LSCount = BlitLength;
+					goto BlitTransparent;
+				}
+			}
+			else
+			{
+				if (PxCount > LSCount)
+				{
+					SrcPtr += LSCount;
+					PxCount -= LSCount;
+					LSCount = BlitLength;
+					goto BlitNonTransLoop;
+				}
+				SrcPtr += PxCount;
+			}
+		}
+
+		LSCount = BlitLength;
+		while (LSCount > 0)
+		{
+			PxCount = *SrcPtr++;
+			if (PxCount & 0x80)
+			{
+BlitTransparent: // skip transparent pixels
+				PxCount &= 0x7F;
+				if (PxCount > LSCount) PxCount = LSCount;
+				LSCount -= PxCount;
+				DestPtr += 2 * PxCount;
+				ZPtr    += 2 * PxCount;
+				for (;;)
+				{
+					if (PxCount >= usZColsToGo)
+					{
+						PxCount -= usZColsToGo;
+						usZColsToGo = 20;
+
+						INT8 delta = pZArray[usZIndex++];
+						if (delta < 0)
+						{
+							usZLevel -= Z_STRIP_DELTA_Y;
+						}
+						else if (delta > 0)
+						{
+							usZLevel += Z_STRIP_DELTA_Y;
+						}
+					}
+					else
+					{
+						usZColsToGo -= PxCount;
+						break;
+					}
+				}
+			}
+			else
+			{
+BlitNonTransLoop: // blit non-transparent pixels
+				if (PxCount > LSCount)
+				{
+					Unblitted = PxCount - LSCount;
+					PxCount = LSCount;
+				}
+				else
+				{
+					Unblitted = 0;
+				}
+				LSCount -= PxCount;
+
+				do
+				{
+					if (*(UINT16*)ZPtr <= usZLevel)
+					{
+						*(UINT16*)ZPtr = usZLevel;
+						*(UINT16*)DestPtr = p16BPPPalette[*SrcPtr];
+					}
+					SrcPtr++;
+					DestPtr += 2;
+					ZPtr += 2;
+					if (--usZColsToGo == 0)
+					{
+						usZColsToGo = 20;
+
+						INT8 delta = pZArray[usZIndex++];
+						if (delta < 0)
+						{
+							usZLevel -= Z_STRIP_DELTA_Y;
+						}
+						else if (delta > 0)
+						{
+							usZLevel += Z_STRIP_DELTA_Y;
+						}
+					}
+				}
+				while (--PxCount > 0);
+				SrcPtr += Unblitted;
+			}
+		}
+
+		while (*SrcPtr++ != 0) {} // skip along until we hit and end-of-line marker
+		DestPtr += LineSkip;
+		ZPtr += LineSkip;
+	}
+	while (--BlitHeight > 0);
 #else
 	__asm {
 
