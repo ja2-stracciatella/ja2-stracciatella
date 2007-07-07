e554013e4cf7af0c59d13f4d9b8dd676c4af0e30
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2007-07-07 15:08:56 +0000

    Remove an unused variable.
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@1180 5e31c081-6ce3-0310-bb30-f584a8092234

--- ja2/Build/JAScreens.c
+++ ja2/Build/JAScreens.c
@@ -218,9 +218,6 @@ UINT32 ErrorScreenHandle(void)
 {
   InputAtom  InputEvent;
 	static BOOLEAN	fFirstTime = FALSE;
-#ifdef JA2BETAVERSION
-	wchar_t str[256];
-#endif
 
 	// For quick setting of new video stuff / to be changed
 	StartFrameBufferRender( );
