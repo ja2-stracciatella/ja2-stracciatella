92afcd0154e1b2c8ecf572ecb93b45b280496e3f
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2007-07-22 09:01:31 +0000

    Cleanup.
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@1254 5e31c081-6ce3-0310-bb30-f584a8092234

--- sgp/Shading.c
+++ sgp/Shading.c
@@ -2,99 +2,63 @@
 #include "Shading.h"
 #include "VObject.h"
 
-UINT16	IntensityTable[65536];
-UINT16	ShadeTable[65536];
-UINT16	White16BPPPalette[ 256 ];
-static FLOAT guiShadePercent = 0.48;
+UINT16 IntensityTable[65536];
+UINT16 ShadeTable[65536];
+UINT16 White16BPPPalette[256];
+static float guiShadePercent = 0.48;
 
 
-/**********************************************************************************************
- BuildShadeTable
-
-	Builds a 16-bit color shading table. This function should be called only after the current
-	video adapter's pixel format is known (IE: GetRgbDistribution() has been called, and the
-	globals for masks and shifts have been initialized by that function), and before any
-	blitting is done.
-
-	Using the table is a straight lookup. The pixel to be shaded down is used as the index into
-	the table and the entry at that point will be a pixel that is 25% darker.
-
-**********************************************************************************************/
+/* Builds a 16-bit color shading table. This function should be called only
+ * after the current video adapter's pixel format is known (IE:
+ * GetRgbDistribution() has been called, and the globals for masks and shifts
+ * have been initialized by that function), and before any blitting is done.
+ * Using the table is a straight lookup. The pixel to be shaded down is used as
+ * the index into the table and the entry at that point will be a pixel that is
+ * 25% darker.
+ */
 void BuildShadeTable(void)
 {
-	UINT16 red, green, blue;
-	UINT16 index;
-
-	for(red=0; red < 256; red+=4)
-		for(green=0; green < 256; green+=4)
-			for(blue=0; blue < 256; blue+=4)
+	for (UINT16 red = 0; red < 256; red += 4)
+	{
+		for (UINT16 green = 0; green < 256; green += 4)
+		{
+			for (UINT16 blue = 0; blue < 256; blue += 4)
 			{
-				index=Get16BPPColor(FROMRGB(red, green, blue));
-				ShadeTable[index]=Get16BPPColor(FROMRGB(red*guiShadePercent, green*guiShadePercent, blue*guiShadePercent));
+				UINT16 index = Get16BPPColor(FROMRGB(red, green, blue));
+				ShadeTable[index] = Get16BPPColor(FROMRGB(red * guiShadePercent, green * guiShadePercent, blue * guiShadePercent));
 			}
+		}
+	}
 
-	memset( White16BPPPalette, 65535, sizeof( White16BPPPalette ) );
+	memset(White16BPPPalette, 255, sizeof(White16BPPPalette));
 }
 
 
-/**********************************************************************************************
- BuildIntensityTable
-
-	Builds a 16-bit color shading table. This function should be called only after the current
-	video adapter's pixel format is known (IE: GetRgbDistribution() has been called, and the
-	globals for masks and shifts have been initialized by that function), and before any
-	blitting is done.
-
-
-
-**********************************************************************************************/
+/* Builds a 16-bit color shading table. This function should be called only
+ * after the current video adapter's pixel format is known (IE:
+ * GetRgbDistribution() has been called, and the globals for masks and shifts
+ * have been initialized by that function), and before any blitting is done.
+ */
 void BuildIntensityTable(void)
 {
-	UINT16 red, green, blue;
-	UINT16 index;
-	FLOAT  dShadedPercent = (FLOAT)0.80;
+	const float dShadedPercent = 0.80f;
 
-
-#if 0
-
-	UINT32 lumin;
-	UINT32 rmod, gmod, bmod;
-
-	for(red=0; red < 256; red+=4)
-		for(green=0; green < 256; green+=4)
-			for(blue=0; blue < 256; blue+=4)
+	for (UINT16 red = 0; red < 256; red += 4)
+	{
+		for (UINT16 green = 0; green < 256; green += 4)
+		{
+			for (UINT16 blue = 0; blue < 256; blue += 4)
 			{
-				index=Get16BPPColor(FROMRGB(red, green, blue));
-
-				lumin=( red*299/1000)+ ( green*587/1000 ) + ( blue*114/1000 );
-
-				//lumin = __min(lumin, 255);
-				rmod=(255*lumin)/256;
-				gmod=(100*lumin)/256;
-				bmod=(100*lumin)/256;
-
-				//rmod = __m( 255, rmod );
-
-				IntensityTable[index]=Get16BPPColor( FROMRGB( rmod, gmod , bmod ) );
+				UINT16 index = Get16BPPColor(FROMRGB(red, green, blue));
+				IntensityTable[index] = Get16BPPColor(FROMRGB(red * dShadedPercent, green * dShadedPercent, blue * dShadedPercent));
 			}
-#endif
-
-
-
-	for(red=0; red < 256; red+=4)
-		for(green=0; green < 256; green+=4)
-			for(blue=0; blue < 256; blue+=4)
-			{
-				index=Get16BPPColor(FROMRGB(red, green, blue));
-				IntensityTable[index]=Get16BPPColor(FROMRGB(red*dShadedPercent, green*dShadedPercent, blue*dShadedPercent));
-			}
-
+		}
+	}
 }
 
 
-
-void SetShadeTablePercent( FLOAT uiShadePercent )
+void SetShadeTablePercent(float uiShadePercent)
 {
 	guiShadePercent = uiShadePercent;
-	BuildShadeTable( );
+	BuildShadeTable();
 }
--- sgp/Shading.h
+++ sgp/Shading.h
@@ -1,5 +1,5 @@
-#ifndef _SHADING_H_
-#define _SHADING_H_
+#ifndef SHADING_H
+#define SHADING_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -7,16 +7,16 @@ extern "C" {
 
 void BuildShadeTable(void);
 void BuildIntensityTable(void);
-void SetShadeTablePercent( FLOAT uiShadePercent );
+void SetShadeTablePercent(float uiShadePercent);
 
-extern UINT16	IntensityTable[65536];
-extern UINT16	ShadeTable[65536];
-extern UINT16	White16BPPPalette[ 256 ];
+extern UINT16 IntensityTable[65536];
+extern UINT16 ShadeTable[65536];
+extern UINT16 White16BPPPalette[256];
 
 #ifdef __cplusplus
 }
 #endif
 
-#define DEFAULT_SHADE_LEVEL		4
+#define DEFAULT_SHADE_LEVEL 4
 
 #endif
