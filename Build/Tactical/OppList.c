ja2-stracciatella/ja2-stracciatella

commit f9982c7e196f2bed17d424fe52ab2b21c64c7caf

Cleanup.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@1448 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Tactical/Overhead.h
@@ -5,7 +5,7 @@
 #include "Overhead_Types.h"
 
 
-#define  MAX_REALTIME_SPEED_VAL						10
+#define MAX_REALTIME_SPEED_VAL 10
 
 
 // Enums for waiting for mercs to finish codes
@@ -21,20 +21,20 @@ enum
 // TACTICAL ENGINE STATUS FLAGS
 typedef struct
 {
-	UINT8			bFirstID;
-	UINT8			bLastID;
-	COLORVAL  RadarColor;
-	INT8			bSide;
-	INT8			bMenInSector;
-	UINT8			ubLastMercToRadio;
-	INT8			bTeamActive;
-	INT8			bAwareOfOpposition;
-	INT8			bHuman;
+	UINT8    bFirstID;
+	UINT8    bLastID;
+	COLORVAL RadarColor;
+	INT8     bSide;
+	INT8     bMenInSector;
+	UINT8    ubLastMercToRadio;
+	INT8     bTeamActive;
+	INT8     bAwareOfOpposition;
+	INT8     bHuman;
 } TacticalTeamType;
 
 
-#define PANIC_BOMBS_HERE			0x01
-#define PANIC_TRIGGERS_HERE		0x02
+#define PANIC_BOMBS_HERE    0x01
+#define PANIC_TRIGGERS_HERE 0x02
 
 #define NUM_PANIC_TRIGGERS 3
 
@@ -42,133 +42,132 @@ typedef struct
 
 typedef struct
 {
-	UINT32							uiFlags;
-	TacticalTeamType		Team[ MAXTEAMS ];
-	UINT8								ubCurrentTeam;
-	INT16								sSlideTarget;
-	INT16								sSlideReason;
-	UINT32							uiTimeSinceMercAIStart;
-	INT8								fPanicFlags;
-	INT16								sPanicTriggerGridno_UNUSED; // XXX HACK000B
-	INT16								sHandGrid_UNUSED; // XXX HACK000B
-	UINT8								ubSpottersCalledForBy;
-	UINT8								ubTheChosenOne;
-	UINT32							uiTimeOfLastInput;
-	UINT32							uiTimeSinceDemoOn;
-	UINT32							uiCountdownToRestart_UNUSED; // XXX HACK000B
-	BOOLEAN							fGoingToEnterDemo_UNUSED; // XXX HACK000B
-	BOOLEAN							fNOTDOLASTDEMO_UNUSED; // XXX HACK000B
-	BOOLEAN							fMultiplayer_UNUSED; // XXX HACK000B
-	BOOLEAN							fCivGroupHostile[ NUM_CIV_GROUPS ];
-	UINT8								ubLastBattleSectorX;
-	UINT8								ubLastBattleSectorY;
-	BOOLEAN							fLastBattleWon;
-	INT8								bOriginalSizeOfEnemyForce;
-	INT8								bPanicTriggerIsAlarm_UNUSED; // XXX HACK000B
-	BOOLEAN							fVirginSector;
-	BOOLEAN							fEnemyInSector;
-	BOOLEAN							fInterruptOccurred;
-	INT8								bRealtimeSpeed;
-	UINT8								ubEnemyIntention_UNUSED; // XXX HACK000B
-	UINT8								ubEnemyIntendedRetreatDirection_UNUSED; // XXX HACK000B
-	UINT8								ubEnemySightingOnTheirTurnEnemyID;
-	UINT8								ubEnemySightingOnTheirTurnPlayerID_UNUSED; // XXX HACK000B
-	BOOLEAN							fEnemySightingOnTheirTurn;
-	BOOLEAN							fAutoBandageMode;
-	UINT8								ubAttackBusyCount;
-	INT8								bNumEnemiesFoughtInBattle_UNUSED; // XXX HACK000B
-	UINT8								ubEngagedInConvFromActionMercID;
-	UINT16							usTactialTurnLimitCounter;
-	BOOLEAN							fInTopMessage;
-	UINT8								ubTopMessageType;
-	wchar_t							zTopMessageString[20];
-	UINT16							usTactialTurnLimitMax;
-	UINT32							uiTactialTurnLimitClock;
-	BOOLEAN							fTactialTurnLimitStartedBeep;
-	INT8								bBoxingState;
-	INT8								bConsNumTurnsNotSeen;
-	UINT8								ubArmyGuysKilled;
+	UINT32           uiFlags;
+	TacticalTeamType Team[MAXTEAMS];
+	UINT8            ubCurrentTeam;
+	INT16            sSlideTarget;
+	INT16            sSlideReason;
+	UINT32           uiTimeSinceMercAIStart;
+	INT8             fPanicFlags;
+	INT16            sPanicTriggerGridno_UNUSED; // XXX HACK000B
+	INT16            sHandGrid_UNUSED; // XXX HACK000B
+	UINT8            ubSpottersCalledForBy;
+	UINT8            ubTheChosenOne;
+	UINT32           uiTimeOfLastInput;
+	UINT32           uiTimeSinceDemoOn;
+	UINT32           uiCountdownToRestart_UNUSED; // XXX HACK000B
+	BOOLEAN          fGoingToEnterDemo_UNUSED; // XXX HACK000B
+	BOOLEAN          fNOTDOLASTDEMO_UNUSED; // XXX HACK000B
+	BOOLEAN          fMultiplayer_UNUSED; // XXX HACK000B
+	BOOLEAN          fCivGroupHostile[ NUM_CIV_GROUPS ];
+	UINT8            ubLastBattleSectorX;
+	UINT8            ubLastBattleSectorY;
+	BOOLEAN          fLastBattleWon;
+	INT8             bOriginalSizeOfEnemyForce;
+	INT8             bPanicTriggerIsAlarm_UNUSED; // XXX HACK000B
+	BOOLEAN          fVirginSector;
+	BOOLEAN          fEnemyInSector;
+	BOOLEAN          fInterruptOccurred;
+	INT8             bRealtimeSpeed;
+	UINT8            ubEnemyIntention_UNUSED; // XXX HACK000B
+	UINT8            ubEnemyIntendedRetreatDirection_UNUSED; // XXX HACK000B
+	UINT8            ubEnemySightingOnTheirTurnEnemyID;
+	UINT8            ubEnemySightingOnTheirTurnPlayerID_UNUSED; // XXX HACK000B
+	BOOLEAN          fEnemySightingOnTheirTurn;
+	BOOLEAN          fAutoBandageMode;
+	UINT8            ubAttackBusyCount;
+	INT8             bNumEnemiesFoughtInBattle_UNUSED; // XXX HACK000B
+	UINT8            ubEngagedInConvFromActionMercID;
+	UINT16           usTactialTurnLimitCounter;
+	BOOLEAN          fInTopMessage;
+	UINT8            ubTopMessageType;
+	wchar_t          zTopMessageString[20];
+	UINT16           usTactialTurnLimitMax;
+	UINT32           uiTactialTurnLimitClock;
+	BOOLEAN          fTactialTurnLimitStartedBeep;
+	INT8             bBoxingState;
+	INT8             bConsNumTurnsNotSeen;
+	UINT8            ubArmyGuysKilled;
 
-	INT16								sPanicTriggerGridNo[ NUM_PANIC_TRIGGERS ];
-	INT8								bPanicTriggerIsAlarm[ NUM_PANIC_TRIGGERS ];
-	UINT8								ubPanicTolerance[ NUM_PANIC_TRIGGERS ];
-	BOOLEAN							fAtLeastOneGuyOnMultiSelect;
-	BOOLEAN							fSaidCreatureFlavourQuote_UNUSED; // XXX HACK000B
-	BOOLEAN							fHaveSeenCreature_UNUSED; // XXX HACK000B
-	BOOLEAN							fKilledEnemyOnAttack;
-	UINT8								ubEnemyKilledOnAttack;
-	INT8								bEnemyKilledOnAttackLevel;
-	UINT16							ubEnemyKilledOnAttackLocation;
-	BOOLEAN							fItemsSeenOnAttack;
-	BOOLEAN							ubItemsSeenOnAttackSoldier;
-	BOOLEAN							fBeenInCombatOnce_UNUSED; // XXX HACK000B
-	BOOLEAN							fSaidCreatureSmellQuote_UNUSED; // XXX HACK000B
-	UINT16							usItemsSeenOnAttackGridNo;
-	BOOLEAN							fLockItemLocators;
-	UINT8								ubLastQuoteSaid;
-	UINT8								ubLastQuoteProfileNUm;
-	BOOLEAN							fCantGetThrough;
-	INT16								sCantGetThroughGridNo;
-	INT16								sCantGetThroughSoldierGridNo;
-	UINT8								ubCantGetThroughID;
-	BOOLEAN							fDidGameJustStart;
-	BOOLEAN							fStatChangeCheatOn_UNUSED; // XXX HACK000B
-	UINT8								ubLastRequesterTargetID;
-	BOOLEAN							fGoodToAllowCrows;
-	UINT8								ubNumCrowsPossible;
-	UINT32							uiTimeCounterForGiveItemSrc_UNUSED; // XXX HACK000B
-	BOOLEAN							fUnLockUIAfterHiddenInterrupt;
-	INT8								bNumFoughtInBattle[ MAXTEAMS ];
-	UINT32							uiDecayBloodLastUpdate;
-	UINT32							uiTimeSinceLastInTactical;
-	BOOLEAN							fHasAGameBeenStarted;
-	INT8								bConsNumTurnsWeHaventSeenButEnemyDoes;
-	BOOLEAN								fSomeoneHit;
-	UINT8								ubPaddingSmall_UNUSED; // XXX HACK000B
-	UINT32							uiTimeSinceLastOpplistDecay;
-	INT8								bMercArrivingQuoteBeingUsed;
-	UINT8								ubEnemyKilledOnAttackKiller;
-	BOOLEAN							fCountingDownForGuideDescription;
-	INT8								bGuideDescriptionCountDown;
-	UINT8								ubGuideDescriptionToUse;
-	INT8								bGuideDescriptionSectorX;
-	INT8								bGuideDescriptionSectorY;
-	INT8								fEnemyFlags;
-  BOOLEAN             fAutoBandagePending;
-  BOOLEAN             fHasEnteredCombatModeSinceEntering;
-  BOOLEAN             fDontAddNewCrows;
-  UINT8               ubMorePadding_UNUSED; // XXX HACK000B
-  UINT16              sCreatureTenseQuoteDelay;
-  UINT32              uiCreatureTenseQuoteLastUpdate;
+	INT16            sPanicTriggerGridNo[NUM_PANIC_TRIGGERS];
+	INT8             bPanicTriggerIsAlarm[NUM_PANIC_TRIGGERS];
+	UINT8            ubPanicTolerance[NUM_PANIC_TRIGGERS];
+	BOOLEAN          fAtLeastOneGuyOnMultiSelect;
+	BOOLEAN          fSaidCreatureFlavourQuote_UNUSED; // XXX HACK000B
+	BOOLEAN          fHaveSeenCreature_UNUSED; // XXX HACK000B
+	BOOLEAN          fKilledEnemyOnAttack;
+	UINT8            ubEnemyKilledOnAttack;
+	INT8             bEnemyKilledOnAttackLevel;
+	UINT16           ubEnemyKilledOnAttackLocation;
+	BOOLEAN          fItemsSeenOnAttack;
+	BOOLEAN          ubItemsSeenOnAttackSoldier;
+	BOOLEAN          fBeenInCombatOnce_UNUSED; // XXX HACK000B
+	BOOLEAN          fSaidCreatureSmellQuote_UNUSED; // XXX HACK000B
+	UINT16           usItemsSeenOnAttackGridNo;
+	BOOLEAN          fLockItemLocators;
+	UINT8            ubLastQuoteSaid;
+	UINT8            ubLastQuoteProfileNUm;
+	BOOLEAN          fCantGetThrough;
+	INT16            sCantGetThroughGridNo;
+	INT16            sCantGetThroughSoldierGridNo;
+	UINT8            ubCantGetThroughID;
+	BOOLEAN          fDidGameJustStart;
+	BOOLEAN          fStatChangeCheatOn_UNUSED; // XXX HACK000B
+	UINT8            ubLastRequesterTargetID;
+	BOOLEAN          fGoodToAllowCrows;
+	UINT8            ubNumCrowsPossible;
+	UINT32           uiTimeCounterForGiveItemSrc_UNUSED; // XXX HACK000B
+	BOOLEAN          fUnLockUIAfterHiddenInterrupt;
+	INT8             bNumFoughtInBattle[ MAXTEAMS ];
+	UINT32           uiDecayBloodLastUpdate;
+	UINT32           uiTimeSinceLastInTactical;
+	BOOLEAN          fHasAGameBeenStarted;
+	INT8             bConsNumTurnsWeHaventSeenButEnemyDoes;
+	BOOLEAN          fSomeoneHit;
+	UINT8            ubPaddingSmall_UNUSED; // XXX HACK000B
+	UINT32           uiTimeSinceLastOpplistDecay;
+	INT8             bMercArrivingQuoteBeingUsed;
+	UINT8            ubEnemyKilledOnAttackKiller;
+	BOOLEAN          fCountingDownForGuideDescription;
+	INT8             bGuideDescriptionCountDown;
+	UINT8            ubGuideDescriptionToUse;
+	INT8             bGuideDescriptionSectorX;
+	INT8             bGuideDescriptionSectorY;
+	INT8             fEnemyFlags;
+	BOOLEAN          fAutoBandagePending;
+	BOOLEAN          fHasEnteredCombatModeSinceEntering;
+	BOOLEAN          fDontAddNewCrows;
+	UINT8            ubMorePadding_UNUSED; // XXX HACK000B
+	UINT16           sCreatureTenseQuoteDelay;
+	UINT32           uiCreatureTenseQuoteLastUpdate;
 } TacticalStatusType;
 CASSERT(sizeof(TacticalStatusType) == 360)
 
 
-UINT8		gbPlayerNum;
+extern UINT8 gbPlayerNum;
 
-UINT16  gusSelectedSoldier;
+extern UINT16 gusSelectedSoldier;
 
-extern CHAR8		gzActionStr[][ 30 ];
+extern const char* const gzActionStr[][30];
 
 // Soldier List used for all soldier overhead interaction
-SOLDIERTYPE	 Menptr[ TOTAL_SOLDIERS ];
-SOLDIERTYPE	 *MercPtrs[ TOTAL_SOLDIERS ];
+extern SOLDIERTYPE  Menptr[TOTAL_SOLDIERS];
+extern SOLDIERTYPE* MercPtrs[TOTAL_SOLDIERS];
 
 // MERC SLOTS - A LIST OF ALL ACTIVE MERCS
-SOLDIERTYPE	 *MercSlots[ TOTAL_SOLDIERS ];
-UINT32			 guiNumMercSlots;
+extern SOLDIERTYPE* MercSlots[TOTAL_SOLDIERS];
+extern UINT32       guiNumMercSlots;
 
 
-TacticalStatusType	gTacticalStatus;
+extern TacticalStatusType gTacticalStatus;
 
 
+BOOLEAN InitTacticalEngine(void);
+void ShutdownTacticalEngine(void);
 
-BOOLEAN InitTacticalEngine( );
-void ShutdownTacticalEngine( );
 
-
-BOOLEAN InitOverhead( );
-BOOLEAN ShutdownOverhead( );
+BOOLEAN InitOverhead(void);
+BOOLEAN ShutdownOverhead(void);
 SOLDIERTYPE* GetSoldier(UINT16 usSoldierIndex);
 
 INT16 NewOKDestination(const SOLDIERTYPE* pCurrSoldier, INT16 sGridNo, BOOLEAN fPeopleToo, INT8 bLevel);
@@ -179,7 +178,7 @@ extern BOOLEAN IsLocationSittableExcludingPeople( INT32 iMapIndex, BOOLEAN fOnRo
 extern BOOLEAN FlatRoofAboveGridNo( INT32 iMapIndex );
 
 
-BOOLEAN ExecuteOverhead( );
+BOOLEAN ExecuteOverhead(void);
 
 void EndTurn(UINT8 ubNextTeam);
 void StartPlayerTeamTurn( BOOLEAN fDoBattleSnd, BOOLEAN fEnteringCombatMode );
@@ -194,7 +193,7 @@ void BeginTeamTurn( UINT8 ubTeam );
 void SlideTo(INT16 sGridno, UINT16 usSoldierID , UINT16 usReasonID, BOOLEAN fSetLocator) ;
 void SlideToLocation( UINT16 usReasonID, INT16 sDestGridNo );
 
-void RebuildAllSoldierShadeTables( );
+void RebuildAllSoldierShadeTables(void);
 void HandlePlayerTeamMemberDeath( SOLDIERTYPE *pSoldier );
 BOOLEAN SoldierOnVisibleWorldTile( SOLDIERTYPE *pSoldier );
 
@@ -215,13 +214,13 @@ BOOLEAN IsValidTargetMerc( UINT8 ubSoldierID );
 
 
 // FUNCTIONS FOR MANIPULATING MERC SLOTS - A LIST OF ALL ACTIVE MERCS
-INT32	AddMercSlot( SOLDIERTYPE *pSoldier );
+INT32 AddMercSlot(SOLDIERTYPE* pSoldier);
 BOOLEAN RemoveMercSlot( SOLDIERTYPE *pSoldier  );
 
-INT32		AddAwaySlot( SOLDIERTYPE *pSoldier );
-BOOLEAN RemoveAwaySlot( SOLDIERTYPE *pSoldier );
-INT32		MoveSoldierFromMercToAwaySlot( SOLDIERTYPE * pSoldier );
-INT32		MoveSoldierFromAwayToMercSlot( SOLDIERTYPE * pSoldier );
+INT32   AddAwaySlot(SOLDIERTYPE* pSoldier);
+BOOLEAN RemoveAwaySlot(SOLDIERTYPE* pSoldier);
+INT32   MoveSoldierFromMercToAwaySlot(SOLDIERTYPE* pSoldier);
+INT32   MoveSoldierFromAwayToMercSlot(SOLDIERTYPE* pSoldier);
 
 void EnterCombatMode( UINT8 ubStartingTeam );
 void ExitCombatMode( );
@@ -241,7 +240,7 @@ BOOLEAN PlayerTeamFull( );
 
 void SetActionToDoOnceMercsGetToLocation(UINT8 ubActionCode, INT8 bNumMercsWaiting);
 
-void ResetAllMercSpeeds( );
+void ResetAllMercSpeeds(void);
 
 BOOLEAN HandleGotoNewGridNo( SOLDIERTYPE *pSoldier, BOOLEAN *pfKeepMoving, BOOLEAN fInitialMove, UINT16 usAnimState );
 
@@ -288,7 +287,5 @@ UINT8 NumPCsInSector( void );
 
 void SetSoldierNonNeutral( SOLDIERTYPE * pSoldier );
 void SetSoldierNeutral( SOLDIERTYPE * pSoldier );
-
-
 
 #endif

commit 4395efd056c109605c5a419676ac53830019f2a2

Fix the declaration of gzActionStr[], which was broken in r1448. Also fix the format specifiers for printing it. It is only used for debugging.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@1489 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Tactical/OppList.c
@@ -3685,7 +3685,7 @@ void DebugSoldierPage3( )
 		SetFontShade(LARGEFONT1, FONT_SHADE_GREEN);
 		gprintf( 0, LINE_HEIGHT * ubLine, L"Action:");
 		SetFontShade(LARGEFONT1, FONT_SHADE_NEUTRAL);
-		gprintf( 150, LINE_HEIGHT * ubLine, L"%ls", gzActionStr[ pSoldier->bAction ] );
+		gprintf(150, LINE_HEIGHT * ubLine, L"%hs", gzActionStr[pSoldier->bAction]);
 		if (pSoldier->uiStatusFlags & SOLDIER_ENEMY )
 		{
 			gprintf(350, LINE_HEIGHT * ubLine, L"Alert %hs", gzAlertStr[pSoldier->bAlertStatus]);
@@ -3739,7 +3739,7 @@ void DebugSoldierPage3( )
 		SetFontShade(LARGEFONT1, FONT_SHADE_GREEN);
 		gprintf( 0, LINE_HEIGHT * ubLine, L"Last Action:");
 		SetFontShade(LARGEFONT1, FONT_SHADE_NEUTRAL);
-		gprintf( 150, LINE_HEIGHT * ubLine, L"%ls", gzActionStr[ pSoldier->bLastAction ]  );
+		gprintf(150, LINE_HEIGHT * ubLine, L"%hs", gzActionStr[pSoldier->bLastAction]);
 		ubLine++;
 
 		if ( gubWatchedLocPoints[ pSoldier->ubID ][ 2 ] > 0 )

Build/Tactical/Overhead.c
@@ -153,7 +153,7 @@ UINT8 gbPlayerNum = 0;
 UINT16 gusSelectedSoldier = NO_SOLDIER;
 
 
-const char* const gzActionStr[][30] =
+const char* const gzActionStr[] =
 {
 	"NONE",
 

Build/Tactical/Overhead.h
@@ -148,7 +148,7 @@ extern UINT8 gbPlayerNum;
 
 extern UINT16 gusSelectedSoldier;
 
-extern const char* const gzActionStr[][30];
+extern const char* const gzActionStr[];
 
 // Soldier List used for all soldier overhead interaction
 extern SOLDIERTYPE  Menptr[TOTAL_SOLDIERS];

Build/TacticalAI/AIMain.c
@@ -469,7 +469,7 @@ void HandleSoldierAI( SOLDIERTYPE *pSoldier )
 			// display deadlock message
 			gfUIInDeadlock = TRUE;
 			gUIDeadlockedSoldier = pSoldier->ubID;
-			DebugAI( String("DEADLOCK soldier %d action %s ABC %d", pSoldier->ubID, gzActionStr[pSoldier->bAction], gTacticalStatus.ubAttackBusyCount ) );
+			DebugAI(String("DEADLOCK soldier %d action %hs ABC %d", pSoldier->ubID, gzActionStr[pSoldier->bAction], gTacticalStatus.ubAttackBusyCount));
 #else
 
 			// If we are in beta version, also report message!
@@ -1686,11 +1686,11 @@ INT8 ExecuteAction(SOLDIERTYPE *pSoldier)
 	#ifdef TESTAICONTROL
 	if (gfTurnBasedAI || gTacticalStatus.fAutoBandageMode)
 	{
-		DebugAI( String( "%d does %s (a.d. %d) in %d with %d APs left", pSoldier->ubID, gzActionStr[pSoldier->bAction], pSoldier->usActionData, pSoldier->sGridNo, pSoldier->bActionPoints ) );
+		DebugAI(String("%d does %hs (a.d. %d) in %d with %d APs left", pSoldier->ubID, gzActionStr[pSoldier->bAction], pSoldier->usActionData, pSoldier->sGridNo, pSoldier->bActionPoints));
 	}
 	#endif
 
-	DebugAI( String( "%d does %s (a.d. %d) at time %ld", pSoldier->ubID, gzActionStr[pSoldier->bAction], pSoldier->usActionData, GetJA2Clock() ) );
+	DebugAI(String("%d does %hs (a.d. %d) at time %ld", pSoldier->ubID, gzActionStr[pSoldier->bAction], pSoldier->usActionData, GetJA2Clock()));
 
 	switch (pSoldier->bAction)
 	{