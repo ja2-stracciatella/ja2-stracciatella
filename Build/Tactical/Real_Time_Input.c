ja2-stracciatella/ja2-stracciatella

commit df322a366d146986ccd3533483a3ace8001db950

Change BOOLEAN FindSoldierFromMouse(UINT16* pusSoldierIndex) to SOLDIERTYPE* FindSoldierFromMouse(void).
git-svn-id: svn://tron.homeunix.org/ja2/trunk@1759 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Tactical/Real_Time_Input.c
@@ -76,7 +76,6 @@ void GetRTMouseButtonInput(UINT32* puiNewEvent)
 
 static void QueryRTLeftButton(UINT32* puiNewEvent)
 {
-	UINT16	usSoldierIndex;
 	static	UINT32 uiSingleClickTime;
 	UINT16	usMapPos;
 	BOOLEAN fDone = FALSE;
@@ -235,20 +234,17 @@ static void QueryRTLeftButton(UINT32* puiNewEvent)
 					// HERE FOR CLICK-DRAG CLICK
 					switch( gCurrentUIMode )
 					{
-
 						case MOVE_MODE:
 						case CONFIRM_MOVE_MODE:
+						{
 							// First check if we clicked on a guy, if so, make selected if it's ours
-							if (FindSoldierFromMouse(&usSoldierIndex))
+							const SOLDIERTYPE* const s = FindSoldierFromMouse();
+							if (s != NULL)
 							{
 								 // Select guy
-								if (usSoldierIndex == gusSelectedSoldier)
+								if (s == GetSelectedMan() && s->bLife >= OKLIFE && !(s->uiStatusFlags & SOLDIER_VEHICLE))
 								{
-									const SOLDIERTYPE* const s = MercPtrs[usSoldierIndex];
-									if (s->bLife >= OKLIFE && !(s->uiStatusFlags & SOLDIER_VEHICLE))
-									{
-										*puiNewEvent = M_CHANGE_TO_ADJPOS_MODE;
-									}
+									*puiNewEvent = M_CHANGE_TO_ADJPOS_MODE;
 								}
 							}
 							else
@@ -300,6 +296,7 @@ static void QueryRTLeftButton(UINT32* puiNewEvent)
 								}
 							}
 							break;
+						}
 					}
 				}
 			}
@@ -439,18 +436,15 @@ static void QueryRTLeftButton(UINT32* puiNewEvent)
 															break;
 
 														case IDLE_MODE:
-
+														{
 															// First check if we clicked on a guy, if so, make selected if it's ours
-															if (FindSoldierFromMouse(&usSoldierIndex))
+															const SOLDIERTYPE* const s = FindSoldierFromMouse();
+															if (s != NULL && IsOwnedMerc(s))
 															{
-																 // Select guy
-																if (IsOwnedMerc(GetMan(usSoldierIndex)))
-																 {
-																		*puiNewEvent = I_SELECT_MERC;
-																 }
-
+																*puiNewEvent = I_SELECT_MERC;
 															}
 															break;
+														}
 
 													case HANDCURSOR_MODE:
 
@@ -647,8 +641,8 @@ static void QueryRTLeftButton(UINT32* puiNewEvent)
 																			if (pSoldier != NULL)
 																			{
 																				// First check if we clicked on a guy, if so, make selected if it's ours
-																				if (FindSoldierFromMouse(&usSoldierIndex) &&
-																						IsOwnedMerc(GetMan(usSoldierIndex)))
+																				const SOLDIERTYPE* const s = FindSoldierFromMouse();
+																				if (s != NULL && IsOwnedMerc(s))
 																				{
 																					 // Select guy
 																						*puiNewEvent = I_SELECT_MERC;

Build/Tactical/OppList.c
@@ -3185,14 +3185,12 @@ extern UINT32 guiNumBackSaves;
 
 void DebugSoldierPage1( )
 {
-	UINT16						usSoldierIndex;
 	UINT16						usMapPos;
 	UINT8							ubLine=0;
 
-	if (FindSoldierFromMouse(&usSoldierIndex))
+	const SOLDIERTYPE* const pSoldier = FindSoldierFromMouse();
+	if (pSoldier != NULL)
 	{
-		SOLDIERTYPE* pSoldier = GetSoldier(usSoldierIndex);
-
 		SetFont( LARGEFONT1 );
 		gprintf( 0,0,L"DEBUG SOLDIER PAGE ONE, GRIDNO %d", pSoldier->sGridNo );
 		SetFont( LARGEFONT1 );
@@ -3378,16 +3376,14 @@ void DebugSoldierPage2( )
 		"NORTH"
 	};
 
-	UINT16						usSoldierIndex;
 	UINT16						usMapPos;
   TILE_ELEMENT							 TileElem;
 	LEVELNODE					*pNode;
 	UINT8							ubLine;
 
-	if (FindSoldierFromMouse(&usSoldierIndex))
+	const SOLDIERTYPE* const pSoldier = FindSoldierFromMouse();
+	if (pSoldier != NULL)
 	{
-		SOLDIERTYPE* pSoldier = GetSoldier(usSoldierIndex);
-
 		SetFont( LARGEFONT1 );
 		gprintf( 0,0,L"DEBUG SOLDIER PAGE TWO, GRIDNO %d", pSoldier->sGridNo );
 		SetFont( LARGEFONT1 );
@@ -3654,14 +3650,12 @@ void DebugSoldierPage3( )
 		"BLACK"
 	};
 
-	UINT16						usSoldierIndex;
 	UINT16						usMapPos;
 	UINT8							ubLine;
 
-	if (FindSoldierFromMouse(&usSoldierIndex))
+	const SOLDIERTYPE* const pSoldier = FindSoldierFromMouse();
+	if (pSoldier != NULL)
 	{
-		SOLDIERTYPE* pSoldier = GetSoldier(usSoldierIndex);
-
 		SetFont( LARGEFONT1 );
 		gprintf( 0,0,L"DEBUG SOLDIER PAGE THREE, GRIDNO %d", pSoldier->sGridNo );
 		SetFont( LARGEFONT1 );
@@ -4046,13 +4040,11 @@ static void WriteQuantityAndAttachments(const OBJECTTYPE* pObject, INT32 yp)
 
 void DebugSoldierPage4( )
 {
-	UINT16						usSoldierIndex;
 	UINT8							ubLine;
 
-	if (FindSoldierFromMouse(&usSoldierIndex))
+	const SOLDIERTYPE* const pSoldier = FindSoldierFromMouse();
+	if (pSoldier != NULL)
 	{
-		SOLDIERTYPE* pSoldier = GetSoldier(usSoldierIndex);
-
 		SetFont( LARGEFONT1 );
 		gprintf( 0,0,L"DEBUG SOLDIER PAGE FOUR, GRIDNO %d", pSoldier->sGridNo );
 		SetFont( LARGEFONT1 );

Build/Tactical/Soldier_Find.c
@@ -75,18 +75,19 @@ SOLDIER_STACK_TYPE		gSoldierStack;
 BOOLEAN								gfHandleStack = FALSE;
 
 
-BOOLEAN FindSoldierFromMouse(UINT16* pusSoldierIndex)
+SOLDIERTYPE* FindSoldierFromMouse(void)
 {
-	INT16							sMapPos;
+	INT16 sMapPos;
 	if (GetMouseMapPos(&sMapPos))
 	{
-		if (FindSoldier(sMapPos, pusSoldierIndex, FINDSOLDIERSAMELEVEL(gsInterfaceLevel)))
+		UINT16 soldier_index;
+		if (FindSoldier(sMapPos, &soldier_index, FINDSOLDIERSAMELEVEL(gsInterfaceLevel)))
 		{
-			return TRUE;
+			return GetMan(soldier_index);
 		}
 	}
 
-	return FALSE;
+	return NULL;
 }
 
 

Build/Tactical/Soldier_Find.h
@@ -27,7 +27,7 @@
 #define			FINDSOLDIERSELECTIVESAMELEVEL( l )		(	( ( FIND_SOLDIER_SELECTIVE | FIND_SOLDIER_SAMELEVEL ) | ( l << 16 ) ) )
 
 
-BOOLEAN FindSoldierFromMouse(UINT16* pusSoldierIndex);
+SOLDIERTYPE* FindSoldierFromMouse(void);
 BOOLEAN FindSoldier(INT16 sGridNo, UINT16* pusSoldierIndex, UINT32 uiFlags);
 SOLDIERTYPE * SimpleFindSoldier( INT16 sGridNo, INT8 bLevel );
 

commit e1b9a499a2ba0cacfc9a169602f705cb859bc358

Fix a crash caused by r1759 when selecting a merc and no merc was selected before.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@1784 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Tactical/Real_Time_Input.c
@@ -242,7 +242,7 @@ static void QueryRTLeftButton(UINT32* puiNewEvent)
 							if (s != NULL)
 							{
 								 // Select guy
-								if (s == GetSelectedMan() && s->bLife >= OKLIFE && !(s->uiStatusFlags & SOLDIER_VEHICLE))
+								if (s->ubID == gusSelectedSoldier && s->bLife >= OKLIFE && !(s->uiStatusFlags & SOLDIER_VEHICLE))
 								{
 									*puiNewEvent = M_CHANGE_TO_ADJPOS_MODE;
 								}