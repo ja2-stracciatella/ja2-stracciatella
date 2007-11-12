ja2-stracciatella/ja2-stracciatella

commit 4fab271331af9d22d440e596fadd42db4db34583

Use EXTR/INJ to load/save struct SCHEDULENODE from/to savegames.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@1911 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Strategic/Scheduling.c
@@ -1,5 +1,6 @@
 #include "FileMan.h"
 #include "Font_Control.h"
+#include "LoadSaveData.h"
 #include "Types.h"
 #include "Scheduling.h"
 #include "Soldier_Control.h"
@@ -395,14 +396,11 @@ extern BOOLEAN gfSchedulesHosed;
 BOOLEAN LoadSchedulesFromSave( HWFILE hFile )
 {
 	SCHEDULENODE *pSchedule = NULL;
-	SCHEDULENODE temp;
 	UINT8 ubNum;
 	UINT32 ubRealNum;
-	UINT32 uiNumBytesToRead;
 
 	//LOADDATA( &ubNum, *hBuffer, sizeof( UINT8 ) );
-	uiNumBytesToRead = sizeof( UINT8 );
-	if (!FileRead(hFile, &ubNum, uiNumBytesToRead)) return FALSE;
+	if (!FileRead(hFile, &ubNum, sizeof(ubNum))) return FALSE;
 
 	//Hack problem with schedules getting misaligned.
 	ubRealNum = gfSchedulesHosed ? ubNum + 256 : ubNum;
@@ -410,23 +408,31 @@ BOOLEAN LoadSchedulesFromSave( HWFILE hFile )
 	gubScheduleID = 1;
 	while( ubRealNum )
 	{
-		uiNumBytesToRead = sizeof( SCHEDULENODE );
-		if (!FileRead(hFile, &temp, uiNumBytesToRead)) return FALSE;
-		//LOADDATA( &temp, *hBuffer, sizeof( SCHEDULENODE ) );
+		BYTE data[36];
+		if (!FileRead(hFile, data, sizeof(data))) return FALSE;
 
-		if( gpScheduleList )
+		SCHEDULENODE* const node = MemAlloc(sizeof(*node));
+
+		const BYTE* s = data;
+		EXTR_PTR( s, node->next)
+		EXTR_U16A(s, node->usTime,   lengthof(node->usTime))
+		EXTR_U16A(s, node->usData1,  lengthof(node->usData1))
+		EXTR_U16A(s, node->usData2,  lengthof(node->usData2))
+		EXTR_U8A( s, node->ubAction, lengthof(node->ubAction))
+		EXTR_U8(  s, node->ubScheduleID)
+		EXTR_U8(  s, node->ubSoldierID)
+		EXTR_U16( s, node->usFlags)
+		Assert(s == endof(data));
+
+		if (pSchedule != NULL)
 		{
-			pSchedule->next = (SCHEDULENODE*)MemAlloc( sizeof( SCHEDULENODE ) );
-			Assert( pSchedule->next );
-			pSchedule = pSchedule->next;
-			*pSchedule = temp;
+			pSchedule->next = node;
 		}
 		else
 		{
-			gpScheduleList = (SCHEDULENODE*)MemAlloc( sizeof( SCHEDULENODE ) );
-			Assert( gpScheduleList );
-			*gpScheduleList = temp;
-			pSchedule = gpScheduleList;
+			Assert(gpScheduleList == NULL);
+			pSchedule      = node;
+			gpScheduleList = node;
 		}
 
 		// should be unnecessary here, then we can toast reconnect schedule
@@ -435,7 +441,6 @@ BOOLEAN LoadSchedulesFromSave( HWFILE hFile )
 		pSchedule->ubSoldierID = NO_SOLDIER;
 		*/
 
-		pSchedule->next = NULL;
 		gubScheduleID++;
 		ubRealNum--;
 	}
@@ -527,7 +532,20 @@ BOOLEAN SaveSchedules( HWFILE hFile )
 			{
 				return( TRUE );
 			}
-			if (!FileWrite(hFile, curr, sizeof(SCHEDULENODE))) return FALSE;
+
+			BYTE data[36];
+			BYTE* d = data;
+			INJ_PTR( d, curr->next)
+			INJ_U16A(d, curr->usTime,   lengthof(curr->usTime))
+			INJ_U16A(d, curr->usData1,  lengthof(curr->usData1))
+			INJ_U16A(d, curr->usData2,  lengthof(curr->usData2))
+			INJ_U8A( d, curr->ubAction, lengthof(curr->ubAction))
+			INJ_U8(  d, curr->ubScheduleID)
+			INJ_U8(  d, curr->ubSoldierID)
+			INJ_U16( d, curr->usFlags)
+			Assert(d == endof(data));
+
+			if (!FileWrite(hFile, data, sizeof(data))) return FALSE;
 		}
 		curr = curr->next;
 	}

Build/Strategic/Scheduling.h
@@ -51,7 +51,6 @@ typedef struct SCHEDULENODE
 	UINT8 ubSoldierID;
 	UINT16 usFlags;
 }SCHEDULENODE;
-CASSERT(sizeof(SCHEDULENODE) == 36)
 
 
 extern UINT8				gubScheduleID;

commit cd4e13f29d18be6ccef54d792f8dce386f0545ea

Fix loading schedules from the savegames.  This was broken in r1911.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@1942 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Strategic/Scheduling.c
@@ -434,14 +434,15 @@ BOOLEAN LoadSchedulesFromSave( HWFILE hFile )
 
 		if (pSchedule != NULL)
 		{
+			Assert(pSchedule->next == NULL);
 			pSchedule->next = node;
 		}
 		else
 		{
 			Assert(gpScheduleList == NULL);
-			pSchedule      = node;
 			gpScheduleList = node;
 		}
+		pSchedule = node;
 
 		gubScheduleID++;
 		ubRealNum--;