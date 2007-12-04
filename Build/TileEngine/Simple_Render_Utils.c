903452208bded04d7874c7a17a359bde887c467c
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2007-12-04 07:47:44 +0000

    Use ConvertGridNoToCellXY() to reduce code duplication.
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@2304 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/TileEngine/Simple_Render_Utils.c
+++ Build/TileEngine/Simple_Render_Utils.c
@@ -9,24 +9,12 @@ void MarkMapIndexDirty( INT32 iMapIndex )
 	SetRenderFlags( RENDER_FLAG_MARKED );
 }
 
-void CenterScreenAtMapIndex( INT32 iMapIndex )
+
+void CenterScreenAtMapIndex(const INT32 iMapIndex)
 {
-	INT16 sWorldX, sWorldY;
-	INT16 sCellX, sCellY;
-
-	//Get X, Y world GRID Coordinates
-	sWorldY = ( iMapIndex / WORLD_COLS );
-	sWorldX = iMapIndex - ( sWorldY * WORLD_COLS );
-
-	//Convert into cell coords
-	sCellY = sWorldY * CELL_Y_SIZE;
-	sCellX = sWorldX * CELL_X_SIZE;
-
 	//Set the render values, so that the screen will render here next frame.
-	gsRenderCenterX = sCellX;
-	gsRenderCenterY = sCellY;
-
-	SetRenderFlags( RENDER_FLAG_FULL );
+	ConvertGridNoToCellXY(iMapIndex, &gsRenderCenterX, &gsRenderCenterY);
+	SetRenderFlags(RENDER_FLAG_FULL);
 }
 
 
