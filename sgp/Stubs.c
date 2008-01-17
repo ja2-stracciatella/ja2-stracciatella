638db1d3d1904f709fec8fb36cca44c8f50c25f3
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2008-01-17 14:10:57 +0000

    Remove GetPrivateProfileString().
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@2742 5e31c081-6ce3-0310-bb30-f584a8092234

--- sgp/Stubs.c
+++ sgp/Stubs.c
@@ -1,13 +1,5 @@
+#include <stdio.h>
 #include "Stubs.h"
-#include "Types.h"
-
-
-size_t GetPrivateProfileString(const char* app_name, const char* key_name, const char* default_value, char* out_buf, size_t buf_size, const char* filename)
-{
-	FIXME
-	strlcpy(out_buf, default_value, buf_size);
-	return strlen(out_buf);
-}
 
 
 void OutputDebugString(const char* str)
--- sgp/Stubs.h
+++ sgp/Stubs.h
@@ -1,13 +1,8 @@
 #ifndef STUBS_H
 #define STUBS_H
 
-#include <stddef.h>
-
 
 #define FILE_ATTRIBUTE_DIRECTORY 0 /* XXX */
-
-
-size_t GetPrivateProfileString(const char* app_name, const char* key_name, const char* default_value, char* out_buf, size_t buf_size, const char* filename);
 
 
 void OutputDebugString(const char*);
