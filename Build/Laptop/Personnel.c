ja2-stracciatella/ja2-stracciatella

commit dd570c0b1291050e45c75607273253ed2beba1e6

Handle selection of the first displayed merc consistently in the personnel screen.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@1955 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Laptop/Personnel.c
@@ -287,7 +287,7 @@ void GameInitPersonnel(void)
 static void CreateDestroyCurrentDepartedMouseRegions(BOOLEAN create);
 static void CreateDestroyMouseRegionsForPersonnelPortraits(BOOLEAN create);
 static void CreatePersonnelButtons(void);
-static INT32 GetIdOfFirstDisplayedMerc(void);
+static void SelectFirstDisplayedMerc(void);
 static INT32 GetNumberOfMercsDeadOrAliveOnPlayersTeam(void);
 static BOOLEAN LoadPersonnelGraphics(void);
 static BOOLEAN LoadPersonnelScreenBackgroundGraphics(void);
@@ -297,8 +297,6 @@ static void SetPersonnelButtonStates(void);
 void EnterPersonnel(void)
 {
 	fReDrawScreenFlag=TRUE;
-
-	iCurrentPersonSelectedId = -1;
 
 	uiCurrentInventoryIndex = 0;
 	guiSliderPosition = 0;
@@ -318,11 +316,7 @@ void EnterPersonnel(void)
 	// render screen
 	RenderPersonnel();
 
-	// how many people do we have?..if you have someone set default to 0
-	if (GetNumberOfMercsDeadOrAliveOnPlayersTeam() > 0)
-	{
-		iCurrentPersonSelectedId = GetIdOfFirstDisplayedMerc();
-	}
+	SelectFirstDisplayedMerc();
 
 	CreateDestroyMouseRegionsForPersonnelPortraits(TRUE);
 	CreateDestroyCurrentDepartedMouseRegions(TRUE);
@@ -1882,14 +1876,7 @@ static void PersonnelCurrentTeamCallback(MOUSE_REGION* pRegion, INT32 iReason)
 
 		if (fCurrentTeamMode)
 		{
-			iCurrentPersonSelectedId = -1;
-
-			// how many people do we have?..if you have someone set default to 0
-			if (GetNumberOfMercsDeadOrAliveOnPlayersTeam() > 0)
-			{
-				// get id of first merc in list
-				iCurrentPersonSelectedId = GetIdOfFirstDisplayedMerc();
-			}
+			SelectFirstDisplayedMerc();
 		}
 
 		fCurrentTeamMode = TRUE;
@@ -1906,13 +1893,7 @@ static void PersonnelDepartedTeamCallback(MOUSE_REGION* pRegion, INT32 iReason)
 
 		if (!fCurrentTeamMode)
 		{
-			iCurrentPersonSelectedId = -1;
-
-			// how many departed people?
-			if (GetNumberOfPastMercsOnPlayersTeam() > 0)
-			{
-				iCurrentPersonSelectedId = 0;
-			}
+			SelectFirstDisplayedMerc();
 
 			//Switch the panel on the right to be the stat panel
 			gubPersonnelInfoState = PERSONNEL_STAT_BTN;
@@ -2449,8 +2430,8 @@ BOOLEAN RemoveNewlyHiredMercFromPersonnelDepartedList(UINT8 ubProfile)
 }
 
 
-// grab the id of the first merc being displayed
-static INT32 GetIdOfFirstDisplayedMerc(void)
+// Select the first displayed merc, if there is any
+static void SelectFirstDisplayedMerc(void)
 {
 	// set current soldier
 	if (fCurrentTeamMode)
@@ -2463,15 +2444,15 @@ static INT32 GetIdOfFirstDisplayedMerc(void)
 		{
 			if (pSoldier->bActive && !(pSoldier->uiStatusFlags & SOLDIER_VEHICLE))
 			{
-				return 0;
+				iCurrentPersonSelectedId = 0;
+				return;
 			}
 		}
-		return -1;
+		iCurrentPersonSelectedId = -1;
 	}
 	else
 	{
-		// run through list of soldier on players old team...the slot id will be translated
-		return 0;
+		iCurrentPersonSelectedId = GetNumberOfPastMercsOnPlayersTeam() > 0 ? 0 : -1;
 	}
 }
 

commit 142e12d4aa78f439506be7a9bd254c282118af95

Fix a regression caused by r1955, which lead to a crash in the personnel screen, when the number of mercs on the team dropped and a slot now after last was selected.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@3056 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Laptop/Personnel.c
@@ -297,11 +297,10 @@ void EnterPersonnel(void)
 	// load personnel
 	LoadPersonnelScreenBackgroundGraphics();
 
+	SelectFirstDisplayedMerc();
 
 	// render screen
 	RenderPersonnel();
-
-	SelectFirstDisplayedMerc();
 
 	CreateDestroyMouseRegionsForPersonnelPortraits(TRUE);
 	CreateDestroyCurrentDepartedMouseRegions(TRUE);