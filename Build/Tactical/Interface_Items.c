ja2-stracciatella/ja2-stracciatella

commit 7f81309a877a8216c323de6c7e4bcc98a08d6ce5

Change BOOLEAN IsValidTalkableNPCFromMouse(UINT8* pubSoldierID, BOOLEAN, BOOLEAN, BOOLEAN) to SOLDIERTYPE* GetValidTalkableNPCFromMouse(BOOLEAN, BOOLEAN, BOOLEAN).
git-svn-id: svn://tron.homeunix.org/ja2/trunk@1766 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Tactical/Interface_Items.c
@@ -3884,7 +3884,6 @@ void DrawItemTileCursor( )
 {
 	UINT16						usMapPos;
 	UINT16						usIndex;
-	UINT8							ubSoldierID;
 	INT16							sAPCost;
 	BOOLEAN						fRecalc;
 	UINT32						uiCursorFlags;
@@ -3922,7 +3921,7 @@ void DrawItemTileCursor( )
 		gfUIHandleShowMoveGrid = FALSE;
 
 		// If we are over a talkable guy, set flag
-		if ( IsValidTalkableNPCFromMouse( &ubSoldierID, TRUE, FALSE, TRUE ) )
+		if (GetValidTalkableNPCFromMouse(TRUE, FALSE, TRUE) != NULL)
 		{
 			fGiveItem = TRUE;
 		}
@@ -4193,14 +4192,12 @@ BOOLEAN HandleItemPointerClick( UINT16 usMapPos )
 {
 	// Determine what to do
 	UINT8 ubDirection;
-	UINT8	ubSoldierID;
 	UINT16	  usItem;
 	INT16			sAPCost;
 	SOLDIERTYPE		*pSoldier=NULL;
 	UINT8			ubThrowActionCode=0;
 	UINT32		uiThrowActionData=0;
 	INT16			sEndZ = 0;
-	BOOLEAN		fGiveItem = FALSE;
 	OBJECTTYPE TempObject;
 	INT16			sGridNo;
 	INT16			sDist;
@@ -4257,10 +4254,8 @@ BOOLEAN HandleItemPointerClick( UINT16 usMapPos )
 	}
 
 	// SEE IF WE ARE OVER A TALKABLE GUY!
-	if ( IsValidTalkableNPCFromMouse( &ubSoldierID, TRUE, FALSE, TRUE ) )
-	{
-		fGiveItem = TRUE;
-	}
+	SOLDIERTYPE* const tgt = GetValidTalkableNPCFromMouse(TRUE, FALSE, TRUE);
+	BOOLEAN fGiveItem = (tgt != NULL);
 
 	// OK, if different than default, change....
 	if ( gfItemPointerDifferentThanDefault )
@@ -4278,10 +4273,10 @@ BOOLEAN HandleItemPointerClick( UINT16 usMapPos )
 		usItem = gpItemPointer->usItem;
 
 		// If the target is a robot,
-		if ( MercPtrs[ ubSoldierID ]->uiStatusFlags & SOLDIER_ROBOT )
+		if (tgt->uiStatusFlags & SOLDIER_ROBOT)
 		{
 			// Charge APs to reload robot!
-			sAPCost = GetAPsToReloadRobot( gpItemPointerSoldier,  MercPtrs[ ubSoldierID ] );
+			sAPCost = GetAPsToReloadRobot(gpItemPointerSoldier, tgt);
 		}
 		else
 		{
@@ -4300,12 +4295,12 @@ BOOLEAN HandleItemPointerClick( UINT16 usMapPos )
 		}
 /*
 		//if the user just clicked on an arms dealer
-		if( IsMercADealer( MercPtrs[ ubSoldierID ]->ubProfile ) )
+		if (IsMercADealer(tgt->ubProfile))
 		{
 			if ( EnoughPoints( gpItemPointerSoldier, sAPCost, 0, TRUE ) )
 			{
 				//Enter the shopkeeper interface
-				EnterShopKeeperInterfaceScreen( MercPtrs[ ubSoldierID ]->ubProfile );
+				EnterShopKeeperInterfaceScreen(tgt->ubProfile);
 
 				EndItemPointer( );
 			}
@@ -4317,10 +4312,10 @@ BOOLEAN HandleItemPointerClick( UINT16 usMapPos )
 		if ( EnoughPoints( gpItemPointerSoldier, sAPCost, 0, TRUE ) )
 		{
 			// If we are a robot, check if this is proper item to reload!
-			if ( MercPtrs[ ubSoldierID ]->uiStatusFlags & SOLDIER_ROBOT )
+			if (tgt->uiStatusFlags & SOLDIER_ROBOT)
 			{
 				// Check if we can reload robot....
-				if ( IsValidAmmoToReloadRobot( MercPtrs[ ubSoldierID ], &TempObject ) )
+				if (IsValidAmmoToReloadRobot(tgt, &TempObject))
 				{
 					 INT16	sActionGridNo;
 					 UINT8	ubDirection;
@@ -4328,7 +4323,7 @@ BOOLEAN HandleItemPointerClick( UINT16 usMapPos )
 
 					 // Walk up to him and reload!
 					 // See if we can get there to stab
-					 sActionGridNo =  FindAdjacentGridEx( gpItemPointerSoldier, MercPtrs[ ubSoldierID ]->sGridNo, &ubDirection, &sAdjustedGridNo, TRUE, FALSE );
+					 sActionGridNo = FindAdjacentGridEx(gpItemPointerSoldier, tgt->sGridNo, &ubDirection, &sAdjustedGridNo, TRUE, FALSE);
 
 					 if ( sActionGridNo != -1 && gbItemPointerSrcSlot != NO_SLOT )
 					 {
@@ -4372,13 +4367,13 @@ BOOLEAN HandleItemPointerClick( UINT16 usMapPos )
 				//if (gbItemPointerSrcSlot != NO_SLOT )
 				{
 					// Give guy this item.....
-					SoldierGiveItem( gpItemPointerSoldier, MercPtrs[ ubSoldierID ], &TempObject, gbItemPointerSrcSlot );
+					SoldierGiveItem(gpItemPointerSoldier, tgt, &TempObject, gbItemPointerSrcSlot);
 
 					gfDontChargeAPsToPickup = FALSE;
 					EndItemPointer( );
 
 					// If we are giving it to somebody not on our team....
-					if ( MercPtrs[ ubSoldierID ]->ubProfile < FIRST_RPC || RPC_RECRUITED( MercPtrs[ ubSoldierID ] ) )
+					if (tgt->ubProfile < FIRST_RPC || RPC_RECRUITED(tgt))
 					{
 
 					}

Build/Tactical/Handle_UI.c
@@ -4271,10 +4271,8 @@ static UINT32 UIHandleLCLook(UI_EVENT* pUIEvent)
 
 static UINT32 UIHandleTOnTerrain(UI_EVENT* pUIEvent)
 {
-	UINT8							ubTargID;
 	UINT32						uiRange;
 	UINT16						usMapPos;
-	BOOLEAN						fValidTalkableGuy = FALSE;
 	INT16							sTargetGridNo;
 	INT16							sDistVisible;
 
@@ -4300,14 +4298,11 @@ static UINT32 UIHandleTOnTerrain(UI_EVENT* pUIEvent)
 
 
 	//CHECK FOR VALID TALKABLE GUY HERE
-	fValidTalkableGuy = IsValidTalkableNPCFromMouse( &ubTargID, FALSE, TRUE , FALSE );
+	const SOLDIERTYPE* const tgt = GetValidTalkableNPCFromMouse(FALSE, TRUE, FALSE);
 
 	// USe cursor based on distance
 	// Get distance away
-	if ( fValidTalkableGuy )
-	{
-		sTargetGridNo = MercPtrs[ ubTargID ]->sGridNo;
-	}
+	if (tgt != NULL) sTargetGridNo = tgt->sGridNo;
 
 	uiRange = GetRangeFromGridNoDiff( pSoldier->sGridNo, sTargetGridNo );
 
@@ -4319,7 +4314,7 @@ static UINT32 UIHandleTOnTerrain(UI_EVENT* pUIEvent)
 
 	if ( uiRange <= NPC_TALK_RADIUS )
 	{
-		if ( fValidTalkableGuy )
+		if (tgt != NULL)
 		{
 			guiNewUICursor = TALK_A_UICURSOR;
 		}
@@ -4330,7 +4325,7 @@ static UINT32 UIHandleTOnTerrain(UI_EVENT* pUIEvent)
 	}
 	else
 	{
-		if ( fValidTalkableGuy )
+		if (tgt != NULL)
 		{
 			//guiNewUICursor = TALK_OUT_RANGE_A_UICURSOR;
 			guiNewUICursor = TALK_A_UICURSOR;
@@ -4341,7 +4336,7 @@ static UINT32 UIHandleTOnTerrain(UI_EVENT* pUIEvent)
 		}
 	}
 
-	if ( fValidTalkableGuy )
+	if (tgt != NULL)
 	{
 		if ( !SoldierTo3DLocationLineOfSightTest( pSoldier, sTargetGridNo,  pSoldier->bLevel, 3, (UINT8) sDistVisible, TRUE ) )
 		{
@@ -4960,17 +4955,12 @@ static UINT32 UIHandleLAEndLockOurTurn(UI_EVENT* pUIEvent)
 }
 
 
-
-BOOLEAN IsValidTalkableNPCFromMouse( UINT8 *pubSoldierID , BOOLEAN fGive, BOOLEAN fAllowMercs, BOOLEAN fCheckCollapsed )
+SOLDIERTYPE* GetValidTalkableNPCFromMouse(BOOLEAN fGive, BOOLEAN fAllowMercs, BOOLEAN fCheckCollapsed)
 {
 	// Check if there is a guy here to talk to!
-	if (gusUIFullTargetID != NOBODY)
-	{
-			*pubSoldierID = (UINT8)gusUIFullTargetID;
-			return( IsValidTalkableNPC( (UINT8)gusUIFullTargetID, fGive, fAllowMercs, fCheckCollapsed ) );
-	}
-
-	return( FALSE );
+	if (gusUIFullTargetID == NOBODY) return NULL;
+	if (!IsValidTalkableNPC(gusUIFullTargetID, fGive, fAllowMercs, fCheckCollapsed)) return NULL;
+	return GetMan(gusUIFullTargetID);
 }
 
 

Build/Tactical/Handle_UI.h
@@ -246,7 +246,7 @@ BOOLEAN UIMouseOnValidAttackLocation( SOLDIERTYPE *pSoldier );
 
 BOOLEAN UIOkForItemPickup( SOLDIERTYPE *pSoldier, INT16 sGridNo );
 
-BOOLEAN IsValidTalkableNPCFromMouse( UINT8 *pubSoldierID, BOOLEAN fGive, BOOLEAN fAllowMercs, BOOLEAN fCheckCollapsed );
+SOLDIERTYPE* GetValidTalkableNPCFromMouse(BOOLEAN fGive, BOOLEAN fAllowMercs, BOOLEAN fCheckCollapsed);
 BOOLEAN IsValidTalkableNPC( UINT8 ubSoldierID, BOOLEAN fGive, BOOLEAN fAllowMercs, BOOLEAN fCheckCollapsed );
 
 BOOLEAN HandleTalkInit(  );

commit 624d3581c1325141f984d86df3d070a592ed2f43

Fix a crash caused by r1766 when using alternate give mode (right-click) to hand an item to another merc in tactical mode.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@3104 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Tactical/Interface_Items.c
@@ -3687,8 +3687,8 @@ BOOLEAN HandleItemPointerClick( UINT16 usMapPos )
 	}
 
 	// SEE IF WE ARE OVER A TALKABLE GUY!
-	SOLDIERTYPE* const tgt = GetValidTalkableNPCFromMouse(TRUE, FALSE, TRUE);
-	BOOLEAN fGiveItem = (tgt != NULL);
+	SOLDIERTYPE* const tgt = gUIFullTarget;
+	BOOLEAN fGiveItem = tgt != NULL && IsValidTalkableNPC(tgt, TRUE, FALSE, TRUE);
 
 	// OK, if different than default, change....
 	if ( gfItemPointerDifferentThanDefault )