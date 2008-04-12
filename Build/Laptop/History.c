ja2-stracciatella/ja2-stracciatella

commit 4d20e13d81a34fa6ea4880c1db1dab7de57da267

Fix a vanilla bug: When starting a new game, the date of the first history entry was set to the date of the last loaded game. Noticed by: "InHuMan"
git-svn-id: svn://tron.homeunix.org/ja2/trunk@3721 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Strategic/Game_Init.c
@@ -408,10 +408,8 @@ BOOLEAN InitNewGame( BOOLEAN fReset )
 	// IF our first time, go into laptop!
 	if ( gubScreenCount == 0 )
 	{
-		//Init the laptop here
-		InitLaptopAndLaptopScreens();
-
 		InitStrategicLayer();
+		InitLaptopAndLaptopScreens();
 
 		// Set new game flag
 		SetLaptopNewGameFlag( );

commit 1c81cdfdb4a9badf26fee98e487887175454dca3

Fix a regression introduced in r3721: The letter delivery quest did not show up in the history log. Reported by: "InHuMan"
git-svn-id: svn://tron.homeunix.org/ja2/trunk@3728 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Laptop/History.c
@@ -161,9 +161,6 @@ void GameInitHistory()
 		FileClearAttributes( HISTORY_DATA_FILE );
 	  FileDelete( HISTORY_DATA_FILE );
 	}
-
-	AddHistoryToPlayersLog(HISTORY_ACCEPTED_ASSIGNMENT_FROM_ENRICO, 0, GetWorldTotalMin( ), -1, -1);
-
 }
 
 

Build/Strategic/Game_Init.c
@@ -403,8 +403,8 @@ BOOLEAN InitNewGame( BOOLEAN fReset )
 	// IF our first time, go into laptop!
 	if ( gubScreenCount == 0 )
 	{
-		InitStrategicLayer();
 		InitLaptopAndLaptopScreens();
+		InitStrategicLayer();
 
 		// Set new game flag
 		SetLaptopNewGameFlag( );

Build/Strategic/Quests.c
@@ -1252,6 +1252,7 @@ void CheckForQuests( UINT32 uiDay )
 	// already started
 	if (gubQuest[QUEST_DELIVER_LETTER] == QUESTNOTSTARTED)
 	{
+		AddHistoryToPlayersLog(HISTORY_ACCEPTED_ASSIGNMENT_FROM_ENRICO, 0, GetWorldTotalMin(), -1, -1);
 		StartQuest( QUEST_DELIVER_LETTER, -1, -1 );
 #ifdef TESTQUESTS
 		ScreenMsg( MSG_FONT_RED, MSG_DEBUG, L"Started DELIVER LETTER quest");