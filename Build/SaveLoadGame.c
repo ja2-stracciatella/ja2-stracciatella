ja2-stracciatella/ja2-stracciatella

commit 356e3e06b9d4faac5c11386231e4d85604038cdb

Fix a vanilla memory leak when loading savegames.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@3598 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Strategic/Strategic_Movement.c
@@ -2851,8 +2851,7 @@ static BOOLEAN LoadWayPointList(HWFILE hFile, GROUP* pGroup);
 
 BOOLEAN LoadStrategicMovementGroupsFromSavedGameFile(const HWFILE f)
 {
-	// Delete the existing group list
-	while (gpGroupList) RemoveGroupFromList(gpGroupList);
+	RemoveAllGroups();
 
 	// Load the number of nodes in the list
 	UINT32 uiNumberOfGroups;

commit 0f4f7abc616110d8becf061243cc56ff5b8b5259

Resolve a regression in r3598, which broke strategic movement when loading a savegame and a game was already running (deleting data of old game with new game partially loaded).
git-svn-id: svn://tron.homeunix.org/ja2/trunk@3803 5e31c081-6ce3-0310-bb30-f584a8092234

Build/SaveLoadGame.c
@@ -1100,6 +1100,7 @@ BOOLEAN LoadSavedGame( UINT8 ubSavedGameID )
 	uiRelStartPerc = uiRelEndPerc =0;
 
   TrashAllSoldiers( );
+  RemoveAllGroups();
 
 	//Empty the dialogue Queue cause someone could still have a quote in waiting
 	EmptyDialogueQueue( );

Build/Strategic/Strategic_Movement.c
@@ -2839,7 +2839,7 @@ static BOOLEAN LoadWayPointList(HWFILE hFile, GROUP* pGroup);
 
 BOOLEAN LoadStrategicMovementGroupsFromSavedGameFile(const HWFILE f)
 {
-	RemoveAllGroups();
+	Assert(gpGroupList == NULL);
 
 	// Load the number of nodes in the list
 	UINT32 uiNumberOfGroups;