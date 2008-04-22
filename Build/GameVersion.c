6f1c56108e48ca472395e9fc5d9dc1c09bd3313b
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2008-04-22 20:09:05 +0000

    Prepare for 0.9.
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@3873 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/GameVersion.c
+++ Build/GameVersion.c
@@ -11,7 +11,7 @@ const char g_version_label[] =
 	"Beta v. 0.98";
 #else
 	//RELEASE BUILD VERSION
-	"Release v0.8 - Lemon";
+	"Release v0.9 - Blueberry";
 #endif
 
 const char g_version_number[] = "Build 04.12.02";
