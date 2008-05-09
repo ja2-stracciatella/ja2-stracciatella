46f8a3b75b92733a827efacf325a27ab300a7004
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2008-05-09 08:10:44 +0000

    Remove unnecessary typedefs in forward declarations.
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@4026 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/JA2Types.h
+++ Build/JA2Types.h
@@ -8,30 +8,30 @@ typedef INT16 GridNo;
 typedef UINT8 ProfileID;
 typedef INT32 TIMECOUNTER;
 
-typedef struct ANIM_PROF          ANIM_PROF;
-typedef struct BACKGROUND_SAVE    BACKGROUND_SAVE;
-typedef struct BULLET             BULLET;
-typedef struct DB_STRUCTURE_REF   DB_STRUCTURE_REF;
-typedef struct EXPLOSIONTYPE      EXPLOSIONTYPE;
-typedef struct FACETYPE           FACETYPE;
-typedef struct GROUP              GROUP;
-typedef struct ITEM_POOL          ITEM_POOL;
-typedef struct LEVELNODE          LEVELNODE;
-typedef struct LIGHT_SPRITE       LIGHT_SPRITE;
-typedef struct MERCPROFILESTRUCT  MERCPROFILESTRUCT;
-typedef struct PathSt             PathSt;
-typedef struct PopUpBox           PopUpBox;
-typedef struct REAL_OBJECT        REAL_OBJECT;
-typedef struct SMOKEEFFECT        SMOKEEFFECT;
-typedef struct SOLDIERTYPE        SOLDIERTYPE;
-typedef struct STRUCTURE          STRUCTURE;
-typedef struct STRUCTURE_FILE_REF STRUCTURE_FILE_REF;
-typedef struct TILE_IMAGERY       TILE_IMAGERY;
-typedef struct VEHICLETYPE        VEHICLETYPE;
-typedef struct VIDEO_OVERLAY      VIDEO_OVERLAY;
+struct ANIM_PROF;
+struct BACKGROUND_SAVE;
+struct BULLET;
+struct DB_STRUCTURE_REF;
+struct EXPLOSIONTYPE;
+struct FACETYPE;
+struct GROUP;
+struct ITEM_POOL;
+struct LEVELNODE;
+struct LIGHT_SPRITE;
+struct MERCPROFILESTRUCT;
+struct PathSt;
+struct PopUpBox;
+struct REAL_OBJECT;
+struct SMOKEEFFECT;
+struct SOLDIERTYPE;
+struct STRUCTURE;
+struct STRUCTURE_FILE_REF;
+struct TILE_IMAGERY;
+struct VEHICLETYPE;
+struct VIDEO_OVERLAY;
 
 #ifdef JA2EDITOR
-typedef struct SUMMARYFILE SUMMARYFILE;
+struct SUMMARYFILE;
 #endif
 
 typedef void (*MSGBOX_CALLBACK)(UINT8 bExitValue);
