7a106703663cafe5d30afb295ce40fddfe5cfc87
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2008-06-01 12:58:24 +0000

    Clean up AddLightToUndoList().
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@4291 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/Editor/Editor_Undo.cc
+++ Build/Editor/Editor_Undo.cc
@@ -245,43 +245,33 @@ static void CropStackToMaxLength(INT32 iMaxCmds)
 //this will handle the way the undo command is handled.  If there is no lightradius in
 //our saved light, then we intend on erasing the light upon undo execution, otherwise, we
 //save the light radius and light ID, so that we place it during undo execution.
-void AddLightToUndoList(const INT32 iMapIndex, const INT32 iLightRadius)
+void AddLightToUndoList(INT32 const iMapIndex, INT32 const iLightRadius)
 {
-	if(	!gfUndoEnabled )
-		return;
-	//When executing an undo command (by adding a light or removing one), that command
-	//actually tries to add it to the undo list.  So we wrap the execution of the undo
-	//command by temporarily setting this flag, so it'll ignore, and not place a new undo
-	//command.  When finished, the flag is cleared, and lights are again allowed to be saved
-	//in the undo list.
-	if( gfIgnoreUndoCmdsForLights )
-		return;
+	if (!gfUndoEnabled) return;
 
-	undo_stack* const pNode = MALLOC(undo_stack);
-	if( !pNode )
-		return;
+	/* When executing an undo command (by adding a light or removing one), that
+	 * command actually tries to add it to the undo list.  So we wrap the
+	 * execution of the undo command by temporarily setting this flag, so it'll
+	 * ignore, and not place a new undo command.  When finished, the flag is
+	 * cleared, and lights are again allowed to be saved in the undo list. */
+	if (gfIgnoreUndoCmdsForLights) return;
 
-	undo_struct* const pUndoInfo = MALLOC(undo_struct);
-	if( !pUndoInfo )
-	{
-		MemFree( pNode );
-		return;
-	}
-
-	pUndoInfo->fLightSaved = TRUE;
-	//if ubLightRadius is 0, then we don't need to save the light information because we
-	//will erase it when it comes time to execute the undo command.
-	pUndoInfo->ubLightRadius = (UINT8)iLightRadius;
-	pUndoInfo->iMapIndex = iMapIndex;
-	pUndoInfo->pMapTile = NULL;
+	SGP::PODObj<undo_struct> undo_info;
+	undo_info->fLightSaved = TRUE;
+	/* if ubLightRadius is 0, then we don't need to save the light information
+	 * because we will erase it when it comes time to execute the undo command. */
+	undo_info->ubLightRadius = iLightRadius;
+	undo_info->iMapIndex     = iMapIndex;
+	undo_info->pMapTile      = NULL;
 
 	//Add to undo stack
-	pNode->iCmdCount = 1;
-	pNode->pData = pUndoInfo;
-	pNode->pNext = gpTileUndoStack;
-	gpTileUndoStack = pNode;
+	SGP::PODObj<undo_stack> n;
+	n->iCmdCount    = 1;
+	n->pData        = undo_info.Release();
+	n->pNext        = gpTileUndoStack;
+	gpTileUndoStack = n.Release();
 
-	CropStackToMaxLength( MAX_UNDO_COMMAND_LENGTH );
+	CropStackToMaxLength(MAX_UNDO_COMMAND_LENGTH);
 }
 
 
