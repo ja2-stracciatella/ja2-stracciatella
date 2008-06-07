044e0f520f68b420b981d448ee5b5bf6f7341c42
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2008-06-07 18:32:25 +0000

    Remove the unused macro constants SCR_INACTIVE, SCR_INITIALIZING, SCR_ACTIVE and SCR_SHUTTING_DOWN.
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@4348 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/Screens.h
+++ Build/Screens.h
@@ -15,15 +15,6 @@ typedef struct Screens
 	void (*ShutdownScreen)(void);
 } Screens;
 
-// These defines are used as flags for each screen state. The only legal transition of states is
-// SCR_INACTIVE -> SCR_INITIALIZING -> SCR_ACTIVE -> SCR_SHUTTING_DOWN -> SCR_INACTIVE ... Anything else
-// will cause the system to yak.
-
-#define SCR_INACTIVE         0x00
-#define SCR_INITIALIZING     0x01
-#define SCR_ACTIVE           0x02
-#define SCR_SHUTTING_DOWN    0x04
-
 // This extern is made available to make sure that external modules will have access to the screen information
 
 extern Screens const GameScreens[];
