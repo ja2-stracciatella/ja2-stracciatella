ja2-stracciatella/ja2-stracciatella

commit a1608c2f55b340915ba7033697e9ebca2d65f4b8

Add class SGP::PODObj.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@4290 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Editor/Editor_Undo.cc
@@ -1,5 +1,6 @@
 #ifdef JA2EDITOR
 
+#include "PODObj.h"
 #include "Structure.h"
 #include "TileDef.h"
 #include "WorldDef.h"
@@ -317,63 +318,31 @@ static BOOLEAN CopyMapElementFromWorld(MAP_ELEMENT* pNewMapElement, INT32 iMapIn
 
 
 static BOOLEAN AddToUndoListCmd(INT32 iMapIndex, INT32 iCmdCount)
+try
 {
 	STRUCTURE			*pStructure;
 	INT32					iCoveredMapIndex;
 	UINT8					ubLoop;
 
-	undo_stack* const pNode = MALLOC(undo_stack);
-	if (pNode == NULL) return FALSE;
-
-	undo_struct* const pUndoInfo = MALLOC(undo_struct);
-	if (pUndoInfo == NULL)
-	{
-		MemFree(pNode);
-		return FALSE;
-	}
-
-	MAP_ELEMENT* const pData = MALLOC(MAP_ELEMENT);
-	if (pData == NULL)
-	{
-		MemFree(pNode);
-		MemFree(pUndoInfo);
-		return FALSE;
-	}
-
-	// Init map element struct
-	pData->pLandHead = pData->pLandStart = NULL;
-	pData->pObjectHead = NULL;
-	pData->pStructHead = NULL;
-	pData->pShadowHead = NULL;
-	pData->pMercHead = NULL;
-	pData->pRoofHead = NULL;
-	pData->pOnRoofHead = NULL;
-	pData->pTopmostHead = NULL;
-	pData->pStructureHead = pData->pStructureTail = NULL;
-	pData->sHeight = 0;
-
+	SGP::PODObj<undo_stack>  pNode;
+	SGP::PODObj<undo_struct> pUndoInfo;
 
 	// Copy the world map's tile
-	if (!CopyMapElementFromWorld(pData, iMapIndex))
-	{
-		MemFree( pNode );
-		MemFree( pUndoInfo );
-		MemFree( pData );
-		return( FALSE );
-	}
+	SGP::PODObj<MAP_ELEMENT> pData;
+	if (!CopyMapElementFromWorld(pData, iMapIndex)) return FALSE;
 
 	// copy the room number information (it's not in the mapelement structure)
 	pUndoInfo->ubRoomNum = gubWorldRoomInfo[ iMapIndex ];
 
 	pUndoInfo->fLightSaved = FALSE;
 	pUndoInfo->ubLightRadius = 0;
-	pUndoInfo->pMapTile = pData;
+	pUndoInfo->pMapTile = pData.Release();
 	pUndoInfo->iMapIndex = iMapIndex;
 
-	pNode->pData = pUndoInfo;
+	pNode->pData = pUndoInfo.Release();
 	pNode->iCmdCount = iCmdCount;
 	pNode->pNext = gpTileUndoStack;
-	gpTileUndoStack = pNode;
+	gpTileUndoStack = pNode.Release();
 
 	// loop through pData->pStructureHead list
 	// for each structure
@@ -398,6 +367,7 @@ static BOOLEAN AddToUndoListCmd(INT32 iMapIndex, INT32 iCmdCount)
 
 	return( TRUE );
 }
+catch (...) { return FALSE; }
 
 
 void RemoveAllFromUndoList(void)

Build/TileEngine/Tile_Surface.cc
@@ -1,4 +1,5 @@
 #include "HImage.h"
+#include "PODObj.h"
 #include "Structure.h"
 #include "TileDef.h"
 #include "Tile_Surface.h"
@@ -66,7 +67,7 @@ TILE_IMAGERY* LoadTileSurface(const char* cFilename)
 			}
 		}
 
-		TILE_IMAGERY* const pTileSurf = MALLOCZ(TILE_IMAGERY);
+		SGP::PODObj<TILE_IMAGERY> pTileSurf;
 
 		if (pStructureFileRef && pStructureFileRef->pAuxData != NULL)
 		{
@@ -77,11 +78,7 @@ TILE_IMAGERY* LoadTileSurface(const char* cFilename)
 		{
 			// Valid auxiliary data, so make a copy of it for TileSurf
 			pTileSurf->pAuxData = MALLOCN(AuxObjectData, hVObject->SubregionCount());
-			if (pTileSurf->pAuxData == NULL)
-			{
-				MemFree(pTileSurf);
-				return NULL;
-			}
+			if (!pTileSurf->pAuxData) return 0;
 			memcpy( pTileSurf->pAuxData, hImage->pAppData, hImage->uiAppDataSize );
 		}
 		else
@@ -91,7 +88,7 @@ TILE_IMAGERY* LoadTileSurface(const char* cFilename)
 
 		pTileSurf->vo                = hVObject.Release();
 		pTileSurf->pStructureFileRef = pStructureFileRef.Release();
-		return pTileSurf;
+		return pTileSurf.Release();
 	}
 	catch (...)
 	{

sgp/FileMan.cc
@@ -11,6 +11,7 @@
 #include "FileMan.h"
 #include "LibraryDataBase.h"
 #include "MemMan.h"
+#include "PODObj.h"
 #include "WCheck.h"
 
 #ifdef _WIN32
@@ -289,6 +290,7 @@ BOOLEAN FileDelete(const char* const path)
 
 
 HWFILE FileOpen(const char* const filename, const FileOpenFlags flags)
+try
 {
 	const char* fmode;
 	int         mode;
@@ -302,8 +304,7 @@ HWFILE FileOpen(const char* const filename, const FileOpenFlags flags)
 		default: abort();
 	}
 
-	SGPFile* const file = MALLOCZ(SGPFile);
-	if (!file) return NULL;
+	SGP::PODObj<SGPFile> file;
 
 	int d;
 	if (flags & FILE_CREATE_ALWAYS)
@@ -325,7 +326,7 @@ HWFILE FileOpen(const char* const filename, const FileOpenFlags flags)
 			d = open(path, mode);
 			if (d < 0)
 			{
-				if (OpenFileFromLibrary(filename, &file->u.lib)) return file;
+				if (OpenFileFromLibrary(filename, &file->u.lib)) return file.Release();
 
 				if (flags & FILE_OPEN_ALWAYS)
 				{
@@ -335,21 +336,20 @@ HWFILE FileOpen(const char* const filename, const FileOpenFlags flags)
 		}
 	}
 
-	if (d >= 0)
+	if (d < 0) return 0;
+
+	FILE* const f = fdopen(d, fmode);
+	if (!f)
 	{
-		FILE* const f = fdopen(d, fmode);
-		if (f != NULL)
-		{
-			file->flags  = SGPFILE_REAL;
-			file->u.file = f;
-			return file;
-		}
 		close(d);
+		return 0;
 	}
 
-	MemFree(file);
-	return NULL;
+	file->flags  = SGPFILE_REAL;
+	file->u.file = f;
+	return file.Release();
 }
+catch (...) { return 0; }
 
 
 void FileClose(const HWFILE f)

sgp/HImage.cc
@@ -6,6 +6,7 @@
 #include "HImage.h"
 #include "ImpTGA.h"
 #include "PCX.h"
+#include "PODObj.h"
 #include "STCI.h"
 #include "WCheck.h"
 #include "VObject.h"
@@ -33,8 +34,7 @@ SGPImage* CreateImage(const char* const filename, const UINT16 fContents)
 	if (!dot) throw std::logic_error("Tried to load image with no extension");
 	const char* const ext = dot + 1;
 
-	SGPImage* const img = MALLOCZ(SGPImage);
-	if (!img) throw std::bad_alloc();
+	SGP::PODObj<SGPImage> img;
 	strcpy(img->ImageFile, filename);
 
 	// determine type from extension
@@ -53,17 +53,12 @@ SGPImage* CreateImage(const char* const filename, const UINT16 fContents)
 	}
 	else
 	{
-		MemFree(img);
 		throw std::logic_error("Tried to load image with unknown extension");
 	}
 
-	if (!ret)
-	{
-		MemFree(img);
-		throw std::runtime_error("Error occured while loading image");
-	}
+	if (!ret) throw std::runtime_error("Error occured while loading image");
 
-	return img;
+	return img.Release();
 }
 
 

sgp/PCX.cc
@@ -3,6 +3,7 @@
 #include "PCX.h"
 #include "MemMan.h"
 #include "FileMan.h"
+#include "PODObj.h"
 
 
 #define PCX_NORMAL         1
@@ -106,8 +107,7 @@ try
 	const UINT32 file_size   = FileGetSize(f);
 	const UINT32 buffer_size = file_size - sizeof(PcxHeader) - 768;
 
-	PcxObject* const pcx_obj = MALLOC(PcxObject);
-	if (pcx_obj == NULL) return NULL;
+	SGP::PODObj<PcxObject> pcx_obj;
 
 	pcx_obj->pPcxBuffer = MALLOCN(UINT8, buffer_size);
 	if (pcx_obj->pPcxBuffer != NULL)
@@ -120,12 +120,11 @@ try
 		if (FileRead(f, pcx_obj->pPcxBuffer, buffer_size) &&
 				FileRead(f, pcx_obj->ubPalette, sizeof(pcx_obj->ubPalette)))
 		{
-			return pcx_obj;
+			return pcx_obj.Release();
 		}
 
 		MemFree(pcx_obj->pPcxBuffer);
 	}
-	MemFree(pcx_obj);
 	return NULL;
 }
 catch (...) { return 0; }

sgp/PODObj.h
@@ -1,0 +1,41 @@
+#ifndef PODOBJ_H
+#define PODOBJ_H
+
+#include <stdexcept>
+
+#include "MemMan.h"
+
+
+namespace SGP
+{
+	template<typename T> class PODObj
+	{
+		public:
+			PODObj() : p_(MALLOC(T))
+			{
+				if (!p_) throw std::bad_alloc();
+				memset(p_, 0, sizeof(*p_));
+			}
+
+			~PODObj() { if (p_) MemFree(p_); }
+
+			T* Release()
+			{
+				T* const p = p_;
+				p_ = 0;
+				return p;
+			}
+
+			operator T*() const { return p_; }
+
+			T* operator ->() const { return p_; }
+
+		private:
+			T* p_;
+
+			PODObj(const PODObj&);          /* no copy */
+			void operator =(const PODObj&); /* no assignment */
+	};
+}
+
+#endif

commit 47fe2a52d52bc3faebb55aebb0b7d733861f89dd

Fix a crash in the editor caused by r4290.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@4358 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Editor/Editor_Undo.cc
@@ -308,7 +308,6 @@ static BOOLEAN CopyMapElementFromWorld(MAP_ELEMENT* pNewMapElement, INT32 iMapIn
 static BOOLEAN AddToUndoListCmd(INT32 iMapIndex, INT32 iCmdCount)
 try
 {
-	STRUCTURE			*pStructure;
 	INT32					iCoveredMapIndex;
 	UINT8					ubLoop;
 
@@ -318,6 +317,8 @@ try
 	// Copy the world map's tile
 	SGP::PODObj<MAP_ELEMENT> pData;
 	if (!CopyMapElementFromWorld(pData, iMapIndex)) return FALSE;
+
+	STRUCTURE* pStructure = pData->pStructureHead;
 
 	// copy the room number information (it's not in the mapelement structure)
 	pUndoInfo->ubRoomNum = gubWorldRoomInfo[ iMapIndex ];
@@ -338,7 +339,6 @@ try
 	//   reference the db structure
 	//   if number of tiles > 1
 	//     add all covered tiles to undo list
-	pStructure = pData->pStructureHead;
 	while (pStructure)
 	{
 		for (ubLoop = 1; ubLoop < pStructure->pDBStructureRef->pDBStructure->ubNumberOfTiles; ubLoop++)