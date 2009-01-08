02c202e8abb2bb88107057fc59f7c6209985fb1c
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2009-01-08 08:48:03 +0000

    Partially clean up IanDisplayWrappedString().
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@5016 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/Utils/WordWrap.cc
+++ Build/Utils/WordWrap.cc
@@ -162,462 +162,181 @@ void DrawTextToScreen(const wchar_t* pStr, UINT16 usLocX, UINT16 usLocY, UINT16 
 }
 
 
-//
+static void IanDrawTextToScreen(wchar_t const* const str, wchar_t* const end, UINT16 const x, UINT16 const y, UINT16 const w, Font const font, UINT8 const foreground, UINT8 const background, UINT32 flags, UINT32 const ian_flags)
+{
+	*end = L'\0';
+	if (ian_flags & IAN_WRAP_NO_SHADOW) SetFontShadow(NO_SHADOW);
+	flags |= ian_flags & MARK_DIRTY;
+	DrawTextToScreen(str, x, y, w, font, foreground, background, flags);
+	if (ian_flags & IAN_WRAP_NO_SHADOW) SetFontShadow(DEFAULT_SHADOW);
+}
+
+
 // Pass in, the x,y location for the start of the string,
 //					the width of the buffer (how many pixels wide for word wrapping)
 //					the gap in between the lines
-//
-
-UINT16 IanDisplayWrappedString(UINT16 usPosX, UINT16 usPosY, UINT16 usWidth, UINT8 ubGap, Font const font, UINT8 ubColor, const wchar_t* pString, UINT8 ubBackGroundColor, UINT32 uiFlags)
+UINT16 IanDisplayWrappedString(UINT16 const sx, UINT16 const sy, UINT16 const max_w, UINT8 const gap, Font const font, UINT8 const foreground, wchar_t const* const str, UINT8 const background, UINT32 const flags)
 {
-	UINT32 draw_flags = uiFlags & MARK_DIRTY;
-	UINT16	usSourceCounter=0,usDestCounter=0,usWordLengthPixels,usLineLengthPixels=0,usPhraseLengthPixels=0;
-	UINT16	usLinesUsed=1,usLocalWidth=usWidth;
-	Font    uiLocalFont = font;
-	UINT16	usJustification = LEFT_JUSTIFIED,usLocalPosX=usPosX;
-	UINT8		ubLocalColor = ubColor;
-	BOOLEAN fBoldOn=FALSE;
-
-	CHAR16	zLineString[128] = L"",zWordString[64]= L"";
-
+	wchar_t        line_buf[128];
+	wchar_t*       line_pos       = line_buf;
+	wchar_t const* i              = str;
+	UINT16         cur_max_w      = max_w;
+	UINT16         line_w         = 0;
+	UINT16         x              = sx;
+	UINT16         y              = sy;
+	Font           cur_font       = font;
+	UINT16         h              = GetFontHeight(cur_font) + gap;
+	bool           is_bold        = false;
+	UINT8          cur_foreground = foreground;
+	UINT16         justification  = LEFT_JUSTIFIED;
 	do
 	{
 		// each character goes towards building a new word
-		if (pString[usSourceCounter] != TEXT_SPACE && pString[usSourceCounter] != 0)
+		wchar_t const* word_start = i;
+		while (*i != TEXT_SPACE && *i != L'\0') ++i;
+
+		// we hit a space (or end of record), so this is the END of a word!
+		switch (word_start[0])
 		{
-			zWordString[usDestCounter++] = pString[usSourceCounter];
-		}
-		else
-		{
-			// we hit a space (or end of record), so this is the END of a word!
-
-			// is this a special CODE?
-			if (zWordString[0] >= TEXT_CODE_NEWLINE && zWordString[0] <= TEXT_CODE_DEFCOLOR)
-			{
-				switch(zWordString[0])
+			case TEXT_CODE_CENTER:
+				if (justification != CENTER_JUSTIFIED)
 				{
-					case TEXT_CODE_CENTER:
-
-						if (usJustification != CENTER_JUSTIFIED)
-						{
-							usJustification = CENTER_JUSTIFIED;
-
-							// erase this word string we've been building - it was just a code
-							memset(zWordString,0,sizeof(zWordString));
-
-							// erase the line string, we're starting from scratch
-							memset(zLineString,0,sizeof(zLineString));
-
-							// reset the line length - we're starting from scratch
-							usLineLengthPixels = 0;
-
-							// reset dest char counter
-							usDestCounter = 0;
-						}
-						else	// turn OFF centering...
-						{
-
-							// shadow control
-							if( IAN_WRAP_NO_SHADOW & uiFlags )
-							{
-		            // turn off shadow
-		            SetFontShadow( NO_SHADOW );
-							}
-
-
-							// time to draw this line of text (centered)!
-							DrawTextToScreen(zLineString, usLocalPosX, usPosY, usLocalWidth, uiLocalFont, ubLocalColor, ubBackGroundColor, usJustification | draw_flags);
-
-              // shadow control
-							if( IAN_WRAP_NO_SHADOW & uiFlags )
-							{
-		            // turn on shadow
-		            SetFontShadow( DEFAULT_SHADOW );
-							}
-
-							// increment Y position for next time
-  						usPosY += GetFontHeight(uiLocalFont) + ubGap;
-
-							// we just used a line, so note that
-							usLinesUsed++;
-
-							// reset x position
-							usLocalPosX = usPosX;
-
-							// erase line string
-							memset(zLineString,0,sizeof(zLineString));
-
-							// erase word string
-							memset(zWordString,0,sizeof(zWordString));
-
-							// reset the line length
-							usLineLengthPixels = 0;
-
-							// reset dest char counter
-							usDestCounter = 0;
-
-							// turn off centering...
-							usJustification = LEFT_JUSTIFIED;
-						}
-
-						break;
-
-
-
-					case TEXT_CODE_NEWLINE:
-
-						// NEWLINE character!
-
-						// shadow control
-						if( IAN_WRAP_NO_SHADOW & uiFlags )
-						{
-		           // turn off shadow
-		           SetFontShadow( NO_SHADOW );
-						}
-
-						// Display what we have up to now
-						DrawTextToScreen(zLineString, usLocalPosX, usPosY, usLocalWidth, uiLocalFont, ubLocalColor, ubBackGroundColor, usJustification | draw_flags);
-
-            // shadow control
-						if( IAN_WRAP_NO_SHADOW & uiFlags )
-						{
-		           // turn on shadow
-		           SetFontShadow( DEFAULT_SHADOW );
-						}
-
-
-						// increment Y position for next time
-  					usPosY += GetFontHeight(uiLocalFont) + ubGap;
-
-						// we just used a line, so note that
-						usLinesUsed++;
-
-						// reset x position
-						usLocalPosX = usPosX;
-
-						// erase line string
-						memset(zLineString,0,sizeof(zLineString));
-
-						// erase word string
-						memset(zWordString,0,sizeof(zWordString));
-
-						// reset the line length
-						usLineLengthPixels = 0;
-
-						// reset width
-						usLocalWidth = usWidth;
-
-						// reset dest char counter
-						usDestCounter = 0;
-
-						break;
-
-
-					case TEXT_CODE_BOLD:
-
-						if (!fBoldOn)
-						{
-
-							// shadow control
-						  if( IAN_WRAP_NO_SHADOW & uiFlags )
-							{
-		             // turn off shadow
-		             SetFontShadow( NO_SHADOW );
-							}
-
-							// turn bold ON.... but first, write whatever we have in normal now...
-							DrawTextToScreen(zLineString, usLocalPosX, usPosY, usLocalWidth, uiLocalFont, ubColor, ubBackGroundColor, usJustification | draw_flags);
-
-							// shadow control
-						  if( IAN_WRAP_NO_SHADOW & uiFlags )
-							{
-		             // turn on shadow
-		             SetFontShadow( DEFAULT_SHADOW );
-							}
-
-							// calc length of what we just wrote
-							usPhraseLengthPixels = StringPixLength(zLineString, uiLocalFont);
-
-							// calculate new x position for next time
-							usLocalPosX += usPhraseLengthPixels;
-
-							// shorten width for next time
-							usLocalWidth -= usLineLengthPixels;
-
-							// erase line string
-							memset(zLineString,0,sizeof(zLineString));
-
-							// erase word string
-							memset(zWordString,0,sizeof(zWordString));
-
-							// turn bold ON
-							uiLocalFont = FONT10ARIALBOLD;
-							SetFontShadow(NO_SHADOW);
-							fBoldOn     = TRUE;
-
-							// reset dest char counter
-							usDestCounter = 0;
-						}
-						else
-						{
-
-
-							// shadow control
-						  if( IAN_WRAP_NO_SHADOW & uiFlags )
-							{
-		             // turn off shadow
-		             SetFontShadow( NO_SHADOW );
-							}
-
-							// turn bold OFF - write whatever we have in bold now...
-							DrawTextToScreen(zLineString, usLocalPosX, usPosY, usLocalWidth, uiLocalFont, ubColor, ubBackGroundColor, usJustification | draw_flags);
-
-							// shadow control
-						  if( IAN_WRAP_NO_SHADOW & uiFlags )
-							{
-		             // turn on shadow
-		             SetFontShadow( DEFAULT_SHADOW );
-							}
-
-  						// calc length of what we just wrote
-							usPhraseLengthPixels = StringPixLength(zLineString, uiLocalFont);
-
-							// calculate new x position for next time
-							usLocalPosX += usPhraseLengthPixels;
-
-							// shorten width for next time
-							usLocalWidth -= usLineLengthPixels;
-
-							// erase line string
-							memset(zLineString,0,sizeof(zLineString));
-
-// new by Ian Nov 30th, 1998
-
-/*
-DEF: commented out for Beta.  Nov 30
-
-							// measure length of WordString and if length > 1, then we have a word to deal with
-							if (wcslen(zWordString) > 1)
-							{
-								// need to reduce the usSourceCounter by the true word length (not counting the code char)
-								usSourceCounter -= (wcslen(zWordString) - 1);
-							}
-
-*/
-							// erase word string
-							memset(zWordString,0,sizeof(zWordString));
-
-							// turn bold OFF
-							uiLocalFont = font;
-							fBoldOn     = FALSE;
-
-							// reset dest char counter
-							usDestCounter = 0;
-						}
-
-						break;
-
-
-
-
-					case TEXT_CODE_NEWCOLOR:
-
-
-					  // shadow control
-						if( IAN_WRAP_NO_SHADOW & uiFlags )
-						{
-		           // turn off shadow
-		           SetFontShadow( NO_SHADOW );
-						}
-
-						// change to new color.... but first, write whatever we have in normal now...
-						DrawTextToScreen(zLineString, usLocalPosX, usPosY, usLocalWidth, uiLocalFont, ubLocalColor, ubBackGroundColor, usJustification | draw_flags);
-
-						 // shadow control
-						if( IAN_WRAP_NO_SHADOW & uiFlags )
-						{
-		           // turn on shadow
-		           SetFontShadow( DEFAULT_SHADOW );
-						}
-
-
-						// the new color value is the next character in the word
-						if (zWordString[1] != TEXT_SPACE && zWordString[1] < 256)
-							ubLocalColor = (UINT8) zWordString[1];
-
-						ubLocalColor = 184;
-
-						// calc length of what we just wrote
-						usPhraseLengthPixels = StringPixLength(zLineString, uiLocalFont);
-
-						// calculate new x position for next time
-						usLocalPosX += usPhraseLengthPixels;
-
-						// shorten width for next time
-						usLocalWidth -= usLineLengthPixels;
-
-						// erase line string
-						memset(zLineString,0,sizeof(zLineString));
-
-						// erase word string
-						memset(zWordString,0,sizeof(zWordString));
-
-						// reset dest char counter
-						usDestCounter = 0;
-						break;
-
-
-
-					case TEXT_CODE_DEFCOLOR:
-
-						// shadow control
-						if( IAN_WRAP_NO_SHADOW & uiFlags )
-						{
-		           // turn off shadow
-		           SetFontShadow( NO_SHADOW );
-						}
-
-						// turn color back to default - write whatever we have in bold now...
-						DrawTextToScreen(zLineString, usLocalPosX, usPosY, usLocalWidth, uiLocalFont, ubLocalColor, ubBackGroundColor, usJustification | draw_flags);
-
-						 // shadow control
-						if( IAN_WRAP_NO_SHADOW & uiFlags )
-						{
-		           // turn on shadow
-		           SetFontShadow( DEFAULT_SHADOW );
-						}
-
-  					// calc length of what we just wrote
-						usPhraseLengthPixels = StringPixLength(zLineString, uiLocalFont);
-
-						// calculate new x position for next time
-						usLocalPosX += usPhraseLengthPixels;
-
-						// shorten width for next time
-						usLocalWidth -= usLineLengthPixels;
-
-						// erase line string
-						memset(zLineString,0,sizeof(zLineString));
-
-						// erase word string
-						memset(zWordString,0,sizeof(zWordString));
-
-						// change color back to default color
-						ubLocalColor = ubColor;
-
-						// reset dest char counter
-						usDestCounter = 0;
-						break;
-
-
-				}		// end of switch of CODES
-
-			}
-			else // not a special character
-			{
-				// terminate the string TEMPORARILY
-				zWordString[usDestCounter]   = 0;
-
-				// get the length (in pixels) of this word
-				usWordLengthPixels = StringPixLength(zWordString, uiLocalFont);
-
-				// add a space (in case we add another word to it)
-				zWordString[usDestCounter++] = 32;
-
-				// RE-terminate the string
-				zWordString[usDestCounter]   = 0;
-
-				// can we fit it onto the length of our "line" ?
-				if ((usLineLengthPixels + usWordLengthPixels) < usWidth)
+					justification = CENTER_JUSTIFIED;
+				}
+				else	// turn OFF centering...
 				{
-					// yes we can fit this word.
+					// time to draw this line of text (centered)!
+					IanDrawTextToScreen(line_buf, line_pos, x, y, cur_max_w, cur_font, cur_foreground, background, justification, flags);
 
-					// get the length AGAIN (in pixels with the SPACE) for this word
-				  usWordLengthPixels = StringPixLength(zWordString, uiLocalFont);
+					x  = sx;
+					y += h;
 
-					// calc new pixel length for the line
-					usLineLengthPixels += usWordLengthPixels;
+					// turn off centering...
+					justification = LEFT_JUSTIFIED;
+				}
 
-					// reset dest char counter
-					usDestCounter = 0;
+				// reset the line
+				line_pos = line_buf;
+				line_w   = 0;
+				break;
 
-					// add the word (with the space) to the line
-					wcscat(zLineString, zWordString);
+			case TEXT_CODE_NEWLINE:
+				// Display what we have up to now
+				IanDrawTextToScreen(line_buf, line_pos, x, y, cur_max_w, cur_font, cur_foreground, background, justification, flags);
+
+				x  = sx;
+				y += h;
+
+				// reset the line
+				line_pos = line_buf;
+				line_w   = 0;
+
+				// reset width
+				cur_max_w = max_w;
+				break;
+
+			case TEXT_CODE_BOLD:
+				IanDrawTextToScreen(line_buf, line_pos, x, y, cur_max_w, cur_font, foreground, background, justification, flags);
+				// calculate new x position for next time
+				x += StringPixLength(line_buf, cur_font);
+
+				// shorten width for next time
+				cur_max_w -= line_w;
+
+				is_bold = !is_bold;
+				if (is_bold)
+				{ // turn bold ON
+					SetFontShadow(NO_SHADOW);
+					cur_font = FONT10ARIALBOLD;
 				}
 				else
+				{ // turn bold OFF
+					cur_font = font;
+				}
+				h = GetFontHeight(cur_font) + gap;
+
+				// erase line string
+				line_pos = line_buf;
+				break;
+
+			case TEXT_CODE_NEWCOLOR:
+				// change to new color.... but first, write whatever we have in normal now...
+				IanDrawTextToScreen(line_buf, line_pos, x, y, cur_max_w, cur_font, cur_foreground, background, justification, flags);
+				// calculate new x position for next time
+				x += StringPixLength(line_buf, cur_font);
+
+				// the new color value is the next character in the word
+				if (word_start[1] != TEXT_SPACE && word_start[1] < 256)
+					cur_foreground = (UINT8)word_start[1];
+
+				cur_foreground = 184;
+
+				// shorten width for next time
+				cur_max_w -= line_w;
+
+				// erase line string
+				line_pos = line_buf;
+				break;
+
+			case TEXT_CODE_DEFCOLOR:
+				// turn color back to default - write whatever we have in bold now...
+				IanDrawTextToScreen(line_buf, line_pos, x, y, cur_max_w, cur_font, cur_foreground, background, justification, flags);
+				// calculate new x position for next time
+				x += StringPixLength(line_buf, cur_font);
+
+				// shorten width for next time
+				cur_max_w -= line_w;
+
+				// erase line string
+				line_pos = line_buf;
+
+				// change color back to default color
+				cur_foreground = foreground;
+				break;
+
+			default: // not a special character
+				// get the length (in pixels) of this word
+				UINT16 word_w = 0;
+				for (wchar_t const* k = word_start; k != i; ++k)
 				{
-					// can't fit this word!
+					word_w += GetCharWidth(cur_font, *k);
+				}
 
-					// shadow control
-					if( IAN_WRAP_NO_SHADOW & uiFlags )
-					{
-		         // turn off shadow
-		         SetFontShadow( NO_SHADOW );
-					}
-
-
+				// can we fit it onto the length of our "line"?
+				if (line_w + word_w >= max_w)
+				{ // can't fit this word!
 					// Display what we have up to now
-					DrawTextToScreen(zLineString, usLocalPosX, usPosY, usLocalWidth, uiLocalFont, ubLocalColor, ubBackGroundColor, usJustification | draw_flags);
+					IanDrawTextToScreen(line_buf, line_pos, x, y, cur_max_w, cur_font, cur_foreground, background, justification, flags);
 
-           // shadow control
-					if( IAN_WRAP_NO_SHADOW & uiFlags )
-					{
-		          // turn off shadow
-		          SetFontShadow( DEFAULT_SHADOW );
-					}
-
-
-					// increment Y position for next time
-					usPosY += GetFontHeight(uiLocalFont) + ubGap;
-
-					// reset x position
-					usLocalPosX = usPosX;
-
-					// we just used a line, so note that
-					usLinesUsed++;
+					x  = sx;
+					y += h;
 
 					// start off next line string with the word we couldn't fit
-					wcscpy(zLineString,zWordString);
-
-  				// remeasure the line length
-					usLineLengthPixels = StringPixLength(zLineString, uiLocalFont);
-
-					// reset dest char counter
-					usDestCounter = 0;
+					line_pos = line_buf;
+					line_w   = 0;
 
 					// reset width
-					usLocalWidth = usWidth;
+					cur_max_w = max_w;
 				}
-			}		// end of this word was NOT a special code
 
+				// add the word (with the space) to the line
+				while (word_start != i) *line_pos++ = *word_start++;
+				*line_pos++ = L' ';
+
+				// calc new pixel length for the line
+				line_w += word_w + GetCharWidth(cur_font, L' ');
+				break;
 		}
-
-
-
-	} while (pString[usSourceCounter++] != 0);
-
-	 // shadow control
-	if( IAN_WRAP_NO_SHADOW & uiFlags )
-	{
-		 // turn off shadow
-		 SetFontShadow( NO_SHADOW );
 	}
+	while (*i++ != '\0');
 
 	// draw the paragraph
-	DrawTextToScreen(zLineString, usLocalPosX, usPosY, usLocalWidth, uiLocalFont, ubLocalColor, ubBackGroundColor, usJustification | draw_flags);
-
-
-	// shadow control
-	if( IAN_WRAP_NO_SHADOW & uiFlags )
-	{
-		 // turn on shadow
-		 SetFontShadow( DEFAULT_SHADOW );
-	}
+	IanDrawTextToScreen(line_buf, line_pos, x, y, cur_max_w, cur_font, cur_foreground, background, justification, flags);
+	y += h;
 
 	// return how many Y pixels we used
-	return usLinesUsed * (GetFontHeight(font) + ubGap);
+	return y - sy;
 }
-
 
 
 void CleanOutControlCodesFromString(const wchar_t* pSourceString, wchar_t* pDestString)
