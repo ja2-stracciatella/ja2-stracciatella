6088fef0ed037b11f324f0efb64fed78f3bd4d12
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2009-01-31 21:41:27 +0000

    Sort.
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@5193 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/Utils/Multi_Language_Graphic_Utils.cc
+++ Build/Utils/Multi_Language_Graphic_Utils.cc
@@ -17,17 +17,21 @@ char const* GetMLGFilename(MultiLanguageGraphic const id)
 		case MLG_FUNERALAD9:         return "LAPTOP/FuneralAd_9.sti";
 		case MLG_GOLDPIECEBUTTONS:   return "INTERFACE/goldpiecebuttons.sti";
 		case MLG_HISTORY:            return "LAPTOP/history.sti";
+		case MLG_IMPSYMBOL:          return "LAPTOP/IMPSymbol.sti";
 		case MLG_INSURANCEAD10:      return "LAPTOP/insurancead_10.sti";
 		case MLG_INSURANCELINK:      return "LAPTOP/insurancelink.sti";
 		case MLG_INSURANCETITLE:     return "LAPTOP/largetitle.sti";
 		case MLG_LARGEFLORISTSYMBOL: return "LAPTOP/LargeSymbol.sti";
-		case MLG_SMALLFLORISTSYMBOL: return "LAPTOP/SmallSymbol.sti";
+		case MLG_LOADSAVEHEADER:     return "INTERFACE/loadscreenaddons.sti";
 		case MLG_MCGILLICUTTYS:      return "LAPTOP/McGillicuttys.sti";
 		case MLG_MORTUARY:           return "LAPTOP/Mortuary.sti";
 		case MLG_MORTUARYLINK:       return "LAPTOP/MortuaryLink.sti";
+		case MLG_OPTIONHEADER:       return "INTERFACE/optionscreenaddons.sti";
 		case MLG_ORDERGRID:          return "LAPTOP/OrderGrid.sti";
 		case MLG_PREBATTLEPANEL:     return "INTERFACE/PreBattlePanel.sti";
+		case MLG_SMALLFLORISTSYMBOL: return "LAPTOP/SmallSymbol.sti";
 		case MLG_SMALLTITLE:         return "LAPTOP/SmallTitle.sti";
+		case MLG_SPLASH:             return "INTERFACE/splash.sti";
 		case MLG_STATSBOX:           return "LAPTOP/StatsBox.sti";
 		case MLG_STOREPLAQUE:        return "LAPTOP/BobbyStorePlaque.sti";
 #	if defined JA2DEMO
@@ -40,10 +44,6 @@ char const* GetMLGFilename(MultiLanguageGraphic const id)
 		case MLG_TOSTATS:            return "LAPTOP/ToStats.sti";
 		case MLG_WARNING:            return "LAPTOP/Warning.sti";
 		case MLG_YOURAD13:           return "LAPTOP/YourAd_13.sti";
-		case MLG_OPTIONHEADER:       return "INTERFACE/optionscreenaddons.sti";
-		case MLG_LOADSAVEHEADER:     return "INTERFACE/loadscreenaddons.sti";
-		case MLG_SPLASH:             return "INTERFACE/splash.sti";
-		case MLG_IMPSYMBOL:          return "LAPTOP/IMPSymbol.sti";
 #elif defined GERMAN
 		case MLG_AIMSYMBOL:          return "LAPTOP/AimSymbol.sti";     // Same graphic (no translation needed)
 		case MLG_BOBBYNAME:          return "LAPTOP/BobbyName.sti";     // Same graphic (no translation needed)
@@ -60,12 +60,16 @@ char const* GetMLGFilename(MultiLanguageGraphic const id)
 		case MLG_INSURANCELINK:      return "GERMAN/insurancelink_german.sti";
 		case MLG_INSURANCETITLE:     return "GERMAN/largetitle_german.sti";
 		case MLG_LARGEFLORISTSYMBOL: return "GERMAN/LargeSymbol_german.sti";
-		case MLG_SMALLFLORISTSYMBOL: return "GERMAN/SmallSymbol_german.sti";
+		case MLG_LOADSAVEHEADER:     return "GERMAN/loadscreenaddons_german.sti";
 		case MLG_MCGILLICUTTYS:      return "GERMAN/McGillicuttys_german.sti";
 		case MLG_MORTUARY:           return "GERMAN/Mortuary_german.sti";
 		case MLG_MORTUARYLINK:       return "GERMAN/MortuaryLink_german.sti";
+		case MLG_OPTIONHEADER:       return "GERMAN/optionscreenaddons_german.sti";
+		case MLG_ORDERGRID:          return "LAPTOP/OrderGrid.sti"; // Same file
 		case MLG_PREBATTLEPANEL:     return "GERMAN/PreBattlePanel_german.sti";
+		case MLG_SMALLFLORISTSYMBOL: return "GERMAN/SmallSymbol_german.sti";
 		case MLG_SMALLTITLE:         return "GERMAN/SmallTitle_german.sti";
+		case MLG_SPLASH:             return "GERMAN/splash_german.sti";
 		case MLG_STATSBOX:           return "LAPTOP/StatsBox.sti"; // Same file
 		case MLG_STOREPLAQUE:        return "GERMAN/StorePlaque_german.sti";
 		case MLG_TITLETEXT:          return "GERMAN/titletext_german.sti";
@@ -74,10 +78,6 @@ char const* GetMLGFilename(MultiLanguageGraphic const id)
 		case MLG_TOSTATS:            return "GERMAN/ToStats_german.sti";
 		case MLG_WARNING:            return "GERMAN/Warning_german.sti";
 		case MLG_YOURAD13:           return "GERMAN/YourAd_13_german.sti";
-		case MLG_OPTIONHEADER:       return "GERMAN/optionscreenaddons_german.sti";
-		case MLG_LOADSAVEHEADER:     return "GERMAN/loadscreenaddons_german.sti";
-		case MLG_ORDERGRID:          return "LAPTOP/OrderGrid.sti"; // Same file
-		case MLG_SPLASH:             return "GERMAN/splash_german.sti";
 #else
 	//The foreign language defined determines the name of the directory and filename.
 	//For example, the German version of:
@@ -114,18 +114,22 @@ char const* GetMLGFilename(MultiLanguageGraphic const id)
 		case MLG_FUNERALAD9:         return STI("FuneralAd_9");
 		case MLG_GOLDPIECEBUTTONS:   return STI("goldpiecebuttons");
 		case MLG_HISTORY:            return STI("history");
+		case MLG_IMPSYMBOL:          return STI("IMPSymbol");
 		case MLG_INSURANCEAD10:      return STI("insurancead_10");
 		case MLG_INSURANCELINK:      return STI("insurancelink");
 		case MLG_INSURANCETITLE:     return STI("largetitle");
 		case MLG_LARGEFLORISTSYMBOL: return STI("LargeSymbol");
-		case MLG_ORDERGRID:          return STI("OrderGrid");
-		case MLG_SMALLFLORISTSYMBOL: return STI("SmallSymbol");
-		case MLG_STATSBOX:           return STI("StatsBox");
+		case MLG_LOADSAVEHEADER:     return STI("loadscreenaddons");
 		case MLG_MCGILLICUTTYS:      return STI("McGillicuttys");
 		case MLG_MORTUARY:           return STI("Mortuary");
 		case MLG_MORTUARYLINK:       return STI("MortuaryLink");
+		case MLG_OPTIONHEADER:       return STI("optionscreenaddons");
+		case MLG_ORDERGRID:          return STI("OrderGrid");
 		case MLG_PREBATTLEPANEL:     return STI("PreBattlePanel");
+		case MLG_SMALLFLORISTSYMBOL: return STI("SmallSymbol");
 		case MLG_SMALLTITLE:         return STI("SmallTitle");
+		case MLG_SPLASH:             return STI("splash");
+		case MLG_STATSBOX:           return STI("StatsBox");
 #if defined RUSSIAN
 		case MLG_STOREPLAQUE:        return STI("BobbyStorePlaque");
 #else
@@ -137,10 +141,6 @@ char const* GetMLGFilename(MultiLanguageGraphic const id)
 		case MLG_TOSTATS:            return STI("ToStats");
 		case MLG_WARNING:            return STI("Warning");
 		case MLG_YOURAD13:           return STI("YourAd_13");
-		case MLG_OPTIONHEADER:       return STI("optionscreenaddons");
-		case MLG_LOADSAVEHEADER:     return STI("loadscreenaddons");
-		case MLG_SPLASH:             return STI("splash");
-		case MLG_IMPSYMBOL:          return STI("IMPSymbol");
 #endif
 		default:                     abort();
 	}
