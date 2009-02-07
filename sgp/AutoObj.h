c7d0d2b155881a754de84828554963af9cd3e815
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2009-02-07 22:15:41 +0000

    Fix nonsensical type qualifications.
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@5241 5e31c081-6ce3-0310-bb30-f584a8092234

--- sgp/AutoObj.h
+++ sgp/AutoObj.h
@@ -27,11 +27,9 @@ namespace SGP
 				p_ = p;
 			}
 
-			T*       operator ->()       { return p_; }
-			T const* operator ->() const { return p_; }
+			T* operator ->() const { return p_; }
 
-			operator T*()             { return p_; }
-			operator T const*() const { return p_; }
+			operator T*() const { return p_; }
 
 		private:
 			T* p_;
