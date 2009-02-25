d30b109a85a38af3f5c75c6188d5fae9cc498da7
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2009-02-25 07:31:07 +0000

    Clean up AddWaypointToPGroup().
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@5432 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/Strategic/Strategic_Movement.cc
+++ Build/Strategic/Strategic_Movement.cc
@@ -373,120 +373,98 @@ BOOLEAN GroupBetweenSectorsAndSectorXYIsInDifferentDirection( GROUP *pGroup, UIN
 static void InitiateGroupMovementToNextSector(GROUP* pGroup);
 
 
-//Appends a waypoint to the end of the list.  Waypoint MUST be on the
-//same horizontal or vertical level as the last waypoint added.
-BOOLEAN AddWaypointToPGroup( GROUP* pGroup, UINT8 ubSectorX, UINT8 ubSectorY ) //Same, but overloaded
+/* Appends a waypoint to the end of the list. Waypoint MUST be on the same
+ * horizontal or vertical level as the last waypoint added. */
+BOOLEAN AddWaypointToPGroup(GROUP* const g, UINT8 const x, UINT8 const y) // Same, but overloaded
 {
-	WAYPOINT *pWay;
-	UINT8 ubNumAlignedAxes = 0;
-	BOOLEAN fReversingDirection = FALSE;
+	AssertMsg(1 <= x && x <= 16, String("AddWaypointToPGroup with out of range sectorX value of %d", x));
+	AssertMsg(1 <= y && y <= 16, String("AddWaypointToPGroup with out of range sectorY value of %d", y));
 
+	if (!g) return FALSE;
 
-	AssertMsg( ubSectorX >= 1 && ubSectorX <= 16, String( "AddWaypointToPGroup with out of range sectorX value of %d", ubSectorX ) );
-	AssertMsg( ubSectorY >= 1 && ubSectorY <= 16, String( "AddWaypointToPGroup with out of range sectorY value of %d", ubSectorY ) );
-
-	if( !pGroup )
-		return FALSE;
-
-	//At this point, we have the group, and a valid coordinate.  Now we must
-	//determine that this waypoint will be aligned exclusively to either the x or y axis of
-	//the last waypoint in the list.
-	pWay = pGroup->pWaypoints;
-	if( !pWay )
+	/* At this point, we have the group, and a valid coordinate. Now we must
+	 * determine that this waypoint will be aligned exclusively to either the x or
+	 * y axis of the last waypoint in the list. */
+	UINT8     n_aligned_axes      = 0;
+	bool      reversing_direction = FALSE;
+	WAYPOINT* wp                  = g->pWaypoints;
+	if (!wp)
 	{
-		if( GroupReversingDirectionsBetweenSectors( pGroup, ubSectorX, ubSectorY, TRUE ) )
+		if (GroupReversingDirectionsBetweenSectors(g, x, y, TRUE))
 		{
-			if( pGroup->fPlayer )
-			{
-				// because we reversed, we must add the new current sector back at the head of everyone's mercpath
-				AddSectorToFrontOfMercPathForAllSoldiersInGroup( pGroup, pGroup->ubSectorX, pGroup->ubSectorY );
+			if (g->fPlayer)
+			{ /* Because we reversed, we must add the new current sector back at the
+				 * head of everyone's mercpath */
+				AddSectorToFrontOfMercPathForAllSoldiersInGroup(g, g->ubSectorX, g->ubSectorY);
 			}
 
-			//Very special case that requiring specific coding.  Check out the comments
-			//at the above function for more information.
-			fReversingDirection = TRUE;
+			/* Very special case that requiring specific coding. Check out the
+			 * comments at the above function for more information. */
+			reversing_direction = TRUE;
 			// ARM:  Kris - new rulez.  Must still fall through and add a waypoint anyway!!!
 		}
 		else
-		{ //No waypoints, so compare against the current location.
-			if( pGroup->ubSectorX == ubSectorX )
-			{
-				ubNumAlignedAxes++;
-			}
-			if( pGroup->ubSectorY == ubSectorY )
-			{
-				ubNumAlignedAxes++;
-			}
+		{ // No waypoints, so compare against the current location.
+			if (g->ubSectorX == x) ++n_aligned_axes;
+			if (g->ubSectorY == y) ++n_aligned_axes;
 		}
 	}
 	else
 	{	//we do have a waypoint list, so go to the last entry
-		while( pWay->next )
+		while (wp->next)
 		{
-			pWay = pWay->next;
+			wp = wp->next;
 		}
-		//now, we are pointing to the last waypoint in the list
-		if( pWay->x == ubSectorX )
-		{
-			ubNumAlignedAxes++;
-		}
-		if( pWay->y == ubSectorY )
-		{
-			ubNumAlignedAxes++;
-		}
+		// Now, we are pointing to the last waypoint in the list
+		if (wp->x == x) ++n_aligned_axes;
+		if (wp->y == y) ++n_aligned_axes;
 	}
 
-	if ( !fReversingDirection )
+	if (!reversing_direction)
 	{
-		if( ubNumAlignedAxes == 0 )
+		if (n_aligned_axes == 0)
 		{
-			AssertMsg( FALSE, String( "Invalid DIAGONAL waypoint being added for groupID %d. AM-0", pGroup->ubGroupID ) );
+			AssertMsg(FALSE, String("Invalid DIAGONAL waypoint being added for groupID %d. AM-0", g->ubGroupID));
 			return FALSE;
 		}
 
-		if( ubNumAlignedAxes >= 2 )
+		if (n_aligned_axes >= 2)
 		{
-			AssertMsg( FALSE, String( "Invalid IDENTICAL waypoint being added for groupID %d. AM-0", pGroup->ubGroupID ) );
+			AssertMsg(FALSE, String("Invalid IDENTICAL waypoint being added for groupID %d. AM-0", g->ubGroupID));
 			return FALSE;
 		}
 
-		// has to be different in exactly 1 axis to be a valid new waypoint
-		Assert( ubNumAlignedAxes == 1 );
+		// Has to be different in exactly 1 axis to be a valid new waypoint
+		Assert(n_aligned_axes == 1);
 	}
 
+	WAYPOINT* const new_wp = MALLOC(WAYPOINT);
+	new_wp->x    = x;
+	new_wp->x    = y;
+	new_wp->next = 0;
 
-	if( !pWay )
-	{ //We are adding the first waypoint.
-		pGroup->pWaypoints = MALLOC(WAYPOINT);
-		pWay = pGroup->pWaypoints;
+	if (wp)
+	{ // Add the waypoint to the end of the list
+		wp->next = new_wp;
 	}
 	else
-	{ //Add the waypoint to the end of the list
-		pWay->next = MALLOC(WAYPOINT);
-		pWay = pWay->next;
-	}
+	{ // We are adding the first waypoint.
+		g->pWaypoints = new_wp;
 
-	//Fill in the information for the new waypoint.
-	pWay->x = ubSectorX;
-	pWay->y = ubSectorY;
-	pWay->next = NULL;
-
-	//IMPORTANT:
-	//The first waypoint added actually initiates the group's movement to the next sector.
-	if( pWay == pGroup->pWaypoints )
-	{
-		// don't do this if we have reversed directions!!!  In that case, the required work has already been done back there
-		if ( !fReversingDirection )
-		{
-			//We need to calculate the next sector the group is moving to and post an event for it.
-			InitiateGroupMovementToNextSector( pGroup );
+		/* Important: The first waypoint added actually initiates the group's
+		 * movement to the next sector. */
+		/* Don't do this if we have reversed directions! In that case, the required
+		 * work has already been done back there */
+		if (!reversing_direction)
+		{ /* We need to calculate the next sector the group is moving to and post an
+			 * event for it. */
+			InitiateGroupMovementToNextSector(g);
 		}
 	}
 
-	if( pGroup->fPlayer )
-	{
-		//Also, nuke any previous "tactical traversal" information.
-		CFOR_ALL_PLAYERS_IN_GROUP(curr, pGroup)
+	if (g->fPlayer)
+	{ // Also, nuke any previous "tactical traversal" information.
+		CFOR_ALL_PLAYERS_IN_GROUP(curr, g)
 		{
 			curr->pSoldier->ubStrategicInsertionCode = 0;
 		}
