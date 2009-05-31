aa25a79dd13e2b49faba50fb22ccabad662fab80
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2009-05-31 09:19:41 +0000

    Clean up PasteTextureCommon().
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@5879 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/Editor/Edit_Sys.cc
+++ Build/Editor/Edit_Sys.cc
@@ -602,55 +602,43 @@ static void PasteHigherTexture(UINT32 iMapIndex, UINT32 fNewType);
 static void PasteTextureEx(GridNo, UINT16 type);
 
 
-//---------------------------------------------------------------------------------------------------------------
-//	PasteTextureCommon
-//
-//	The PasteTexture function calls this one to actually put a ground tile down. If the brush size is larger than
-//	one tile, then the above function will call this one and indicate that they should all be placed into the undo
-//	stack as the same undo command.
-//
-void PasteTextureCommon( UINT32 iMapIndex )
+/* PasteTexture() calls this one to actually put a ground tile down. If the
+ * brush size is larger than one tile, then the above function will call this
+ * one and indicate that they should all be placed into the undo stack as the
+ * same undo command. */
+void PasteTextureCommon(UINT32 const map_idx)
 {
-	 UINT8					ubLastHighLevel;
-	 //UINT16					Dummy;
+	UINT16 const paste = CurrentPaste;
+	if (paste   == NO_TILE) return;
+	if (map_idx >= 0x8000)  return;
 
-	 if ( CurrentPaste != NO_TILE && iMapIndex < 0x8000 )
-	 {
+	// Set undo, then set new
+	AddToUndoList(map_idx);
 
-		 // Set undo, then set new
-		 AddToUndoList( iMapIndex );
+	if (paste == DEEPWATERTEXTURE)
+	{ // If we are pasting deep water and we are not over water, ignore!
+		LEVELNODE const* const land = FindTypeInLandLayer(map_idx, REGWATERTEXTURE);
+		if (!land || !gTileDatabase[land->usIndex].ubFullTile) return;
+	}
 
-		 if ( CurrentPaste == DEEPWATERTEXTURE )
-		 {
-			 // IF WE ARE PASTING DEEP WATER AND WE ARE NOT OVER WATER, IGNORE!
-	 		 LEVELNODE const* const land = FindTypeInLandLayer(iMapIndex, REGWATERTEXTURE);
-	 		 if (!land || !gTileDatabase[land->usIndex].ubFullTile) return;
-		 }
+	// Don't draw over floors
+	if (TypeRangeExistsInLandLayer(map_idx, FIRSTFLOOR, FOURTHFLOOR)) return;
 
-		 // Don't draw over floors
-			if (TypeRangeExistsInLandLayer(iMapIndex, FIRSTFLOOR, FOURTHFLOOR))
-		 {
-			 return;
-		 }
-
-
-
-		 // Compare heights and do appropriate action
-		 if ( AnyHeigherLand( iMapIndex, CurrentPaste, &ubLastHighLevel ) )
-		 {
-			 // Here we do the following:
-			 // - Remove old type from layer
-			 // - Smooth World with old type
-			 // - Add a 3 by 3 square of new type at head
-			 // - Smooth World with new type
-			 PasteHigherTexture( iMapIndex, CurrentPaste );
-
-		 }
-		 else
-		 {
-				PasteTextureEx( (INT16)iMapIndex, CurrentPaste );
-				SmoothTerrainRadius( iMapIndex, CurrentPaste, 1, TRUE );
-		 }
+	// Compare heights and do appropriate action
+	UINT8 last_high_level;
+	if (AnyHeigherLand(map_idx, paste, &last_high_level))
+	{
+		/* Here we do the following:
+		 * - Remove old type from layer
+		 * - Smooth World with old type
+		 * - Add a 3 by 3 square of new type at head
+		 * - Smooth World with new type */
+		PasteHigherTexture(map_idx, paste);
+	}
+	else
+	{
+		PasteTextureEx(map_idx, paste);
+		SmoothTerrainRadius(map_idx, paste, 1, TRUE);
 	}
 }
 
