4439dff8719629f4db2d9963527aeba42f1df177
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2009-06-18 10:52:58 +0000

    Use enum RenderLayerID instead of UINT32.
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@6036 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/TileEngine/RenderWorld.cc
+++ Build/TileEngine/RenderWorld.cc
@@ -71,7 +71,7 @@ ENUM_BITSET(RenderTilesFlags)
 
 
 // RENDERER FLAGS FOR DIFFERENT RENDER LEVELS
-enum
+enum RenderLayerID
 {
 	RENDER_STATIC_LAND,
 	RENDER_STATIC_OBJECTS,
@@ -323,7 +323,7 @@ static BOOLEAN Blt8BPPDataTo16BPPBufferTransZTransShadowIncClip(UINT16* pBuffer,
 static BOOLEAN Blt8BPPDataTo16BPPBufferTransZTransShadowIncObscureClip(UINT16* pBuffer, UINT32 uiDestPitchBYTES, UINT16* pZBuffer, UINT16 usZValue, HVOBJECT hSrcVObject, INT32 iX, INT32 iY, UINT16 usIndex, SGPRect* clipregion, INT16 sZIndex, const UINT16* p16BPPPalette);
 
 
-static void RenderTiles(RenderTilesFlags const uiFlags, INT32 const iStartPointX_M, INT32 const iStartPointY_M, INT32 const iStartPointX_S, INT32 const iStartPointY_S, INT32 const iEndXS, INT32 const iEndYS, UINT8 const ubNumLevels, RenderLayerFlags const* const puiLevels, UINT16 const* const psLevelIDs)
+static void RenderTiles(RenderTilesFlags const uiFlags, INT32 const iStartPointX_M, INT32 const iStartPointY_M, INT32 const iStartPointX_S, INT32 const iStartPointY_S, INT32 const iEndXS, INT32 const iEndYS, UINT8 const ubNumLevels, RenderLayerFlags const* const puiLevels, RenderLayerID const* const psLevelIDs)
 {
 	static UINT8        ubLevelNodeStartIndex[NUM_RENDER_FX_TYPES];
 	static RenderFXType RenderFXList[NUM_RENDER_FX_TYPES];
@@ -1718,7 +1718,7 @@ static void ResetRenderParameters(void);
 void RenderStaticWorldRect(INT16 sLeft, INT16 sTop, INT16 sRight, INT16 sBottom, BOOLEAN fDynamicsToo)
 {
 	RenderLayerFlags uiLevelFlags[10];
-	UINT16           sLevelIDs[10];
+	RenderLayerID    sLevelIDs[10];
 
 	// Calculate render starting parameters
 	CalcRenderParameters(sLeft, sTop, sRight, sBottom);
@@ -1800,7 +1800,7 @@ void RenderStaticWorldRect(INT16 sLeft, INT16 sTop, INT16 sRight, INT16 sBottom,
 static void RenderStaticWorld(void)
 {
 	RenderLayerFlags uiLevelFlags[9];
-	UINT16           sLevelIDs[9];
+	RenderLayerID    sLevelIDs[9];
 
 	// Calculate render starting parameters
 	CalcRenderParameters(gsVIEWPORT_START_X, gsVIEWPORT_START_Y, gsVIEWPORT_END_X, gsVIEWPORT_END_Y);
@@ -1854,7 +1854,7 @@ static void RenderStaticWorld(void)
 static void RenderMarkedWorld(void)
 {
 	RenderLayerFlags uiLevelFlags[4];
-	UINT16           sLevelIDs[4];
+	RenderLayerID    sLevelIDs[4];
 
 	CalcRenderParameters(gsVIEWPORT_START_X, gsVIEWPORT_START_Y, gsVIEWPORT_END_X, gsVIEWPORT_END_Y);
 
@@ -1902,7 +1902,7 @@ static void RenderMarkedWorld(void)
 static void RenderDynamicWorld(void)
 {
 	RenderLayerFlags uiLevelFlags[10];
-	UINT16           sLevelIDs[10];
+	RenderLayerID    sLevelIDs[10];
 
 	CalcRenderParameters(gsVIEWPORT_START_X, gsVIEWPORT_START_Y, gsVIEWPORT_END_X, gsVIEWPORT_END_Y);
 
