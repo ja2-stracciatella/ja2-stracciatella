aa318d78017833b9b652b437a57c4be71ac50882
tron <tron@5e31c081-6ce3-0310-bb30-f584a8092234>
2009-07-04 17:18:10 +0000

    Clean up ShowVehicles().
    git-svn-id: svn://tron.homeunix.org/ja2/trunk@6224 5e31c081-6ce3-0310-bb30-f584a8092234

--- Build/Strategic/Map_Screen_Interface_Map.cc
+++ Build/Strategic/Map_Screen_Interface_Map.cc
@@ -882,24 +882,23 @@ static INT32 ShowAssignedTeam(INT16 const x, INT16 const y, INT32 icon_pos)
 }
 
 
-static INT32 ShowVehicles(INT16 sMapX, INT16 sMapY, INT32 icon_pos)
+static INT32 ShowVehicles(INT16 const x, INT16 const y, INT32 icon_pos)
 {
 	CFOR_ALL_VEHICLES(i)
 	{
 		VEHICLETYPE const& v = *i;
 		// skip the chopper, it has its own icon and displays in airspace mode
 		if (IsHelicopter(v))                          continue;
-		if (v.sSectorX != sMapX)                      continue;
-		if (v.sSectorY != sMapY)                      continue;
+		if (v.sSectorX != x)                          continue;
+		if (v.sSectorY != y)                          continue;
 		if (v.sSectorZ != iCurrentMapSectorZ)         continue;
 		if (PlayerIDGroupInMotion(v.ubMovementGroup)) continue;
 
 		SOLDIERTYPE const& vs = GetSoldierStructureForVehicle(v);
 		if (vs.bTeam != gbPlayerNum) continue;
 
-		DrawMapBoxIcon(guiCHARICONS, SMALL_WHITE_BOX, sMapX, sMapY, icon_pos++);
+		DrawMapBoxIcon(guiCHARICONS, SMALL_WHITE_BOX, x, y, icon_pos++);
 	}
-
 	return icon_pos;
 }
 
