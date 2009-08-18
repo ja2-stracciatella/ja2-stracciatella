ja2-stracciatella/ja2-stracciatella

commit 907ce67b5f27438ca2d822390b227a788ec806d7

Simplify soldier class selection in AddEnemiesToBattle().
git-svn-id: svn://tron.homeunix.org/ja2/trunk@6133 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Strategic/Queen_Command.cc
@@ -1229,27 +1229,11 @@ static void AddEnemiesToBattle(GROUP* const g, UINT8 const strategic_insertion_c
 	ChooseMapEdgepoints(&edgepoint_info, strategic_insertion_code, n_total);
 	while (n_total != 0)
 	{
-		SoldierClass sc;
-		if (n_elites != 0 && Random(n_total) < n_elites)
-		{
-			--n_elites;
-			sc = SOLDIER_CLASS_ELITE;
-		}
-		else if (n_troops != 0 && Random(n_total) < n_elites + n_troops)
-		{
-			--n_troops;
-			sc = SOLDIER_CLASS_ARMY;
-		}
-		else if (n_admins != 0 && Random(n_total) < n_elites + n_troops + n_admins)
-		{
-			--n_admins;
-			sc = SOLDIER_CLASS_ADMINISTRATOR;
-		}
-		else
-		{
-			continue;
-		}
-		--n_total;
+		UINT32       const roll = Random(n_total--);
+		SoldierClass const sc   =
+			roll < n_elites           ? --n_elites, SOLDIER_CLASS_ELITE :
+			roll < n_elites + n_total ? --n_total,  SOLDIER_CLASS_ARMY  :
+			(--n_admins, SOLDIER_CLASS_ADMINISTRATOR);
 
 		SOLDIERTYPE* const s = TacticalCreateEnemySoldier(sc);
 		if (g) s->ubGroupID = g->ubGroupID;

commit 36dacb007ff87dd1b483c950d715f67bc1f9baf4

Correct a regression introduced in r6133, which probably caused the phantom enemy group problem.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@6644 5e31c081-6ce3-0310-bb30-f584a8092234

Build/Strategic/Queen_Command.cc
@@ -1148,8 +1148,8 @@ static void AddEnemiesToBattle(GROUP const& g, UINT8 const strategic_insertion_c
 	{
 		UINT32       const roll = Random(n_total--);
 		SoldierClass const sc   =
-			roll < n_elites           ? --n_elites, SOLDIER_CLASS_ELITE :
-			roll < n_elites + n_total ? --n_total,  SOLDIER_CLASS_ARMY  :
+			roll < n_elites            ? --n_elites, SOLDIER_CLASS_ELITE :
+			roll < n_elites + n_troops ? --n_troops, SOLDIER_CLASS_ARMY  :
 			(--n_admins, SOLDIER_CLASS_ADMINISTRATOR);
 
 		SOLDIERTYPE& s = *TacticalCreateEnemySoldier(sc);
@@ -1167,6 +1167,9 @@ static void AddEnemiesToBattle(GROUP const& g, UINT8 const strategic_insertion_c
 		}
 		UpdateMercInSector(s, gWorldSectorX, gWorldSectorY, 0);
 	}
+	Assert(n_admins == 0);
+	Assert(n_troops == 0);
+	Assert(n_elites == 0);
 }
 
 