ja2-stracciatella/ja2-stracciatella

commit 476408ec735a360d800498b363e23cd7a51de87b

Consistently set the world sector coordinates to the same values when making them invalid.
git-svn-id: svn://tron.homeunix.org/ja2/trunk@7059 5e31c081-6ce3-0310-bb30-f584a8092234

Build/SaveLoadGame.cc
@@ -700,7 +700,7 @@ void LoadSavedGame(UINT8 const save_slot_id)
 		INT8  const bLoadSectorZ = gbWorldSectorZ;
 
 		// This will guarantee that the sector will be loaded
-		gbWorldSectorZ = -1;
+		SetWorldSectorInvalid();
 
 		//if we should load a sector (if the person didnt just start the game game)
 		if (gWorldSectorX != 0 && gWorldSectorY != 0)
@@ -711,8 +711,7 @@ void LoadSavedGame(UINT8 const save_slot_id)
 	}
 	else
 	{ //By clearing these values, we can avoid "in sector" checks -- at least, that's the theory.
-		gWorldSectorX = 0;
-		gWorldSectorY = 0;
+		SetWorldSectorInvalid();
 
 		INT16 const x = SaveGameHeader.sSectorX;
 		INT16 const y = SaveGameHeader.sSectorY;

Build/Editor/LoadScreen.cc
@@ -820,9 +820,7 @@ static void SetGlobalSectorValues()
 		return;
 	}
 invalid:
-	gWorldSectorX  = -1;
-	gWorldSectorY  = -1;
-	gbWorldSectorZ =  0;
+	SetWorldSectorInvalid();
 }
 
 

Build/Init.cc
@@ -128,8 +128,7 @@ editor:
 			//For editor purposes, need to know the default map file.
 			strcpy(g_filename, "none");
 			//also set the sector
-			gWorldSectorX            = 0;
-			gWorldSectorY            = 0;
+			SetWorldSectorInvalid();
 			gfIntendOnEnteringEditor = TRUE;
 			gGameOptions.fGunNut     = TRUE;
 			return GAME_SCREEN;

Build/Strategic/Game_Init.cc
@@ -590,8 +590,7 @@ void ReStartingGame()
 	gfGamePaused = TRUE;
 
 	//Reset the sectors
-	gWorldSectorX = gWorldSectorY = 0;
-	gbWorldSectorZ = -1;
+	SetWorldSectorInvalid();
 
 	SoundStopAll( );
 

Build/Strategic/Meanwhile.cc
@@ -631,9 +631,7 @@ static void DoneFadeOutMeanwhileOnceDone(void)
 	{
 		TrashWorld( );
 		// NB no world is loaded!
-		gWorldSectorX = 0;
-		gWorldSectorY = 0;
-		gbWorldSectorZ = -1;
+		SetWorldSectorInvalid();
 	}
 
 	ChangeSelectedMapSector( gsOldSelectedSectorX, gsOldSelectedSectorY, (INT8) gsOldSelectedSectorZ );

Build/Strategic/StrategicMap.cc
@@ -3824,8 +3824,7 @@ try
 
 
 	//Clear the world sector values.
-	gWorldSectorX = gWorldSectorY = 0;
-	gbWorldSectorZ = -1;
+	SetWorldSectorInvalid();
 
 	//Clear the flags regarding.
 	gfCaves = FALSE;

Build/Strategic/StrategicMap.h
@@ -28,6 +28,13 @@ extern INT16 gWorldSectorX;
 extern INT16 gWorldSectorY;
 extern INT8  gbWorldSectorZ;
 
+static inline void SetWorldSectorInvalid()
+{
+	gWorldSectorX  =  0;
+	gWorldSectorY  =  0;
+	gbWorldSectorZ = -1;
+}
+
 #define NUMBER_OF_SAMS 4
 
 

commit 0a7fc65c58b6cd46d4f54ce1b2ced30c953425b1

Correct a regression in r7059: The world coordinates got cleared, but were still required just afterwards.  Correctly use the saved coordiantes.  This caused enemies to vanish when loading savegames.
Reported by: "BirdFlu"
Patch by: Michael "mgl" Lefebvre

git-svn-id: svn://tron.homeunix.org/ja2/trunk@7060 5e31c081-6ce3-0310-bb30-f584a8092234

Build/SaveLoadGame.cc
@@ -703,7 +703,7 @@ void LoadSavedGame(UINT8 const save_slot_id)
 		SetWorldSectorInvalid();
 
 		//if we should load a sector (if the person didnt just start the game game)
-		if (gWorldSectorX != 0 && gWorldSectorY != 0)
+		if (sLoadSectorX != 0 && sLoadSectorY != 0)
 		{
 			//Load the sector
 			SetCurrentWorldSector(sLoadSectorX, sLoadSectorY, bLoadSectorZ);