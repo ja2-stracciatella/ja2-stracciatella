91daa70e689a96a6504c9f67411f2edd50c7383e
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2013-03-22 02:25:53 +0400

    On Windows source file should have BOM (byte-order mark)

--- sgp/UTF8String_unittest.cc
+++ sgp/UTF8String_unittest.cc
@@ -1,4 +1,4 @@
-#include "gtest/gtest.h"
+﻿#include "gtest/gtest.h"
 
 #include "UTF8String.h"
 
--- sgp/wchar_unittest.cc
+++ sgp/wchar_unittest.cc
@@ -1,7 +1,6 @@
-#include "gtest/gtest.h"
+﻿#include "gtest/gtest.h"
 
 #define ARR_SIZE(x) (sizeof(x)/sizeof(x[0]))
-
 
 TEST(wcharTest, English)
 {
@@ -25,18 +24,44 @@ TEST(wcharTest, RussianWithUnicodeConsts)
   EXPECT_EQ(str[4], 0x00);
 }
 
-
 TEST(wcharTest, Russian)
 {
-  // XXX
-  // This test fails on Visual Studio 10 Express when
-  // the source file has utf-8 encoding.
+  // This test will fail on Visual Studio 10 Express, if the file doesn't
+  // have byte-order mark.
 
   const wchar_t str[] = L"тест";
-  EXPECT_EQ(ARR_SIZE(str), 5);
-  // EXPECT_EQ(str[0], 0x0442);
-  // EXPECT_EQ(str[1], 0x0435);
-  // EXPECT_EQ(str[2], 0x0441);
-  // EXPECT_EQ(str[3], 0x0442);
-  // EXPECT_EQ(str[4], 0x00);
+  ASSERT_EQ(ARR_SIZE(str), 5) << "Russian string check fails.  If Visual Studio, make sure the source file has the byte-order mark";
+  EXPECT_EQ(str[0], 0x0442);
+  EXPECT_EQ(str[1], 0x0435);
+  EXPECT_EQ(str[2], 0x0441);
+  EXPECT_EQ(str[3], 0x0442);
+  EXPECT_EQ(str[4], 0x00);
+}
+
+TEST(wcharTest, Chinese)
+{
+  // This test will fail on Visual Studio 10 Express, if the file doesn't
+  // have byte-order mark.
+
+  const wchar_t str[] = L"测试";
+  ASSERT_EQ(ARR_SIZE(str), 3);
+  EXPECT_EQ(str[0], 0x6d4b);
+  EXPECT_EQ(str[1], 0x8bd5);
+  EXPECT_EQ(str[2], 0x00);
+}
+
+TEST(wcharTest, Greek)
+{
+  // This test will fail on Visual Studio 10 Express, if the file doesn't
+  // have byte-order mark.
+
+  const wchar_t str[] = L"δοκιμή";
+  ASSERT_EQ(ARR_SIZE(str), 7);
+  EXPECT_EQ(str[0], 0x03b4);
+  EXPECT_EQ(str[1], 0x03bf);
+  EXPECT_EQ(str[2], 0x03ba);
+  EXPECT_EQ(str[3], 0x03b9);
+  EXPECT_EQ(str[4], 0x03bc);
+  EXPECT_EQ(str[5], 0x03ae);
+  EXPECT_EQ(str[6], 0x00);
 }
