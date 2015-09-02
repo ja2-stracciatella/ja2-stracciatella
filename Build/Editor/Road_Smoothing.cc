f997e4e1ea7c0e57ad70b626b06b8d187431dd30
Czcibor <foo@foo.com>
2015-09-02 13:55:18 +0200

    Cleaner size calculation

--- Build/Editor/Road_Smoothing.cc
+++ Build/Editor/Road_Smoothing.cc
@@ -373,7 +373,7 @@ void InitializeRoadMacros()
 {
 	INT16 i, end;
 	INT16 sMacro = 0;
-	end = sizeof( gRoadMacros ) / 4;
+	end = sizeof( gRoadMacros ) / sizeof( MACROSTRUCT );
 	for( i = 0; i < end; i++ )
 	{
 		if( gRoadMacros[ i ].sMacroID == sMacro )
