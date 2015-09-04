c2bb742c3702c9a640543197a6dea02a0fc8edd9
Czcibor <foo@foo.com>
2015-09-04 10:36:50 +0200

    Fixed incorrect check in HandleCompatibleAmmoUIForMapScreen()
    Conflicts:
    	Build/Tactical/Interface_Items.cc

--- Build/Tactical/Interface_Items.cc
+++ Build/Tactical/Interface_Items.cc
@@ -1056,7 +1056,7 @@ BOOLEAN HandleCompatibleAmmoUIForMapScreen(const SOLDIERTYPE* pSoldier, INT32 bI
 		return( fFound );
 	}
 
-	if ( (! GCM->getItem(pTestObject->usItem)->getFlags() & ITEM_HIDDEN_ADDON) )
+	if ((!(GCM->getItem(pTestObject->usItem)->getFlags() & ITEM_HIDDEN_ADDON)))
 	{
 		// First test attachments, which almost any type of item can have....
 		for ( cnt = 0; cnt < NUM_INV_SLOTS; cnt++ )
