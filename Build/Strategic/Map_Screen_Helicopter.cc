a1f43245441073bb191a96e095bdf67df32805ce
Stephan Bauroth <der_steffi@gmx.de>
2016-03-07 14:37:19 +0100

    move some assignment out of the loop in FindClosestRefuelSite

--- Build/Strategic/Map_Screen_Helicopter.cc
+++ Build/Strategic/Map_Screen_Helicopter.cc
@@ -313,6 +313,9 @@ static RefuelSite const* FindClosestRefuelSite(bool const must_be_available)
 	INT32             shortest_distance = 9999;
 	RefuelSite const* closest_site      = 0;
 
+	VEHICLETYPE const& v        				= GetHelicopter();
+	INT16 sectorID											= CALCULATE_STRATEGIC_INDEX(v.sSectorX , v.sSectorY);
+	GROUP& g														= *GetGroup(v.ubMovementGroup);
 	// find shortest distance to refuel site
 	for (INT32 i = 0; i < NUMBER_OF_REFUEL_SITES; ++i)
 	{
@@ -320,10 +323,9 @@ static RefuelSite const* FindClosestRefuelSite(bool const must_be_available)
 		if (!fRefuelingSiteAvailable[i] && must_be_available) continue;
 
 		// find if sector is under control, find distance from heli to it
-		VEHICLETYPE const& v        = GetHelicopter();
 		RefuelSite  const& r        = g_refuel_site[i];
 		INT16       const  dest     = r.sector;
-		INT32       const  distance = FindStratPath(CALCULATE_STRATEGIC_INDEX(v.sSectorX , v.sSectorY), dest, *GetGroup(v.ubMovementGroup), FALSE);
+		INT32       const  distance = FindStratPath( sectorID, dest, g, FALSE);
 		if (distance >= shortest_distance) continue;
 
 		// shorter, copy over
