ac6185fdab312e9483d1edcaf3e463ce6c690329
Stephan Bauroth <der_steffi@gmx.de>
2016-03-08 10:37:59 +0100

    cleanup and formatting fixes in MenMan.h

--- sgp/MemMan.h
+++ sgp/MemMan.h
@@ -8,33 +8,32 @@
 
 #include "Types.h"
 
-
 void InitializeMemoryManager(void);
 void ShutdownMemoryManager(void);
 
 // Creates and adds a video object to list
 #if defined EXTREME_MEMORY_DEBUGGING
-/* This is the most effective way to debug memory leaks.  Each memory leak
- * will be recorded in a linked list containing a string referring to the
- * location in code the memory was allocated in addition to the number of
- * occurrences.  The shutdown code will report all unhandled memory with
- * exact location allocated. */
-void DumpMemoryInfoIntoFile(const char* filename, BOOLEAN fAppend);
-#	define MemAlloc(size)        MemAllocXDebug((size), __FILE__, __LINE__)
-#	define MemFree(ptr)          MemFreeXDebug((ptr), __FILE__, __LINE__)
-#	define MemRealloc(ptr, size) MemReallocXDebug((ptr), (size), __FILE__, __LINE__)
-PTR  MemAllocXDebug(size_t size, const char* szCodeString, INT32 iLineNum);
-void MemFreeXDebug(PTR ptr, const char* szCodeString, INT32 iLineNum);
-PTR  MemReallocXDebug(PTR ptr, size_t size, const char* szCodeString, INT32 iLineNum);
+	/* This is the most effective way to debug memory leaks.  Each memory leak
+	 * will be recorded in a linked list containing a string referring to the
+	 * location in code the memory was allocated in addition to the number of
+	 * occurrences.  The shutdown code will report all unhandled memory with
+	 * exact location allocated. */
+	void DumpMemoryInfoIntoFile(const char* filename, BOOLEAN fAppend);
+	void MemFreeXDebug(PTR ptr, const char* szCodeString, INT32 iLineNum);
+	PTR  MemAllocXDebug(size_t size, const char* szCodeString, INT32 iLineNum);
+	PTR  MemReallocXDebug(PTR ptr, size_t size, const char* szCodeString, INT32 iLineNum);
+	#define MemAlloc(size)        MemAllocXDebug((size), __FILE__, __LINE__)
+	#define MemFree(ptr)          MemFreeXDebug((ptr), __FILE__, __LINE__)
+	#define MemRealloc(ptr, size) MemReallocXDebug((ptr), (size), __FILE__, __LINE__)
 #else
-// Release build version
-#		include <stdlib.h>
-#		define MemAlloc(size)        XMalloc((size))
-#		define MemFree(ptr)          free((ptr))
-#		define MemRealloc(ptr, size) XRealloc((ptr), (size))
-void* XMalloc(size_t size);
-void* XRealloc(void* ptr, size_t size);
-#	endif
+	// Release build version
+	#include <stdlib.h>
+	#define MemAlloc(size)        XMalloc((size))
+	#define MemFree(ptr)          free((ptr))
+	#define MemRealloc(ptr, size) XRealloc((ptr), (size))
+	void* XMalloc(size_t size);
+	void* XRealloc(void* ptr, size_t size);
+#endif
 
 static inline void* MallocZ(const size_t n)
 {
@@ -56,7 +55,6 @@ template<typename T> static inline void FreeNull(T*& r) throw()
 #define MALLOCN(type, count)     (type*)MemAlloc(sizeof(type) * (count))
 #define MALLOCNZ(type, count)    (type*)MallocZ(sizeof(type) * (count))
 #define MALLOCZ(type)            (type*)MallocZ(sizeof(type))
-
 #define REALLOC(ptr, type, count) (type*)MemRealloc(ptr, sizeof(type) * (count))
 
-#endif
+#endif /* _MEMMAN_H */
