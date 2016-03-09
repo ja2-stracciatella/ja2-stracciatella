a703e24e1a408fa2edd30f065c937816ebf47168
Stephan Bauroth <der_steffi@gmx.de>
2016-03-09 12:28:38 +0100

    remove the unused AiDebugMsg macro

--- Build/Utils/Debug_Control.cc
+++ Build/Utils/Debug_Control.cc
@@ -1,22 +1,6 @@
 #include "Types.h"
 #include "Debug_Control.h"
 
-#ifdef _AISUBSYSTEM_DEBUG
-
-void AiDbgMessage( CHAR8 *strMessage)
-{
-  FILE      *OutFile;
-
-	if ((OutFile = fopen("AiDebug.txt", "a+")) != NULL)
-	{
-	  fprintf(OutFile, "%s\n", strMessage);
-		fclose(OutFile);
-	}
-}
-
-#endif
-
-
 void LiveMessage(const char *strMessage)
 {
   FILE      *OutFile;
--- Build/Utils/Debug_Control.h
+++ Build/Utils/Debug_Control.h
@@ -3,27 +3,4 @@
 
 #include "Types.h"
 
-
-//#define		_PHYSICSSUBSYSTEM_DEBUG
-//#define		_AISUBSYSTEM_DEBUG
-
-#ifdef JA2BETAVERSION
-//  #define			_ANIMSUBSYSTEM_DEBUG
-#endif
-
-
 void LiveMessage(const char *strMessage);
-
-#ifdef _AISUBSYSTEM_DEBUG
-
-#define AiDebugMsg(c)				AiDbgMessage( (c) )
-
-extern	void		AiDbgMessage( CHAR8 *Str);
-
-#else
-
-#define AiDebugMsg(c) (void)0
-
-#endif
-
-#endif
