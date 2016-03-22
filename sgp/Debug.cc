ce954605cfe0544e322e88c8361ed201db9c3b09
Stephan Bauroth <der_steffi@gmx.de>
2016-03-22 11:27:29 +0100

    use SLOG in Assert and AssertMsg, remove all the now unused functions in sgp/Debug

--- sgp/Debug.cc
+++ sgp/Debug.cc
@@ -1,80 +1,8 @@
 #include "Debug.h"
 
-#if defined ( SGP_DEBUG ) || defined ( FORCE_ASSERTS_ON )
-
 #include <SDL.h>
 #include <stdarg.h>
 #include "Timer.h"
-
-
-static BOOLEAN gfRecordToFile     = FALSE;
-static BOOLEAN gfRecordToDebugger = TRUE;
-
-static STRING512 gpcDebugLogFileName;
-
-
-#ifndef _NO_DEBUG_TXT
-static BOOLEAN DbgGetLogFileName(STRING512 pcName)
-{
-	strcpy(pcName, "debug.txt");
-	return TRUE;
-}
-#endif
-
-
-BOOLEAN InitializeDebugManager(void)
-{
-	gfRecordToFile = TRUE;
-	gfRecordToDebugger = TRUE;
-
-#ifndef _NO_DEBUG_TXT
-	if (!DbgGetLogFileName(gpcDebugLogFileName)) return FALSE;
-	remove(gpcDebugLogFileName);
-#endif
-
-	return(TRUE);
-}
-
-static void _DebugRecordToFile(BOOLEAN gfState)
-{
-	gfRecordToFile = gfState;
-}
-
-
-static void _DebugRecordToDebugger(BOOLEAN gfState)
-{
-	gfRecordToDebugger = gfState;
-}
-
-void _FailMessage(const char *pString, UINT32 uiLineNum, const char *pSourceFile)
-{
-	char ubOutputString[512];
-	if (pString != NULL)
-		sprintf(ubOutputString, "{ %ld } Assertion Failure [Line %d in %s]: %s\n", GetClock(), uiLineNum, pSourceFile, pString);
-	else
-		sprintf(ubOutputString, "{ %ld } Assertion Failure [Line %d in %s]\n", GetClock(), uiLineNum, pSourceFile);
-
-	//Output to debugger
-	if (gfRecordToDebugger)
-		fputs(ubOutputString, stderr);
-
-	//Record to file if required
-#ifndef _NO_DEBUG_TXT
-	if (gfRecordToFile)
-	{
-		FILE* DebugFile = fopen(gpcDebugLogFileName, "a+");
-		if (DebugFile != NULL)
-		{
-			fputs(ubOutputString, DebugFile);
-			fclose(DebugFile);
-		}
-	}
-#endif
-
-	SDL_Quit();
-	abort();
-}
-
 
 const char* String(const char* const fmt, ...)
 {
@@ -93,5 +21,3 @@ const char* String(const char* const fmt, ...)
 
 	return ResultString;
 }
-
-#endif
