68dc5c90098c48f18afd3468709da5dbb41485d3
Stephan Bauroth <der_steffi@gmx.de>
2016-03-22 13:49:27 +0100

    resolve TODO0011
    The proposed solution is the only one that makes sense. The only use of this in Soldier_Control.cc shows a lot of similarity to gubAnimSwatSpeeds which looks quite the same

--- Build/Tactical/Animation_Control.cc
+++ Build/Tactical/Animation_Control.cc
@@ -429,10 +429,8 @@ const ANI_SPEED_DEF gubAnimSwatSpeeds[TOTALBODYTYPES] =
   { -10, 2.2f }  // REGFEMALE
 };
 
-const ANI_SPEED_DEF gubAnimCrawlSpeeds[TOTALBODYTYPES] = { 0 }; // XXX TODO0011 only declared
-
 // Really only the first mercs are using any of these values....
-static const ANI_SPEED_DEF gubAnimCrawlingSpeeds[TOTALBODYTYPES] = // XXX TODO0011 unused, should probably be gubAnimCrawlSpeeds[]
+const ANI_SPEED_DEF gubAnimCrawlSpeeds[TOTALBODYTYPES] =
 {
 	{   0, 0.8f }, // REGMALE
 	{  20, 0.8f }, // BIGMALE
