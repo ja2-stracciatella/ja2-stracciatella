9f10c4b7157f697e034a21920c17b16f4c66332b
Stephan Bauroth <der_steffi@gmx.de>
2016-03-31 12:56:03 +0200

    reduce the number of bytes written for each soldier into savegames by the amount we saved in pathing date. Also correct the loading if new version, use old length for old savegames

--- Build/SaveLoadGame.cc
+++ Build/SaveLoadGame.cc
@@ -1331,7 +1331,7 @@ static void SaveSoldierStructure(HWFILE const f)
 		if (!s.bActive) continue;
 
 		// Save the soldier structure
-		BYTE data[2328];
+		BYTE data[2296];
 		InjectSoldierType(data, &s);
 		writer(f, data, sizeof(data));
 
@@ -1376,9 +1376,18 @@ static void LoadSoldierStructure(HWFILE const f, UINT32 savegame_version, bool s
     }
     else
     {
-      BYTE Data[2328];
-      reader(f, Data, sizeof(Data));
-      ExtractSoldierType(Data, &SavedSoldierInfo, stracLinuxFormat, savegame_version);
+			if(savegame_version < 100)
+			{
+				BYTE Data[2328];
+				reader(f, Data, sizeof(Data));
+				ExtractSoldierType(Data, &SavedSoldierInfo, stracLinuxFormat, savegame_version);
+			}
+			else
+			{
+				BYTE Data[2296];
+				reader(f, Data, sizeof(Data));
+				ExtractSoldierType(Data, &SavedSoldierInfo, stracLinuxFormat, savegame_version);
+			}
     }
 
 		SOLDIERTYPE* const s = TacticalCreateSoldierFromExisting(&SavedSoldierInfo);
@@ -1419,18 +1428,18 @@ static void LoadSoldierStructure(HWFILE const f, UINT32 savegame_version, bool s
 			}
 		}
 
-  if(isGermanVersion())
-  {
-		// Fix neutral flags
-		if (savegame_version < 94 &&
-				s->bTeam == OUR_TEAM  &&
-				s->bNeutral           &&
-				s->bAssignment != ASSIGNMENT_POW)
+		if(isGermanVersion())
 		{
-			// turn off neutral flag
-			s->bNeutral = FALSE;
+			// Fix neutral flags
+			if (savegame_version < 94 &&
+					s->bTeam == OUR_TEAM  &&
+					s->bNeutral           &&
+					s->bAssignment != ASSIGNMENT_POW)
+			{
+				// turn off neutral flag
+				s->bNeutral = FALSE;
+			}
 		}
-  }
 		// JA2Gold: fix next-to-previous attacker value
 		if (savegame_version < 99)
 		{
