4b52491315c9348cc1dfd702905684888377111e
Stefan Lau <github@stefanlau.com>
2016-06-05 21:04:58 +0200

    Fix mingw build.

--- _build/buildboxes/common/ubuntu-bootstrap.sh
+++ _build/buildboxes/common/ubuntu-bootstrap.sh
@@ -3,11 +3,4 @@
 apt-get update
 
 # Build dependencies
-apt-get install -y cmake make gcc g++
-apt-get install -y libsdl1.2-dev libboost-all-dev
-
-# Deb Package
-apt-get install -y git pbuilder debhelper
-
-# Windows Cross-Compilation
-apt-get install -y gcc-mingw-w64 g++-mingw-w64 zip
+apt-get install -y cmake make gcc g++ gcc-mingw-w64 g++-mingw-w64 libsdl1.2-dev libboost-all-dev nsis
