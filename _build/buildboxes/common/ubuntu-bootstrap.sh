1bd037918acf2d4189798f8533b5bf4bf9574a45
Stefan Lau <github@stefanlau.com>
2016-07-24 22:48:21 +0200

    Dont use extra box name

--- _build/buildboxes/common/ubuntu-bootstrap.sh
+++ _build/buildboxes/common/ubuntu-bootstrap.sh
@@ -3,4 +3,4 @@
 apt-get update
 
 # Build dependencies
-apt-get install -y cmake make gcc g++ gcc-mingw-w64 g++-mingw-w64 libsdl1.2-dev libboost-all-dev nsis
+apt-get install -y cmake make gcc g++ gcc-mingw-w64 g++-mingw-w64 libsdl1.2-dev nsis
