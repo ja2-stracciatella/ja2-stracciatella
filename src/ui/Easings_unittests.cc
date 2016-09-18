98b8455074f5519c0de86af438fe4ac5e1e568f5
Stefan Lau <github@stefanlau.com>
2016-09-18 12:56:57 +0200

    Add small unit test for EaseInCubic

---
+++ src/ui/Easings_unittests.cc
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+#include "Easings.h"
+
+TEST(EasingsTest, easeInCubic)
+{
+  ASSERT_EQ(EaseInCubic(100, 101, 100), 0.0);
+  ASSERT_EQ(EaseInCubic(1000, 1100, 1100), 1.0);
+
+  ASSERT_EQ(EaseInCubic(1000, 1100, 1025), 0.0625);
+  ASSERT_EQ(EaseInCubic(1000, 1100, 1075), 0.9375);
+}
