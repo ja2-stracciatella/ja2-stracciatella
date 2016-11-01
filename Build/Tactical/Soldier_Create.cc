ja2-stracciatella/ja2-stracciatella

commit 75695d0ce828db89483cb9836509f0b37294b825

externalise: override enemy elite experience level
game.json:
enemy_elite_minimum_level": 10
enemy_elite_maximum_level": 10

maximum value of 10

increase minimum level to increase challenge

lower maximum to make interrupts favour player

expert difficulty maximum is 8, only setting lower has an effect

Build/Tactical/Soldier_Create.cc
@@ -45,6 +45,7 @@
 
 #include "ContentManager.h"
 #include "GameInstance.h"
+#include "policy/GamePolicy.h"
 
 
 // THESE 3 DIFFICULTY FACTORS MUST ALWAYS ADD UP TO 100% EXACTLY!!!
@@ -1254,6 +1255,8 @@ void CreateDetailedPlacementGivenBasicPlacementInfo( SOLDIERCREATE_STRUCT *pp, B
 			break;
 		case SOLDIER_CLASS_ELITE:
 			pp->bExpLevel = (INT8) 6 + bExpLevelModifier;
+			if(pp->bExpLevel<GCM->getGamePolicy()->enemy_elite_minimum_level) pp->bExpLevel=GCM->getGamePolicy()->enemy_elite_minimum_level;
+			if(pp->bExpLevel>GCM->getGamePolicy()->enemy_elite_maximum_level) pp->bExpLevel=GCM->getGamePolicy()->enemy_elite_maximum_level;
 			break;
 		case SOLDIER_CLASS_GREEN_MILITIA:
 			pp->bExpLevel = (INT8) 2 + bExpLevelModifier;
@@ -1336,6 +1339,7 @@ void CreateDetailedPlacementGivenBasicPlacementInfo( SOLDIERCREATE_STRUCT *pp, B
 	pp->bWisdom				= (INT8)(bBaseAttribute + Random( 9 ) + Random( 8 ));
 	pp->bMorale				= (INT8)(bBaseAttribute + Random( 9 ) + Random( 8 ));
 
+	if(ubSoldierClass==SOLDIER_CLASS_ELITE && pp->bExpLevel>GCM->getGamePolicy()->enemy_elite_maximum_level)
 	// CJC: now calculate the REAL experience level if in the really upper end
 	ReduceHighExpLevels( &( pp->bExpLevel ) );
 

src/policy/DefaultGamePolicy.cc
@@ -32,6 +32,8 @@ DefaultGamePolicy::DefaultGamePolicy(rapidjson::Document *json)
   mod_ai_a                  = (*json)["mod_ai_a"].GetBool();
   threshold_cth_head        = (*json)["threshold_cth_head"].GetInt();
   threshold_cth_legs        = (*json)["threshold_cth_legs"].GetInt();
+  enemy_elite_minimum_level = (*json)["enemy_elite_minimum_level"].GetInt();
+  enemy_elite_maximum_level = (*json)["enemy_elite_maximum_level"].GetInt();
 
   pablo_wont_steal          = (*json)["pablo_wont_steal"].GetBool();
 

src/policy/GamePolicy.h
@@ -49,6 +49,8 @@ public:
   bool mod_ai_a;                        // Enable AI mod "a"
   int8_t threshold_cth_head;            // threshold AI always take head shots, increase game difficulty
   int8_t threshold_cth_legs;            // threshold AI switch to leg shots from torso
+  int8_t enemy_elite_minimum_level;     // increase challenge: minimum experience level for enemy elite soldier
+  int8_t enemy_elite_maximum_level;     // maximum experience level for enemy elite soldier
 
   bool pablo_wont_steal;                // Packages not stolen
 

commit 1ced6b8920b9f0ef5248a42bbed5c52a810112bc

fixed fallout from 75695d0ce and improved bounding (10 would degrade to 9)

Build/Tactical/Soldier_Create.cc
@@ -1255,8 +1255,6 @@ void CreateDetailedPlacementGivenBasicPlacementInfo( SOLDIERCREATE_STRUCT *pp, B
 			break;
 		case SOLDIER_CLASS_ELITE:
 			pp->bExpLevel = (INT8) 6 + bExpLevelModifier;
-			if (pp->bExpLevel < gamepolicy(enemy_elite_minimum_level)) pp->bExpLevel = gamepolicy(enemy_elite_minimum_level);
-			if (pp->bExpLevel > gamepolicy(enemy_elite_maximum_level)) pp->bExpLevel = gamepolicy(enemy_elite_maximum_level);
 			break;
 		case SOLDIER_CLASS_GREEN_MILITIA:
 			pp->bExpLevel = (INT8) 2 + bExpLevelModifier;
@@ -1309,8 +1307,15 @@ void CreateDetailedPlacementGivenBasicPlacementInfo( SOLDIERCREATE_STRUCT *pp, B
 	}
 
 
-	pp->bExpLevel = MAX( 1, pp->bExpLevel ); //minimum exp. level of 1
-	pp->bExpLevel = MIN( 9, pp->bExpLevel ); //maximum exp. level of 9
+	// clamp experience level to 1-9 or the externalised values (elites only)
+	if (ubSoldierClass == SOLDIER_CLASS_ELITE)
+	{
+		pp->bExpLevel = MAX(gamepolicy(enemy_elite_minimum_level), pp->bExpLevel);
+		pp->bExpLevel = MIN(gamepolicy(enemy_elite_maximum_level), pp->bExpLevel);
+	} else {
+		pp->bExpLevel = MAX(1, pp->bExpLevel); //minimum exp. level of 1
+		pp->bExpLevel = MIN(9, pp->bExpLevel); //maximum exp. level of 9
+	}
 
 	ubStatsLevel = pp->bExpLevel + bStatsModifier;
 	#if 0 /* unsigned < 0 ? */
@@ -1339,9 +1344,12 @@ void CreateDetailedPlacementGivenBasicPlacementInfo( SOLDIERCREATE_STRUCT *pp, B
 	pp->bWisdom				= (INT8)(bBaseAttribute + Random( 9 ) + Random( 8 ));
 	pp->bMorale				= (INT8)(bBaseAttribute + Random( 9 ) + Random( 8 ));
 
-	if (ubSoldierClass==SOLDIER_CLASS_ELITE && pp->bExpLevel>gamepolicy(enemy_elite_maximum_level))
-	// CJC: now calculate the REAL experience level if in the really upper end
-	ReduceHighExpLevels( &( pp->bExpLevel ) );
+	if (ubSoldierClass != SOLDIER_CLASS_ELITE)
+	{
+		// CJC: now calculate the REAL experience level if in the really upper end
+		// we're skipping elites, since they have externalised level bounds
+		ReduceHighExpLevels( &( pp->bExpLevel ) );
+	}
 
 	pp->fVisible			= 0;
 