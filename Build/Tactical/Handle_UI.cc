ja2-stracciatella/ja2-stracciatella

commit 989f2d9d8bf7e03569fb649de75a966ad40f65c2

simplify some checks by using a combined bitfield

Build/Tactical/Handle_UI.cc
@@ -1256,7 +1256,7 @@ static ScreenID UIHandleMOnTerrain(UI_EVENT* pUIEvent)
 				if (!UIHandleInteractiveTilesAndItemsOnTerrain(sel, usMapPos, FALSE, TRUE))
 				{
 					// Are we in combat?
-					if ( (gTacticalStatus.uiFlags & INCOMBAT ) && ( gTacticalStatus.uiFlags & TURNBASED ) )
+					if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 					{
 						// If so, draw path, etc
 						fSetCursor = HandleUIMovementCursor(sel, uiCursorFlags, usMapPos, MOVEUI_TARGET_NONE);
@@ -3024,7 +3024,7 @@ static INT8 DrawUIMovementPath(SOLDIERTYPE* const pSoldier, UINT16 usMapPos, Mov
 	INT8							bReturnCode = 0;
 	BOOLEAN						fPlot;
 
-	if ( ((gTacticalStatus.uiFlags & INCOMBAT ) && ( gTacticalStatus.uiFlags & TURNBASED )) || _KeyDown( SHIFT ) )
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) || _KeyDown( SHIFT ))
 	{
 		fPlot = PLOT;
 	}
@@ -3523,7 +3523,7 @@ static void SetUIbasedOnStance(SOLDIERTYPE* pSoldier, INT8 bNewStance)
 
 static void SetMovementModeCursor(const SOLDIERTYPE* pSoldier)
 {
-	if ( gTacticalStatus.uiFlags & TURNBASED && ( gTacticalStatus.uiFlags & INCOMBAT ) )
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 	{
 			if ( ( OK_ENTERABLE_VEHICLE( pSoldier ) ) )
 			{
@@ -3576,7 +3576,7 @@ static void SetMovementModeCursor(const SOLDIERTYPE* pSoldier)
 
 static void SetConfirmMovementModeCursor(SOLDIERTYPE* pSoldier, BOOLEAN fFromMove)
 {
-	if ( gTacticalStatus.uiFlags & TURNBASED && ( gTacticalStatus.uiFlags & INCOMBAT ) )
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 	{
 		if ( gfUIAllMoveOn )
 		{
@@ -4681,7 +4681,7 @@ BOOLEAN HandleTalkInit(  )
 
 void SetUIBusy(const SOLDIERTYPE* const s)
 {
-	if ( (gTacticalStatus.uiFlags & INCOMBAT ) && ( gTacticalStatus.uiFlags & TURNBASED ) && ( gTacticalStatus.ubCurrentTeam == OUR_TEAM ) )
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && (gTacticalStatus.ubCurrentTeam == OUR_TEAM))
 	{
 		if (s == GetSelectedMan())
 		{
@@ -4694,7 +4694,7 @@ void SetUIBusy(const SOLDIERTYPE* const s)
 
 void UnSetUIBusy(const SOLDIERTYPE* const s)
 {
-	if ( (gTacticalStatus.uiFlags & INCOMBAT ) && ( gTacticalStatus.uiFlags & TURNBASED ) && ( gTacticalStatus.ubCurrentTeam == OUR_TEAM ) )
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && (gTacticalStatus.ubCurrentTeam == OUR_TEAM ))
 	{
 		if ( !gTacticalStatus.fUnLockUIAfterHiddenInterrupt )
 		{
@@ -4774,7 +4774,7 @@ static bool UIHandleInteractiveTilesAndItemsOnTerrain(SOLDIERTYPE* const pSoldie
 
 			//Set UI CURSOR
 			guiNewUICursor =
-				fUseOKCursor || (gTacticalStatus.uiFlags & INCOMBAT && gTacticalStatus.uiFlags & TURNBASED) ?
+				fUseOKCursor || (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) ?
 					OKHANDCURSOR_UICURSOR : NORMALHANDCURSOR_UICURSOR;
 
 			HandleUIMovementCursor(pSoldier, uiCursorFlags, sActionGridNo, MOVEUI_TARGET_STEAL);
@@ -4838,7 +4838,7 @@ static bool UIHandleInteractiveTilesAndItemsOnTerrain(SOLDIERTYPE* const pSoldie
 
 			//Set UI CURSOR
 			guiNewUICursor =
-				fUseOKCursor || (gTacticalStatus.uiFlags & INCOMBAT && gTacticalStatus.uiFlags & TURNBASED) ?
+				fUseOKCursor || (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) ?
 				OKHANDCURSOR_UICURSOR : NORMALHANDCURSOR_UICURSOR;
 
 			HandleUIMovementCursor(pSoldier, uiCursorFlags, sActionGridNo, MOVEUI_TARGET_ITEMS);

Build/Tactical/Items.cc
@@ -1621,7 +1621,7 @@ BOOLEAN ReloadGun( SOLDIERTYPE * pSoldier, OBJECTTYPE * pGun, OBJECTTYPE * pAmmo
 					// Copying the old ammo to the cursor in turnbased could screw up for the player
 					// (suppose his inventory is full!)
 
-					if ( (gTacticalStatus.uiFlags & TURNBASED) && (gTacticalStatus.uiFlags & INCOMBAT) && !EnoughPoints( pSoldier, (INT8) (bAPs + AP_PICKUP_ITEM), 0, FALSE ) )
+					if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && !EnoughPoints( pSoldier, (INT8) (bAPs + AP_PICKUP_ITEM), 0, FALSE))
 					{
 						// try autoplace
 						if ( !AutoPlaceObject( pSoldier, &OldAmmo, FALSE ) )

Build/Tactical/OppList.cc
@@ -672,8 +672,7 @@ void HandleSight(SOLDIERTYPE& s, SightFlags const sight_flags)
 	}
 
 	// If we've been told that interrupts are possible as a result of sighting
-	if (gTacticalStatus.uiFlags & TURNBASED &&
-			gTacticalStatus.uiFlags & INCOMBAT  &&
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT &&
 			sight_flags & SIGHT_INTERRUPT)
 	{
 		ResolveInterruptsVs(&s, SIGHTINTERRUPT);
@@ -1987,7 +1986,7 @@ static void OtherTeamsLookForMan(SOLDIERTYPE* pOpponent)
 				// if doing regular in-combat sighting (not on opening doors!)
 				if ( gubBestToMakeSightingSize == BEST_SIGHTING_ARRAY_SIZE_INCOMBAT )
 				{
-					if ( (gTacticalStatus.uiFlags & TURNBASED) && (gTacticalStatus.uiFlags & INCOMBAT) && pSoldier->bNewOppCnt )
+					if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && pSoldier->bNewOppCnt)
 					{
 						// as long as viewer meets minimum interrupt conditions
 						if (gubSightFlags & SIGHT_INTERRUPT && StandardInterruptConditionsMet(pSoldier, pOpponent, bOldOppList))
@@ -3411,8 +3410,7 @@ void OurNoise(SOLDIERTYPE* const noise_maker, INT16 const sGridNo, INT8 const bL
 	ProcessNoise(noise_maker, sGridNo, bLevel, ubVolume, ubNoiseType);
 
 	if (noise_maker != NULL                 &&
-			gTacticalStatus.uiFlags & TURNBASED &&
-			gTacticalStatus.uiFlags & INCOMBAT  &&
+			gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT &&
 			!gfDelayResolvingBestSightingDueToDoor)
 	{
 		// interrupts are possible, resolve them now (we're in control here)
@@ -4124,7 +4122,7 @@ static void HearNoise(SOLDIERTYPE* const pSoldier, SOLDIERTYPE* const noise_make
 			}
 			else
 			{
-				if ( (gTacticalStatus.uiFlags & TURNBASED) && ( gTacticalStatus.uiFlags & INCOMBAT ) )
+				if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 				{
 					if (StandardInterruptConditionsMet(pSoldier, noise_maker, bOldOpplist))
 					{
@@ -4211,7 +4209,7 @@ static void HearNoise(SOLDIERTYPE* const pSoldier, SOLDIERTYPE* const noise_make
 		if ( gubBestToMakeSightingSize == BEST_SIGHTING_ARRAY_SIZE_INCOMBAT )
 		{
 			// if the noise heard was the fall of a rock
-			if ((gTacticalStatus.uiFlags & TURNBASED) && ( gTacticalStatus.uiFlags & INCOMBAT ) && ubNoiseType == NOISE_ROCK_IMPACT )
+			if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && ubNoiseType == NOISE_ROCK_IMPACT)
 			{
 				// give every ELIGIBLE listener an automatic interrupt, since it's
 				// reasonable to assume the guy throwing wants to wait for their reaction!

Build/Tactical/Overhead_Types.h
@@ -60,6 +60,8 @@
 #define			IGNORE_ENGAGED_IN_CONV_UI_UNLOCK		0x020000000
 #define			IGNORE_ALL_OBSTACLES								0x040000000
 #define			IN_DEIDRANNA_ENDGAME								0x080000000
+#define			IN_RT_COMBAT							INCOMBAT|REALTIME
+#define			IN_TB_COMBAT							INCOMBAT|TURNBASED
 
 #define			OKBREATH						10
 #define			OKLIFE							15

Build/Tactical/Soldier_Control.cc
@@ -2845,7 +2845,7 @@ void EVENT_SoldierGotHit(SOLDIERTYPE* pSoldier, const UINT16 usWeaponIndex, INT1
 	}
 
 	// IAN ADDED THIS SAT JUNE 14th : HAVE TO SHOW VICTIM!
-	if (gTacticalStatus.uiFlags & TURNBASED && (gTacticalStatus.uiFlags & INCOMBAT) && pSoldier->bVisible != -1 && pSoldier->bTeam == OUR_TEAM )
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT && pSoldier->bVisible != -1 && pSoldier->bTeam == OUR_TEAM)
 	{
 		LocateSoldier(pSoldier, DONTSETLOCATOR);
 	}
@@ -4872,7 +4872,7 @@ static void CalculateSoldierAniSpeed(SOLDIERTYPE* pSoldier, SOLDIERTYPE* pStatsS
 
 	// MODIFTY NOW BASED ON REAL-TIME, ETC
 	// Adjust speed, make twice as fast if in turn-based!
-	if ( gTacticalStatus.uiFlags & TURNBASED && ( gTacticalStatus.uiFlags & INCOMBAT ) )
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 	{
 		pSoldier->sAniDelay = pSoldier->sAniDelay / 2;
 	}
@@ -4898,7 +4898,7 @@ void SetSoldierAniSpeed(SOLDIERTYPE* pSoldier)
 
 	// ATE: If we are an enemy and are not visible......
 	// Set speed to 0
-	if ( ( gTacticalStatus.uiFlags & TURNBASED && ( gTacticalStatus.uiFlags & INCOMBAT ) ) || gTacticalStatus.fAutoBandageMode )
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT || gTacticalStatus.fAutoBandageMode)
 	{
 		 if ( ( ( pSoldier->bVisible == -1 && pSoldier->bVisible == pSoldier->bLastRenderVisibleValue ) || gTacticalStatus.fAutoBandageMode ) && pSoldier->usAnimState != MONSTER_UP )
 		 {
@@ -8044,7 +8044,7 @@ void SoldierCollapse( SOLDIERTYPE *pSoldier )
 			MakeClosestEnemyChosenOne();
 		}
 
-		if ( (gTacticalStatus.uiFlags & TURNBASED) && (gTacticalStatus.uiFlags & INCOMBAT) && (pSoldier->uiStatusFlags & SOLDIER_UNDERAICONTROL))
+		if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT && (pSoldier->uiStatusFlags & SOLDIER_UNDERAICONTROL))
 		{
 			SLOGD(DEBUG_TAG_AI, "Ending turn for %d because of error from HandleItem", pSoldier->ubID);
 			EndAIGuysTurn(*pSoldier);

Build/Tactical/TeamTurns.cc
@@ -908,7 +908,7 @@ BOOLEAN StandardInterruptConditionsMet(const SOLDIERTYPE* const pSoldier, const 
 	UINT8						ubMinPtsNeeded;
 	INT8						bDir;
 
-	if ( (gTacticalStatus.uiFlags & TURNBASED) && (gTacticalStatus.uiFlags & INCOMBAT) && !(gubSightFlags & SIGHT_INTERRUPT) )
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT && !(gubSightFlags & SIGHT_INTERRUPT))
 	{
 		return( FALSE );
 	}
@@ -1519,7 +1519,7 @@ void ResolveInterruptsVs( SOLDIERTYPE * pSoldier, UINT8 ubInterruptType)
 	UINT8 ubSlot, ubSmallestSlot;
 	BOOLEAN fIntOccurs;
 
-	if ( (gTacticalStatus.uiFlags & TURNBASED) && (gTacticalStatus.uiFlags & INCOMBAT) )
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 	{
 		ubIntCnt = 0;
 

Build/Tactical/Turn_Based_Input.cc
@@ -1379,8 +1379,7 @@ static void HandleModNone(UINT32 const key, UIEventKind* const new_event)
 
 		case 'd':
 			// End turn only if in combat and it is the player's turn
-			if (gTacticalStatus.uiFlags & TURNBASED          &&
-					gTacticalStatus.uiFlags & INCOMBAT           &&
+			if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT &&
 					gTacticalStatus.ubCurrentTeam == OUR_TEAM &&
 					/* Nothing in hand and the Done button for whichever panel we're in must be enabled */
 					!gpItemPointer                               &&
@@ -1395,8 +1394,7 @@ static void HandleModNone(UINT32 const key, UIEventKind* const new_event)
 
 			if (GCM->getGamePolicy()->can_enter_turnbased)
 			{
-				gTacticalStatus.uiFlags |= TURNBASED;
-				gTacticalStatus.uiFlags |= INCOMBAT;
+				gTacticalStatus.uiFlags |= IN_TB_COMBAT;
 			}
 			break;
 
@@ -1903,8 +1901,7 @@ static void HandleModAlt(UINT32 const key, UIEventKind* const new_event)
 
 		case 'd':
 			if (CHEATER_CHEAT_LEVEL()                        &&
-					gTacticalStatus.uiFlags & TURNBASED          &&
-					gTacticalStatus.uiFlags & INCOMBAT           &&
+					gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT &&
 					gTacticalStatus.ubCurrentTeam == OUR_TEAM &&
 					/* Nothing in hand and the Done button for whichever panel we're in must be enabled */
 					!gpItemPointer                               &&
@@ -2249,7 +2246,7 @@ void GetKeyboardInput(UIEventKind* const puiNewEvent)
 			}
 		}
 
-		if ( gTacticalStatus.uiFlags & TURNBASED && (gTacticalStatus.uiFlags & INCOMBAT) )
+		if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 		{
 			{
 				if ( gTacticalStatus.ubCurrentTeam != OUR_TEAM )
@@ -3233,7 +3230,7 @@ INT8 HandleMoveModeInteractiveClick(UINT16 const usMapPos)
 			if (AnyItemsVisibleOnLevel(pItemPool, bZLevel))
 			{
 				SetUIBusy(sel);
-				if (!(gTacticalStatus.uiFlags & INCOMBAT) && !(gTacticalStatus.uiFlags & TURNBASED))
+				if (!(gTacticalStatus.uiFlags & IN_TB_COMBAT))
 				{
 					BeginDisplayTimedCursor(OKHANDCURSOR_UICURSOR, 300);
 				}

Build/Tactical/UI_Cursors.cc
@@ -182,7 +182,7 @@ static UICursorID HandleActivatedTargetCursor(SOLDIERTYPE* const s, GridNo const
 
 	bool enough_points       = true;
 	bool max_point_limit_hit = false;
-	if (gTacticalStatus.uiFlags & TURNBASED && gTacticalStatus.uiFlags & INCOMBAT)
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 	{
 		gsCurrentActionPoints         = CalcTotalAPsToAttack(s, map_pos, TRUE, s->bShownAimTime / 2);
 		gfUIDisplayActionPoints       = TRUE;
@@ -394,7 +394,7 @@ static UICursorID HandleNonActivatedTargetCursor(SOLDIERTYPE* const s, GridNo co
 		}
 	}
 
-	if (gTacticalStatus.uiFlags & TURNBASED && gTacticalStatus.uiFlags & INCOMBAT)
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 	{
 		DetermineCursorBodyLocation(GetSelectedMan(), show_APs, fRecalc);
 
@@ -590,7 +590,7 @@ static UICursorID HandleKnifeCursor(SOLDIERTYPE* const s, GridNo const map_pos, 
 
 		// Calculate action points
 		bool enough_points = true;
-		if (gTacticalStatus.uiFlags & TURNBASED && gTacticalStatus.uiFlags & INCOMBAT)
+		if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 		{
 			gsCurrentActionPoints         = CalcTotalAPsToAttack(s, map_pos, TRUE, s->bShownAimTime / 2);
 			gfUIDisplayActionPoints       = TRUE;
@@ -775,7 +775,7 @@ static UICursorID HandleNonActivatedTossCursor(SOLDIERTYPE* const s, GridNo cons
 	}
 
 	// Add APs
-	if (gTacticalStatus.uiFlags & TURNBASED && gTacticalStatus.uiFlags & INCOMBAT)
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 	{
 		gsCurrentActionPoints =
 			ubItemCursor == TRAJECTORYCURS ? CalcTotalAPsToAttack(s, map_pos, TRUE, s->bShownAimTime / 2) :
@@ -892,7 +892,7 @@ static UICursorID HandleTinCanCursor(SOLDIERTYPE* const s, GridNo const map_pos,
 
 static UICursorID HandleRemoteCursor(SOLDIERTYPE* const s, BOOLEAN const activated, MouseMoveState const uiCursorFlags)
 {
-	if (gTacticalStatus.uiFlags & TURNBASED && gTacticalStatus.uiFlags & INCOMBAT)
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 	{
 		gsCurrentActionPoints         = GetAPsToUseRemote(s);
 		gfUIDisplayActionPoints       = TRUE;
@@ -913,7 +913,7 @@ static UICursorID HandleBombCursor(SOLDIERTYPE* const s, GridNo const map_pos, B
 {
 	HandleUIMovementCursor(s, uiCursorFlags, map_pos, MOVEUI_TARGET_BOMB);
 
-	if (gTacticalStatus.uiFlags & TURNBASED && gTacticalStatus.uiFlags & INCOMBAT)
+	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
 	{
 		gsCurrentActionPoints         = GetTotalAPsToDropBomb(s, map_pos);
 		gfUIDisplayActionPoints       = TRUE;

commit ca56681b8458cca1b62ba2bf9703c2e5cc5b271c

fixed regression from 989f2d9, fixes #447
== has precedence over &
and constants need to be resolved first

Build/Tactical/Handle_UI.cc
@@ -1256,7 +1256,7 @@ static ScreenID UIHandleMOnTerrain(UI_EVENT* pUIEvent)
 				if (!UIHandleInteractiveTilesAndItemsOnTerrain(sel, usMapPos, FALSE, TRUE))
 				{
 					// Are we in combat?
-					if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+					if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 					{
 						// If so, draw path, etc
 						fSetCursor = HandleUIMovementCursor(sel, uiCursorFlags, usMapPos, MOVEUI_TARGET_NONE);
@@ -3024,7 +3024,7 @@ static INT8 DrawUIMovementPath(SOLDIERTYPE* const pSoldier, UINT16 usMapPos, Mov
 	INT8							bReturnCode = 0;
 	BOOLEAN						fPlot;
 
-	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) || _KeyDown( SHIFT ))
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT || _KeyDown( SHIFT ))
 	{
 		fPlot = PLOT;
 	}
@@ -3523,7 +3523,7 @@ static void SetUIbasedOnStance(SOLDIERTYPE* pSoldier, INT8 bNewStance)
 
 static void SetMovementModeCursor(const SOLDIERTYPE* pSoldier)
 {
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 	{
 			if ( ( OK_ENTERABLE_VEHICLE( pSoldier ) ) )
 			{
@@ -3576,7 +3576,7 @@ static void SetMovementModeCursor(const SOLDIERTYPE* pSoldier)
 
 static void SetConfirmMovementModeCursor(SOLDIERTYPE* pSoldier, BOOLEAN fFromMove)
 {
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 	{
 		if ( gfUIAllMoveOn )
 		{
@@ -4681,7 +4681,7 @@ BOOLEAN HandleTalkInit(  )
 
 void SetUIBusy(const SOLDIERTYPE* const s)
 {
-	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && (gTacticalStatus.ubCurrentTeam == OUR_TEAM))
+	if (((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT) && (gTacticalStatus.ubCurrentTeam == OUR_TEAM))
 	{
 		if (s == GetSelectedMan())
 		{
@@ -4694,7 +4694,7 @@ void SetUIBusy(const SOLDIERTYPE* const s)
 
 void UnSetUIBusy(const SOLDIERTYPE* const s)
 {
-	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && (gTacticalStatus.ubCurrentTeam == OUR_TEAM ))
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT && (gTacticalStatus.ubCurrentTeam == OUR_TEAM ))
 	{
 		if ( !gTacticalStatus.fUnLockUIAfterHiddenInterrupt )
 		{
@@ -4774,7 +4774,7 @@ static bool UIHandleInteractiveTilesAndItemsOnTerrain(SOLDIERTYPE* const pSoldie
 
 			//Set UI CURSOR
 			guiNewUICursor =
-				fUseOKCursor || (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) ?
+				fUseOKCursor || ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT) ?
 					OKHANDCURSOR_UICURSOR : NORMALHANDCURSOR_UICURSOR;
 
 			HandleUIMovementCursor(pSoldier, uiCursorFlags, sActionGridNo, MOVEUI_TARGET_STEAL);
@@ -4838,7 +4838,7 @@ static bool UIHandleInteractiveTilesAndItemsOnTerrain(SOLDIERTYPE* const pSoldie
 
 			//Set UI CURSOR
 			guiNewUICursor =
-				fUseOKCursor || (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) ?
+				fUseOKCursor || ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT) ?
 				OKHANDCURSOR_UICURSOR : NORMALHANDCURSOR_UICURSOR;
 
 			HandleUIMovementCursor(pSoldier, uiCursorFlags, sActionGridNo, MOVEUI_TARGET_ITEMS);

Build/Tactical/Items.cc
@@ -1622,7 +1622,7 @@ BOOLEAN ReloadGun( SOLDIERTYPE * pSoldier, OBJECTTYPE * pGun, OBJECTTYPE * pAmmo
 					// Copying the old ammo to the cursor in turnbased could screw up for the player
 					// (suppose his inventory is full!)
 
-					if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && !EnoughPoints( pSoldier, (INT8) (bAPs + AP_PICKUP_ITEM), 0, FALSE))
+					if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT && !EnoughPoints( pSoldier, (INT8) (bAPs + AP_PICKUP_ITEM), 0, FALSE))
 					{
 						// try autoplace
 						if ( !AutoPlaceObject( pSoldier, &OldAmmo, FALSE ) )

Build/Tactical/OppList.cc
@@ -672,7 +672,7 @@ void HandleSight(SOLDIERTYPE& s, SightFlags const sight_flags)
 	}
 
 	// If we've been told that interrupts are possible as a result of sighting
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT &&
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT &&
 			sight_flags & SIGHT_INTERRUPT)
 	{
 		ResolveInterruptsVs(&s, SIGHTINTERRUPT);
@@ -1986,7 +1986,7 @@ static void OtherTeamsLookForMan(SOLDIERTYPE* pOpponent)
 				// if doing regular in-combat sighting (not on opening doors!)
 				if ( gubBestToMakeSightingSize == BEST_SIGHTING_ARRAY_SIZE_INCOMBAT )
 				{
-					if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && pSoldier->bNewOppCnt)
+					if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT && pSoldier->bNewOppCnt)
 					{
 						// as long as viewer meets minimum interrupt conditions
 						if (gubSightFlags & SIGHT_INTERRUPT && StandardInterruptConditionsMet(pSoldier, pOpponent, bOldOppList))
@@ -3410,7 +3410,7 @@ void OurNoise(SOLDIERTYPE* const noise_maker, INT16 const sGridNo, INT8 const bL
 	ProcessNoise(noise_maker, sGridNo, bLevel, ubVolume, ubNoiseType);
 
 	if (noise_maker != NULL                 &&
-			gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT &&
+			(gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT &&
 			!gfDelayResolvingBestSightingDueToDoor)
 	{
 		// interrupts are possible, resolve them now (we're in control here)
@@ -4122,7 +4122,7 @@ static void HearNoise(SOLDIERTYPE* const pSoldier, SOLDIERTYPE* const noise_make
 			}
 			else
 			{
-				if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+				if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 				{
 					if (StandardInterruptConditionsMet(pSoldier, noise_maker, bOldOpplist))
 					{
@@ -4209,7 +4209,7 @@ static void HearNoise(SOLDIERTYPE* const pSoldier, SOLDIERTYPE* const noise_make
 		if ( gubBestToMakeSightingSize == BEST_SIGHTING_ARRAY_SIZE_INCOMBAT )
 		{
 			// if the noise heard was the fall of a rock
-			if ((gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT) && ubNoiseType == NOISE_ROCK_IMPACT)
+			if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT && ubNoiseType == NOISE_ROCK_IMPACT)
 			{
 				// give every ELIGIBLE listener an automatic interrupt, since it's
 				// reasonable to assume the guy throwing wants to wait for their reaction!

Build/Tactical/Overhead_Types.h
@@ -60,8 +60,8 @@
 #define			IGNORE_ENGAGED_IN_CONV_UI_UNLOCK		0x020000000
 #define			IGNORE_ALL_OBSTACLES								0x040000000
 #define			IN_DEIDRANNA_ENDGAME								0x080000000
-#define			IN_RT_COMBAT							INCOMBAT|REALTIME
-#define			IN_TB_COMBAT							INCOMBAT|TURNBASED
+#define			IN_RT_COMBAT							(INCOMBAT|REALTIME)
+#define			IN_TB_COMBAT							(INCOMBAT|TURNBASED)
 
 #define			OKBREATH						10
 #define			OKLIFE							15

Build/Tactical/Soldier_Control.cc
@@ -2845,7 +2845,7 @@ void EVENT_SoldierGotHit(SOLDIERTYPE* pSoldier, const UINT16 usWeaponIndex, INT1
 	}
 
 	// IAN ADDED THIS SAT JUNE 14th : HAVE TO SHOW VICTIM!
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT && pSoldier->bVisible != -1 && pSoldier->bTeam == OUR_TEAM)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT && pSoldier->bVisible != -1 && pSoldier->bTeam == OUR_TEAM)
 	{
 		LocateSoldier(pSoldier, DONTSETLOCATOR);
 	}
@@ -4872,7 +4872,7 @@ static void CalculateSoldierAniSpeed(SOLDIERTYPE* pSoldier, SOLDIERTYPE* pStatsS
 
 	// MODIFTY NOW BASED ON REAL-TIME, ETC
 	// Adjust speed, make twice as fast if in turn-based!
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 	{
 		pSoldier->sAniDelay = pSoldier->sAniDelay / 2;
 	}
@@ -4898,7 +4898,7 @@ void SetSoldierAniSpeed(SOLDIERTYPE* pSoldier)
 
 	// ATE: If we are an enemy and are not visible......
 	// Set speed to 0
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT || gTacticalStatus.fAutoBandageMode)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT || gTacticalStatus.fAutoBandageMode)
 	{
 		 if ( ( ( pSoldier->bVisible == -1 && pSoldier->bVisible == pSoldier->bLastRenderVisibleValue ) || gTacticalStatus.fAutoBandageMode ) && pSoldier->usAnimState != MONSTER_UP )
 		 {
@@ -8044,7 +8044,7 @@ void SoldierCollapse( SOLDIERTYPE *pSoldier )
 			MakeClosestEnemyChosenOne();
 		}
 
-		if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT && (pSoldier->uiStatusFlags & SOLDIER_UNDERAICONTROL))
+		if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT && (pSoldier->uiStatusFlags & SOLDIER_UNDERAICONTROL))
 		{
 			SLOGD(DEBUG_TAG_AI, "Ending turn for %d because of error from HandleItem", pSoldier->ubID);
 			EndAIGuysTurn(*pSoldier);

Build/Tactical/TeamTurns.cc
@@ -913,7 +913,7 @@ BOOLEAN StandardInterruptConditionsMet(const SOLDIERTYPE* const pSoldier, const 
 	UINT8						ubMinPtsNeeded;
 	INT8						bDir;
 
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT && !(gubSightFlags & SIGHT_INTERRUPT))
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT && !(gubSightFlags & SIGHT_INTERRUPT))
 	{
 		return( FALSE );
 	}
@@ -1524,7 +1524,7 @@ void ResolveInterruptsVs( SOLDIERTYPE * pSoldier, UINT8 ubInterruptType)
 	UINT8 ubSlot, ubSmallestSlot;
 	BOOLEAN fIntOccurs;
 
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 	{
 		ubIntCnt = 0;
 

Build/Tactical/Turn_Based_Input.cc
@@ -1379,7 +1379,7 @@ static void HandleModNone(UINT32 const key, UIEventKind* const new_event)
 
 		case 'd':
 			// End turn only if in combat and it is the player's turn
-			if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT &&
+			if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT &&
 					gTacticalStatus.ubCurrentTeam == OUR_TEAM &&
 					/* Nothing in hand and the Done button for whichever panel we're in must be enabled */
 					!gpItemPointer                               &&
@@ -1900,7 +1900,7 @@ static void HandleModAlt(UINT32 const key, UIEventKind* const new_event)
 
 		case 'd':
 			if (CHEATER_CHEAT_LEVEL()                        &&
-					gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT &&
+					(gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT &&
 					gTacticalStatus.ubCurrentTeam == OUR_TEAM &&
 					/* Nothing in hand and the Done button for whichever panel we're in must be enabled */
 					!gpItemPointer                               &&
@@ -2245,7 +2245,7 @@ void GetKeyboardInput(UIEventKind* const puiNewEvent)
 			}
 		}
 
-		if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+		if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 		{
 			{
 				if ( gTacticalStatus.ubCurrentTeam != OUR_TEAM )

Build/Tactical/UI_Cursors.cc
@@ -182,7 +182,7 @@ static UICursorID HandleActivatedTargetCursor(SOLDIERTYPE* const s, GridNo const
 
 	bool enough_points       = true;
 	bool max_point_limit_hit = false;
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 	{
 		gsCurrentActionPoints         = CalcTotalAPsToAttack(s, map_pos, TRUE, s->bShownAimTime / 2);
 		gfUIDisplayActionPoints       = TRUE;
@@ -394,7 +394,7 @@ static UICursorID HandleNonActivatedTargetCursor(SOLDIERTYPE* const s, GridNo co
 		}
 	}
 
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 	{
 		DetermineCursorBodyLocation(GetSelectedMan(), show_APs, fRecalc);
 
@@ -590,7 +590,7 @@ static UICursorID HandleKnifeCursor(SOLDIERTYPE* const s, GridNo const map_pos, 
 
 		// Calculate action points
 		bool enough_points = true;
-		if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+		if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 		{
 			gsCurrentActionPoints         = CalcTotalAPsToAttack(s, map_pos, TRUE, s->bShownAimTime / 2);
 			gfUIDisplayActionPoints       = TRUE;
@@ -775,7 +775,7 @@ static UICursorID HandleNonActivatedTossCursor(SOLDIERTYPE* const s, GridNo cons
 	}
 
 	// Add APs
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 	{
 		gsCurrentActionPoints =
 			ubItemCursor == TRAJECTORYCURS ? CalcTotalAPsToAttack(s, map_pos, TRUE, s->bShownAimTime / 2) :
@@ -892,7 +892,7 @@ static UICursorID HandleTinCanCursor(SOLDIERTYPE* const s, GridNo const map_pos,
 
 static UICursorID HandleRemoteCursor(SOLDIERTYPE* const s, BOOLEAN const activated, MouseMoveState const uiCursorFlags)
 {
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 	{
 		gsCurrentActionPoints         = GetAPsToUseRemote(s);
 		gfUIDisplayActionPoints       = TRUE;
@@ -913,7 +913,7 @@ static UICursorID HandleBombCursor(SOLDIERTYPE* const s, GridNo const map_pos, B
 {
 	HandleUIMovementCursor(s, uiCursorFlags, map_pos, MOVEUI_TARGET_BOMB);
 
-	if (gTacticalStatus.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT)
+	if ((gTacticalStatus.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT)
 	{
 		gsCurrentActionPoints         = GetTotalAPsToDropBomb(s, map_pos);
 		gfUIDisplayActionPoints       = TRUE;

Build/TacticalAI/AIMain.cc
@@ -642,7 +642,7 @@ void StartNPCAI(SOLDIERTYPE& s)
 	 * HandleSoldierAI() will not be called! */
 
 	// Locate to soldier, if we are not in an interrupt situation.
-	if (ts.uiFlags & IN_TB_COMBAT == IN_TB_COMBAT && gubOutOfTurnPersons == 0)
+	if ((ts.uiFlags & IN_TB_COMBAT) == IN_TB_COMBAT && gubOutOfTurnPersons == 0)
 	{
 		if ((!(s.uiStatusFlags & SOLDIER_VEHICLE) || GetNumberInVehicle(GetVehicle(s.bVehicleID)) != 0) &&
 				((s.bVisible != -1 && s.bLife != 0) || ts.uiFlags & SHOW_ALL_MERCS))