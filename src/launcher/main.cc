df904723b721a627dd6ec7582859bda9c030103a
Manuel K <Manuel-K@users.noreply.github.com>
2017-06-08 14:09:01 +0200

    Use rust to determine config folder

--- src/launcher/main.cc
+++ src/launcher/main.cc
@@ -4,6 +4,7 @@
 
 #include <FileMan.h>
 #include <Launcher.h>
+#include "RustInterface.h"
 
 const std::string getJa2Executable(char* argv[]) {
     std::string exeFolder = FileMan::getParentPath(argv[0], true);
@@ -19,6 +20,18 @@ int main(int argc, char* argv[]) {
     SLOG_SetLevel(SLOG_WARNING, SLOG_WARNING);
 
     Launcher launcher(getJa2Executable(argv));
+    {
+        engine_options_t* params = create_engine_options(argv, argc);
+        if (params == NULL) {
+          return EXIT_FAILURE;
+        }
+
+        char* rustConfigFolderPath = get_stracciatella_home(params);
+        std::string configFolderPath = std::string(rustConfigFolderPath);
+        launcher.setConfigPath(configFolderPath);
+        free_rust_string(rustConfigFolderPath);
+    }
+
     launcher.show();
     return Fl::run();
 }
