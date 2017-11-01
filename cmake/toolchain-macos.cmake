b0437d073a2ef05fa8d605376b403aef40f9fc22
Stefan Lau <github@stefanlau.com>
2017-11-01 13:40:57 +0100

    Try to build on macOS and MSVC

--- cmake/toolchain-macos.cmake
+++ cmake/toolchain-macos.cmake
@@ -5,6 +5,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_OSX_ARCHITECTURES "x86_64" CACHE STRING "" FORCE)
 set(LOCAL_SDL_LIB "dependencies/lib-SDL2-2.0.4-macos" CACHE STRING "" FORCE)
 set(LOCAL_BOOST_LIB ON CACHE BOOL "" FORCE)
+set(LOCAL_FLTK_LIB ON CACHE BOOL "" FORCE)
 set(CMAKE_MACOSX_RPATH ON CACHE BOOL "" FORCE)
 
-set(CMAKE_EXE_LINKER_FLAGS "-framework IOKit -framework Carbon -framework AudioUnit -framework AudioToolbox -framework OpenGL" CACHE STRING "" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "-framework IOKit -framework Carbon -framework AudioUnit -framework AudioToolbox -framework OpenGL -framework CoreFoundation -framework AppKit" CACHE STRING "" FORCE)
