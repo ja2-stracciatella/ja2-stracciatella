8d28f0585eb2e2a11798ecf4d8b0078014efadfd
Stefan Lau <github@stefanlau.com>
2018-10-14 16:25:24 +0200

    Fix standalone cargo test command

--- rust/build.rs
+++ rust/build.rs
@@ -5,8 +5,9 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let header_env = env::var("HEADER_LOCATION").unwrap();
+    let crate_dir = path::PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let default_header_path = crate_dir.join("stracciatella.h").into_os_string().into_string().unwrap();
+    let header_env = env::var("HEADER_LOCATION").unwrap_or(default_header_path);
     let header_path = path::PathBuf::from(&header_env);
     let header_dir = header_path.parent().unwrap();
     let config = cbindgen::Config::from_file("cbindgen.toml").expect("Failed to read `cbindgen.toml`!");
