1b2da3711537e9c81982b419b987f7a5bd6de439
Stefan Lau <github@stefanlau.com>
2019-08-08 20:33:41 +0200

    Rename log file for launcher to match executable name

--- src/launcher/main.cc
+++ src/launcher/main.cc
@@ -5,7 +5,7 @@
 #include "RustInterface.h"
 
 int main(int argc, char* argv[]) {
-	Logger_Init("stracciatella-launcher.log");
+	Logger_Init("ja2-launcher.log");
 
 	Launcher launcher(argc, argv);
 	launcher.loadJa2Json();
