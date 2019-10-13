cbd656d4e190c9bfadf9ad30425ba9d5704a582a
Flávio J. Saraiva <flaviojs2005@gmail.com>
2019-10-13 10:05:20 +0100

    Remove SOURCE_PATH_SIZE
    An absolute path is assumed but it might be relative.

--- src/sgp/Logger.h
+++ src/sgp/Logger.h
@@ -2,21 +2,19 @@
 
 #include "RustInterface.h"
 
-#define __FILENAME__ (__FILE__ + SOURCE_PATH_SIZE)
-
 void LogMessage(bool isAssert, LogLevel level, const char *file, const char *format, ...);
 
 /** Print debug message macro. */
-#define SLOGD(FORMAT, ...) LogMessage(false, LogLevel::Debug, __FILENAME__, FORMAT, ##__VA_ARGS__)
+#define SLOGD(FORMAT, ...) LogMessage(false, LogLevel::Debug, __FILE__, FORMAT, ##__VA_ARGS__)
 
 /** Print info message macro. */
-#define SLOGI(FORMAT, ...) LogMessage(false, LogLevel::Info,  __FILENAME__, FORMAT, ##__VA_ARGS__)
+#define SLOGI(FORMAT, ...) LogMessage(false, LogLevel::Info,  __FILE__, FORMAT, ##__VA_ARGS__)
 
 /** Print warning message macro. */
-#define SLOGW(FORMAT, ...) LogMessage(false, LogLevel::Warn, __FILENAME__, FORMAT, ##__VA_ARGS__)
+#define SLOGW(FORMAT, ...) LogMessage(false, LogLevel::Warn, __FILE__, FORMAT, ##__VA_ARGS__)
 
 /** Print error message macro. */
-#define SLOGE(FORMAT, ...) LogMessage(false, LogLevel::Error, __FILENAME__, FORMAT, ##__VA_ARGS__)
+#define SLOGE(FORMAT, ...) LogMessage(false, LogLevel::Error, __FILE__, FORMAT, ##__VA_ARGS__)
 
 /** Print error message macro. */
-#define SLOGA(FORMAT, ...) LogMessage(true, LogLevel::Error, __FILENAME__, FORMAT, ##__VA_ARGS__)
+#define SLOGA(FORMAT, ...) LogMessage(true, LogLevel::Error, __FILE__, FORMAT, ##__VA_ARGS__)
