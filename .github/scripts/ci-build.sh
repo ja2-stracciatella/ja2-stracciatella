5f9f9cd42eb5f00e4ac3cc1f8efffaec2cf3352d
Flávio J. Saraiva <flaviojs2005@gmail.com>
2019-11-15 20:49:46 +0000

    build

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 set -x
+set -e
 
-echo "# set up environment"
-if [[ "$GITHUB_REF" == "refs/heads/nightly" ]]; then
+echo "#\n# prepare environment\n#"
+if [[ "${GITHUB_REF}" == "refs/heads/nightly" ]]; then
   echo "-- NIGHTLY --"
   export PUBLISH_BINARY="true"
   export PUBLISH_DIR="nightlies"
   export BUILD_TYPE="ReleaseWithDebInfo"
   export VERSION_TAG="$(date +%Y%m%d)"
-elif [[ "$GITHUB_REF" == "refs/tags/"* ]]; then
+elif [[ "${GITHUB_REF}" == "refs/tags/"* ]]; then
   echo "-- RELEASE --"
   export PUBLISH_BINARY="true"
   export PUBLISH_DIR="releases"
   export BUILD_TYPE="ReleaseWithDebInfo"
-elif [[ "$GITHUB_REF" == "refs/pull/"* ]]; then
-  export PULL_REQUEST=$(echo "$GITHUB_REF" | cut -d '/' -f 3)
+elif [[ "${GITHUB_REF}" == "refs/pull/"* ]]; then
+  export PULL_REQUEST=$(echo "${GITHUB_REF}" | cut -d '/' -f 3)
   echo "-- PULL REQUEST ${PULL_REQUEST} --"
   export PUBLISH_BINARY="true"
   export PUBLISH_DIR="pull-requests/${PULL_REQUEST}"
@@ -27,22 +28,32 @@ else
   export BUILD_SWITCHES=""
   export BUILD_TYPE="Debug"
 fi
-export BUILD_SWITCHES="-DCMAKE_BUILD_TYPE=$BUILD_TYPE -DVERSION_TAG=$VERSION_TAG $CI_BUILD_SWITCHES"
-[[ "$BUILD_TYPE" != "Debug" ]] && export BUILD_SWITCHES="$BUILD_SWITCHES -DWITH_EDITOR_SLF=ON";
-[[ "$SFTP_PASSWORD" == "" ]] && export PUBLISH_BINARY="false"
-echo "$PUBLISH_BINARY"
-echo "$PUBLISH_DIR"
-echo "$BUILD_SWITCHES"
-
-echo "# install dependencies"
-export DEP_MANAGER="apt"
-[[ "$CI_OS" == "macos-latest" ]] && export DEP_MANAGER="brew"
-sudo $DEP_MANAGER update
-sudo $DEP_MANAGER install cmake make g++ libsdl2-dev libboost-all-dev fluid libfltk1.3-dev fakeroot
-[[ "$CI_MINGW" == "true" ]] && sudo $DEP_MANAGER install mingw-w64
+export BUILD_SWITCHES="-DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DVERSION_TAG=${VERSION_TAG}"
+if [[ "${BUILD_TYPE}" != "Debug" ]]; then
+   export BUILD_SWITCHES="${BUILD_SWITCHES} -DWITH_EDITOR_SLF=ON"
+fi
+if [[ "${SFTP_PASSWORD}" == "" ]]; then
+  export PUBLISH_BINARY="false"
+fi
+if [[ "$CI_target" == "linux" ]]; then
+  sudo apt update
+  sudo apt install cmake make g++ libsdl2-dev libboost-all-dev fluid libfltk1.3-dev fakeroot
+  export BUILD_SWITCHES="${BUILD_SWITCHES} -DCMAKE_INSTALL_PREFIX=/usr -DEXTRA_DATA_DIR=/usr/share/ja2 -DLOCAL_BOOST_LIB=ON -DCPACK_GENERATOR=DEB"
+elif [[ "$CI_TARGET" == "mingw" ]]; then
+  sudo apt update
+  sudo apt install cmake make g++ libsdl2-dev libboost-all-dev fluid libfltk1.3-dev fakeroot mingw-w64
+  export BUILD_SWITCHES="${BUILD_SWITCHES} -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchain-mingw.cmake -DCPACK_GENERATOR=ZIP"
+elif [[ "$CI_TARGET" == "mac" ]]; then
+  sudo brew update
+  sudo brew install cmake make g++ libsdl2-dev libboost-all-dev fluid libfltk1.3-dev fakeroot
+  export BUILD_SWITCHES="${BUILD_SWITCHES} -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchain-macos.cmake -DCPACK_GENERATOR=Bundle"
+else
+  echo "unexpected target ${CI_TARGET}"
+  exit 1
+fi
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain=$(cat ./rust-toolchain) -y --profile minimal
 export PATH=$PATH:$HOME/.cargo/bin
-if [[ "$CI_MINGW" == "true" ]]; then
+if [[ "$CI_TARGET" == "mingw" ]]; then
   rustup target add x86_64-pc-windows-gnu
 else
   rustup component add rustfmt
@@ -50,34 +61,45 @@ else
   cargo clippy -- -V
   cargo fmt -- -V
 fi
+export BUILD_CMD="cmake --build . --config $BUILD_TYPE"
+echo "$PUBLISH_BINARY"
+echo "$PUBLISH_DIR"
+echo "$BUILD_SWITCHES"
 rustc -V
 cargo -V
 cmake --version
 fakeroot -v
 
-echo "# configure, build, test"
+echo "#\n# configure, build, test\n#"
 mkdir ci-build
 cd ci-build
 cmake $BUILD_SWITCHES ..
-make
-if [[ "$CI_MINGW" != "true" ]]; then
-  sudo make install
-  make cargo-fmt-test
-  make cargo-clippy-test
-  make cargo-test
+cat ./CMakeCache.txt
+$BUILD_CMD
+if [[ "$CI_TARGET" != "mingw" ]]; then
+  sudo $BUILD_CMD --target install
+  $BUILD_CMD --target cargo-fmt-test
+  $BUILD_CMD --target cargo-clippy-test
+  $BUILD_CMD --target cargo-test
   ./ja2 -unittests
   ./ja2-launcher -help
-  sudo make uninstall;
+  sudo $BUILD_CMD --target uninstall
 fi
 
-echo "# package, publish"
-make package
+echo "#\n# package, publish\n#"
+$BUILD_CMD --target package
 for file in ja2-stracciatella_*; do
-echo "$file"
-[[ "$file" == *".deb" ]] && dpkg -c "$file"
-[[ "$file" == *".zip" ]] && unzip -l "$file"
-[[ "$file" == *".dmg" ]] && echo "TODO list contents"
-[[ "$PUBLISH_BINARY" == "true" ]] && curl -v --retry 3 --connect-timeout 60 --max-time 150 --ftp-create-dirs -T "$file" -u $SFTP_USER:$SFTP_PASSWORD ftp://www61.your-server.de/$PUBLISH_DIR/
+  echo "$file"
+  if [[ "$file" == *".deb" ]]; then
+    dpkg -c "$file"
+  elif [[ "$file" == *".zip" ]]; then
+    unzip -l "$file"
+  else
+    echo "TODO list contents"
+  fi
+  if [[ "$PUBLISH_BINARY" == "true" ]]; then
+    curl -v --retry 3 --connect-timeout 60 --max-time 150 --ftp-create-dirs -T "$file" -u $SFTP_USER:$SFTP_PASSWORD ftp://www61.your-server.de/$PUBLISH_DIR/
+  fi
 done
 
-echo "# done"
+echo "#\n# done\n#"
