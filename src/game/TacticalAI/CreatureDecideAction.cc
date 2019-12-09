4df9f783fd9f33f3b1f4ddad1b778f052809acc9
Flávio J. Saraiva <flaviojs2005@gmail.com>
2019-12-09 11:38:21 +0000

    Replace memset in CreatureDecideAction.cc

--- src/game/TacticalAI/CreatureDecideAction.cc
+++ src/game/TacticalAI/CreatureDecideAction.cc
@@ -993,7 +993,7 @@ static INT8 CreatureDecideActionBlack(SOLDIERTYPE* pSoldier)
 	BestShot.ubPossible  = FALSE;	// by default, assume Shooting isn't possible
 	BestStab.ubPossible  = FALSE;	// by default, assume Stabbing isn't possible
 
-	memset(&BestAttack, 0, sizeof(BestAttack)); // XXX HACK000E
+	BestAttack = ATTACKTYPE{};
 
 	bSpitIn = NO_SLOT;
 
