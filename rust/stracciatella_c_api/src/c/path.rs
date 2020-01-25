39085ef5386d0134b6603972a14d58b18c9747b1
Flávio J. Saraiva <flaviojs2005@gmail.com>
2020-01-25 12:38:08 +0000

    Add Path_extension

--- rust/stracciatella_c_api/src/c/path.rs
+++ rust/stracciatella_c_api/src/c/path.rs
@@ -6,6 +6,19 @@ use std::ptr;
 
 use crate::c::common::*;
 
+/// Gets the extension of the path.
+/// Returns null if there is no extension.
+#[no_mangle]
+pub extern "C" fn Path_extension(path: *const c_char) -> *mut c_char {
+    let path = path_from_c_str_or_panic(unsafe_c_str(path));
+    if let Some(extension) = path.extension() {
+        let c_extension = c_string_from_path_or_panic(extension.as_ref());
+        c_extension.into_raw()
+    } else {
+        ptr::null_mut()
+    }
+}
+
 /// Gets the filename of the path.
 /// Returns null if there is no filename.
 #[no_mangle]
