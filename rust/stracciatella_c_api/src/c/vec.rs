83c8b8b1dccdb248623b5c68383fac7260ae34ef
Flávio J. Saraiva <flaviojs2005@gmail.com>
2020-04-01 02:29:09 +0100

    Add VecU8

--- rust/stracciatella_c_api/src/c/vec.rs
+++ rust/stracciatella_c_api/src/c/vec.rs
@@ -54,3 +54,58 @@ pub extern "C" fn VecCString_push(vec: *mut VecCString, value: *const c_char) {
     let value = unsafe_c_str(value);
     vec.inner.push(value.to_owned())
 }
+
+/// A wrapper around `Vec<u8>` for C.
+#[derive(Default)]
+pub struct VecU8 {
+    inner: Vec<u8>,
+}
+
+impl From<Vec<u8>> for VecU8 {
+    fn from(vec: Vec<u8>) -> Self {
+        Self { inner: vec }
+    }
+}
+
+/// Creates an empty vector.
+/// coverity[+alloc]
+#[no_mangle]
+pub extern "C" fn VecU8_create() -> *mut VecU8 {
+    let vec = VecU8::default();
+    into_ptr(vec)
+}
+
+/// Destroys the vector.
+/// coverity[+free : arg-0]
+#[no_mangle]
+pub extern "C" fn VecU8_destroy(vec: *mut VecU8) {
+    let _drop_me = from_ptr(vec);
+}
+
+/// Returns the raw pointer to the vector data.
+#[no_mangle]
+pub extern "C" fn VecU8_as_ptr(vec: *mut VecU8) -> *const u8 {
+    let vec = unsafe_ref(vec);
+    vec.inner.as_ptr()
+}
+
+/// Returns the length of the vector.
+#[no_mangle]
+pub extern "C" fn VecU8_len(vec: *mut VecU8) -> usize {
+    let vec = unsafe_ref(vec);
+    vec.inner.len()
+}
+
+/// Returns the `u8` value at the vector index.
+#[no_mangle]
+pub extern "C" fn VecU8_get(vec: *mut VecU8, index: usize) -> u8 {
+    let vec = unsafe_ref(vec);
+    vec.inner[index]
+}
+
+/// Adds a `u8` value to the end of the vector.
+#[no_mangle]
+pub extern "C" fn VecU8_push(vec: *mut VecU8, value: u8) {
+    let vec = unsafe_mut(vec);
+    vec.inner.push(value)
+}
