2130e3bbec89ed6ffaf56005e7313e0c53bef0e5
Flávio J. Saraiva <flaviojs2005@gmail.com>
2020-04-11 22:20:30 +0100

    Convert GameInitOptionsScreen.cc

--- src/game/GameInitOptionsScreen.cc
+++ src/game/GameInitOptionsScreen.cc
@@ -28,6 +28,9 @@
 #include "WordWrap.h"
 #include "UILayout.h"
 
+#include <string_theory/string>
+
+
 #define GIO_TITLE_FONT			FONT16ARIAL//FONT14ARIAL
 #define GIO_TITLE_COLOR		FONT_MCOLOR_WHITE
 
@@ -177,7 +180,7 @@ static void GetGIOScreenUserInput(void);
 static void RestoreGIOButtonBackGrounds(void);
 static void DoneFadeOutForExitGameInitOptionScreen(void);
 static void DisplayMessageToUserAboutGameDifficulty(void);
-static void DisplayMessageToUserAboutDeadIsDeadSaveScreen(const wchar_t*, MSGBOX_CALLBACK);
+static void DisplayMessageToUserAboutDeadIsDeadSaveScreen(const ST::string& zString, MSGBOX_CALLBACK ReturnCallback);
 static void ConfirmGioDifSettingMessageBoxCallBack(MessageBoxReturnValue);
 static BOOLEAN DisplayMessageToUserAboutIronManMode(void);
 static void ConfirmGioIronManMessageBoxCallBack(MessageBoxReturnValue);
@@ -240,7 +243,7 @@ ScreenID GameInitOptionsScreenHandle(void)
 }
 
 
-static GUIButtonRef MakeButton(BUTTON_PICS* const img, const wchar_t* const text, const INT16 x, const GUI_CALLBACK click)
+static GUIButtonRef MakeButton(BUTTON_PICS* img, const ST::string& text, INT16 x, GUI_CALLBACK click)
 {
 	GUIButtonRef const btn = CreateIconAndTextButton(img, text, OPT_BUTTON_FONT, OPT_BUTTON_ON_COLOR, DEFAULT_SHADOW, OPT_BUTTON_OFF_COLOR, DEFAULT_SHADOW, x, GIO_BTN_OK_Y, MSYS_PRIORITY_HIGH, click);
 	SpecifyButtonSoundScheme(btn, BUTTON_SOUND_SCHEME_BIGSWITCH3);
@@ -364,7 +367,7 @@ static void ExitGIOScreen()
 	gfGIOScreenEntry = TRUE;
 }
 
-static void DisplayMessageToUserAboutDeadIsDeadSaveScreen(const wchar_t *zString, MSGBOX_CALLBACK ReturnCallback)
+static void DisplayMessageToUserAboutDeadIsDeadSaveScreen(const ST::string& zString, MSGBOX_CALLBACK ReturnCallback)
 {
 	gubGameOptionScreenHandler = GIO_EXIT;
 	DoMessageBox(MSG_BOX_BASIC_STYLE, zString, GAME_INIT_OPTIONS_SCREEN, MSG_BOX_FLAG_OK, ReturnCallback, NULL);
@@ -740,7 +743,7 @@ static void DoneFadeOutForExitGameInitOptionScreen(void)
 }
 
 
-static void DoGioMessageBox(const wchar_t *zString, MSGBOX_CALLBACK ReturnCallback)
+static void DoGioMessageBox(const ST::string& zString, MSGBOX_CALLBACK ReturnCallback)
 {
 	DoMessageBox(MSG_BOX_BASIC_STYLE, zString, GAME_INIT_OPTIONS_SCREEN, MSG_BOX_FLAG_YESNO, ReturnCallback, NULL);
 }
@@ -748,8 +751,7 @@ static void DoGioMessageBox(const wchar_t *zString, MSGBOX_CALLBACK ReturnCallba
 
 static void DisplayMessageToUserAboutGameDifficulty(void)
 {
-	const wchar_t* text;
-	text = zGioDifConfirmText[GetCurrentDifficultyButtonSetting()];
+	ST::string text = zGioDifConfirmText[GetCurrentDifficultyButtonSetting()];
 	DoGioMessageBox(text, ConfirmGioDifSettingMessageBoxCallBack);
 }
 
