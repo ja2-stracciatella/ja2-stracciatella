0eb3748ef4008beca115b1b2a630ae7ea942bfbd
Flávio J. Saraiva <flaviojs2005@gmail.com>
2020-04-14 09:58:54 +0100

    Define RUSTC_HOST in CFindRustc.cmake

--- cmake/FindRustc.cmake
+++ cmake/FindRustc.cmake
@@ -3,22 +3,31 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # The module defines the following variables:
-#   RUSTC_FOUND - true if cargo was found
+#   RUSTC_FOUND - true if rust was found
 #   RUSTC_EXECUTABLE - path to the executable
-#   RUSTC_VERSION - cargo version number
+#   RUSTC_VERSION - rust stable version number
+#   RUSTC_HOST = rust host triplet
 # Example usage:
 #   find_package(Rustc 0.10.0 REQUIRED)
 
 find_program(RUSTC_EXECUTABLE rustc HINTS ENV PATH PATHS PATH_SUFFIXES bin)
 
 if (RUSTC_EXECUTABLE)
-    set(COMMAND ${RUSTC_EXECUTABLE} --version)
-    execute_process(COMMAND ${COMMAND} OUTPUT_VARIABLE RUSTC_VERSION_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
-    if(RUSTC_VERSION_OUTPUT MATCHES "rustc ([0-9]+\\.[0-9]+\\.[0-9]+)")
+    execute_process(
+        COMMAND ${RUSTC_EXECUTABLE} -Vv
+        OUTPUT_VARIABLE RUSTC_OUTPUT
+    )
+    if (RUSTC_OUTPUT MATCHES "release: ([^\n]+)\n")
         set(RUSTC_VERSION ${CMAKE_MATCH_1} CACHE INTERNAL "Rustc version")
+    endif()
+    if (RUSTC_OUTPUT MATCHES "host: ([^\n]+)\n")
+        set(RUSTC_HOST ${CMAKE_MATCH_1} CACHE INTERNAL "Rustc host")
     endif()
 endif()
 mark_as_advanced(RUSTC_EXECUTABLE)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Rustc REQUIRED_VARS RUSTC_EXECUTABLE RUSTC_VERSION VERSION_VAR RUSTC_VERSION)
+find_package_handle_standard_args(Rustc
+    REQUIRED_VARS RUSTC_EXECUTABLE RUSTC_VERSION RUSTC_HOST
+    VERSION_VAR RUSTC_VERSION
+)
