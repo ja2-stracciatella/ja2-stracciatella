d2709424507e6ab80c64a18b5768cc4423848852
ja2-derek <sfod_csy@hotmail.com>
2020-04-18 03:13:05 +0800

    Do not do c_str() on temp variable
    The ST::string and hence the c_str() buffer reference may be deallocated when the string variable goes out of scope.
    
    Using ST::format so we can use ST::string directly.

--- src/game/Strategic/Strategic_Mines.cc
+++ src/game/Strategic/Strategic_Mines.cc
@@ -530,7 +530,7 @@ static INT32 MineAMine(INT8 bMineIndex)
 		if( iAmtExtracted > 0 )
 		{
 			// debug message
-			SLOGD("%s - Mine income from %s = $%d", WORLDTIMESTR.c_str(), GCM->getTownName(GetTownAssociatedWithMine(bMineIndex)).c_str(), iAmtExtracted);
+			SLOGD(ST::format("{} - Mine income from {} = ${}", WORLDTIMESTR, GCM->getTownName(GetTownAssociatedWithMine(bMineIndex)), iAmtExtracted));
 
 			// if this is the first time this mine has produced income for the player in the game
 			if ( !gMineStatus[ bMineIndex ].fMineHasProducedForPlayer )
