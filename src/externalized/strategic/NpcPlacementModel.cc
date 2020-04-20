1f2a7e860833061256318896d05b2998ac5d9744
ja2-derek <sfod_csy@hotmail.com>
2020-04-21 06:34:42 +0800

    Adding checks against bad input json
    - Each of the given sectors must be valid, otherwise throw an exception
    - Adda message to the modder that just adding NPCs in json will do nothing

--- src/externalized/strategic/NpcPlacementModel.cc
+++ src/externalized/strategic/NpcPlacementModel.cc
@@ -1,5 +1,3 @@
-#pragma once
-
 #include "NpcPlacementModel.h"
 #include "Campaign_Types.h"
 
@@ -11,7 +9,12 @@ NpcPlacementModel* NpcPlacementModel::deserialize(const rapidjson::Value& elemen
 	std::vector<uint8_t> sectorIds;
 	for (auto& sector : element["sectors"].GetArray())
 	{
-		sectorIds.push_back(SECTOR_FROM_SECTOR_SHORT_STRING(sector.GetString()));
+		auto sectorString = sector.GetString();
+		if (!IS_VALID_SECTOR_SHORT_STRING(sectorString))
+		{
+			throw std::runtime_error("Invalid sector string");
+		}
+		sectorIds.push_back(SECTOR_FROM_SECTOR_SHORT_STRING(sectorString));
 	}
 	return new NpcPlacementModel(
 		element["profileId"].GetInt(),
