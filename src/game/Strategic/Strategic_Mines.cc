3211487fddb4ccb3dc0b5b8df700c9893930c40c
Flávio J. Saraiva <flaviojs2005@gmail.com>
2020-04-24 08:35:30 +0100

    Fix divide by zero in GetAvailableWorkForceForMineForEnemy
    Detected by coverity.

--- src/game/Strategic/Strategic_Mines.cc
+++ src/game/Strategic/Strategic_Mines.cc
@@ -459,17 +459,16 @@ static INT32 GetAvailableWorkForceForMineForEnemy(INT8 bMineIndex)
 
 	bTownId = gMineLocation[ bMineIndex ].bAssociatedTown;
 
-	if( GetTownSectorSize( bTownId )  == 0 )
-	{
-		return 0;
-	}
+	UINT8 numSectors = GetTownSectorSize( bTownId );
+	Assert(numSectors > 0);
+	UINT8 numSectorsUnderControl = GetTownSectorsUnderControl( bTownId );
+	Assert(numSectorsUnderControl <= numSectors);
 
 	// get workforce size (is 0-100 based on REVERSE of local town's loyalty)
 	iWorkForceSize = 100 - gTownLoyalty[ bTownId ].ubRating;
 
 	// now adjust for town size.. the number of sectors you control
-	iWorkForceSize *= ( GetTownSectorSize( bTownId ) - GetTownSectorsUnderControl( bTownId ) );
-	iWorkForceSize /= GetTownSectorSize( bTownId );
+	iWorkForceSize = iWorkForceSize * (numSectors - numSectorsUnderControl) / numSectors;
 
 	return ( iWorkForceSize );
 }
