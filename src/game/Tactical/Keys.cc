ja2-stracciatella/ja2-stracciatella

commit 4ee4f52838b6f8dda43000185fdad2358a80e16e

Fixed reading of free()ed memory.

src/game/Tactical/Keys.cc
@@ -1053,8 +1053,8 @@ static void SynchronizeDoorStatusToStructureData(DOOR_STATUS const& d)
 	if (door_open == struct_open) return;
 
 	// Swap!
-	STRUCTURE* const base = FindBaseStructure(s);
-	SwapStructureForPartner(base);
+	STRUCTURE *base = FindBaseStructure(s);
+	base = SwapStructureForPartner(base);
 	RecompileLocalMovementCosts(base->sGridNo);
 }
 

commit 14c8425fcdcba6e202324111cd2b056980988a20

SwapStructureForPartner can return null
SwapStructureForPartner returns null when a badly placed door collides with a wall.

This issue was first introduced in 4ee4f52838. Since function only handles doors, the gridNo of the base is same as the partner.

Adding a null check after FindBaseStructure. If not null, use the gridNo from base.

src/game/Tactical/Keys.cc
@@ -1005,8 +1005,14 @@ static void SynchronizeDoorStatusToStructureData(DOOR_STATUS const& d)
 
 	// Swap!
 	STRUCTURE *base = FindBaseStructure(s);
-	base = SwapStructureForPartner(base);
-	RecompileLocalMovementCosts(base->sGridNo);
+	if (!base)
+	{
+		SLOGW(ST::format("Door structure data at {} was not found", d.sGridNo));
+		return;
+	}
+	INT16 sBaseGridNo = base->sGridNo;
+	SwapStructureForPartner(base);
+	RecompileLocalMovementCosts(sBaseGridNo);
 }
 
 