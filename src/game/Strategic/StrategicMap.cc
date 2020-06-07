ja2-stracciatella/ja2-stracciatella

commit d1c82a9d96e90d208fd0c4278bd0ef1c4bab76f0

Externalizing mines (#1098)

src/game/Strategic/StrategicMap.cc
@@ -59,6 +59,7 @@
 #include "Message.h"
 #include "MessageBoxScreen.h"
 #include "Militia_Control.h"
+#include "MineModel.h"
 #include "Music_Control.h"
 #include "NPC.h"
 #include "OppList.h"
@@ -758,19 +759,20 @@ void HandleQuestCodeOnSectorEntry( INT16 sNewSectorX, INT16 sNewSectorY, INT8 bN
 		INT8 const ubThisMine = GetMineIndexForSector(sector);
 		if (ubThisMine != -1 && !CheckFact(FACT_MINERS_PLACED, 0))
 		{
-			// SET HEAD MINER LOCATIONS
-			if (ubThisMine != MINE_SAN_MONA) // San Mona is abandoned
+			auto thisMine = GCM->getMine(ubThisMine);
+			// SET HEAD MINER LOCATIONS, unless mine is abandoned
+			if (!thisMine->isAbandoned())
 			{
 				ubMinersPlaced = 0;
 
-				if (ubThisMine != MINE_ALMA)
+				if (!thisMine->headMinerAssigned)
 				{
-					// Fred Morris is always in the first mine sector we enter, unless that's Alma (then he's randomized, too)
+					// Fred Morris is always in the first mine sector we enter, unless head miner here has been pre-determined (then he's randomized, too)
 					MERCPROFILESTRUCT& fred = GetProfile(FRED);
 					fred.sSectorX = sNewSectorX;
 					fred.sSectorY = sNewSectorY;
 					fred.bSectorZ = 0;
-					fred.bTown    = gMineLocation[ubThisMine].bAssociatedTown;
+					fred.bTown    = thisMine->associatedTownId;
 
 					// mark miner as placed
 					ubRandomMiner[ 0 ] = 0;
@@ -778,9 +780,9 @@ void HandleQuestCodeOnSectorEntry( INT16 sNewSectorX, INT16 sNewSectorY, INT8 bN
 				}
 
 				// assign the remaining (3) miners randomly
-				for ( ubMine = 0; ubMine < MAX_NUMBER_OF_MINES; ubMine++ )
+				for (auto ubMine : GCM->getMines())
 				{
-					if ( ubMine == ubThisMine || ubMine == MINE_ALMA || ubMine == MINE_SAN_MONA )
+					if ( ubMine->mineId == ubThisMine || ubMine->headMinerAssigned || ubMine->isAbandoned() )
 					{
 						// Alma always has Matt as a miner, and we have assigned Fred to the current mine
 						// and San Mona is abandoned
@@ -794,11 +796,11 @@ void HandleQuestCodeOnSectorEntry( INT16 sNewSectorX, INT16 sNewSectorY, INT8 bN
 					while( ubRandomMiner[ ubMiner ] == 0 );
 
 					MERCPROFILESTRUCT& p      = GetProfile(ubRandomMiner[ubMiner]);
-					UINT8 const        sector = GetMineSector(ubMine);
+					UINT8 const        sector = ubMine->entranceSector;
 					p.sSectorX = SECTORX(sector);
 					p.sSectorY = SECTORY(sector);
 					p.bSectorZ = 0;
-					p.bTown = gMineLocation[ ubMine ].bAssociatedTown;
+					p.bTown = thisMine->associatedTownId;
 
 					// mark miner as placed
 					ubRandomMiner[ ubMiner ] = 0;

src/externalized/ContentManager.h
@@ -23,6 +23,7 @@ class FactParamsModel;
 class GamePolicy;
 class GarrisonGroupModel;
 class IMPPolicy;
+class MineModel;
 class MovementCostsModel;
 class NpcActionParamsModel;
 class NpcPlacementModel;
@@ -133,6 +134,9 @@ public:
 	virtual const std::vector<const BloodCatPlacementsModel*> & getBloodCatPlacements() const = 0;
 	virtual const std::vector<const BloodCatSpawnsModel*> & getBloodCatSpawns() const = 0;
 	virtual const BloodCatSpawnsModel* getBloodCatSpawnsOfSector(uint8_t sectorId) const = 0;
+	virtual const MineModel* getMineForSector(uint8_t sectorX, uint8_t sectorY, uint8_t sectorZ) const = 0;
+	virtual const MineModel* getMine(uint8_t mineId) const = 0;
+	virtual const std::vector<const MineModel*>& getMines() const = 0;
 	virtual const std::vector<const SamSiteModel*>& getSamSites() const = 0;
 	virtual const int8_t findSamIDBySector(uint8_t sectorId) const = 0;
 	virtual const SamSiteModel* findSamSiteBySector(uint8_t sectorId) const = 0;

src/externalized/DefaultContentManager.cc
@@ -34,6 +34,7 @@
 #include "strategic/BloodCatPlacementsModel.h"
 #include "strategic/BloodCatSpawnsModel.h"
 #include "strategic/FactParamsModel.h"
+#include "strategic/MineModel.h"
 #include "strategic/SamSiteModel.h"
 #include "strategic/TownModel.h"
 #include "strategic/MovementCostsModel.h"
@@ -311,6 +312,7 @@ DefaultContentManager::~DefaultContentManager()
 	deleteElements(m_bloodCatPlacements);
 	deleteElements(m_bloodCatSpawns);
 	deleteElements(m_factParams);
+	deleteElements(m_mines);
 	deleteElements(m_npcActionParams);
 	deleteElements(m_npcPlacements);
 	deleteElements(m_samSites);
@@ -1211,6 +1213,17 @@ bool DefaultContentManager::loadStrategicLayerData() {
 	}
 	delete json;
 
+	json = readJsonDataFile("strategic-mines.json");
+	auto arr = json->GetArray();
+	for (rapidjson::SizeType i = 0; i < arr.Size(); i++)
+	{
+		m_mines.push_back(
+			MineModel::deserialize(i, arr[i].GetObject())
+		);
+	}
+	MineModel::validateData(m_mines);
+	delete json;
+
 	json = readJsonDataFile("strategic-map-sam-sites.json");
 	for (auto& element : json->GetArray())
 	{
@@ -1288,6 +1301,31 @@ const BloodCatSpawnsModel* DefaultContentManager::getBloodCatSpawnsOfSector(uint
 		}
 	}
 	return NULL;
+}
+
+const MineModel* DefaultContentManager::getMineForSector(uint8_t sectorX, uint8_t sectorY, uint8_t sectorZ) const
+{
+	auto sectorId = SECTOR(sectorX, sectorY);
+	for (auto m : m_mines)
+	{
+		if (sectorZ == 0 && sectorId == m->entranceSector) return m;
+		for (auto s : m->mineSectors)
+		{
+			if (s[0] == sectorId && s[1] == sectorZ) return m;
+		}
+	}
+
+	return NULL;
+}
+
+const MineModel* DefaultContentManager::getMine(uint8_t mineId) const
+{
+	return m_mines[mineId];
+}
+
+const std::vector<const MineModel*>& DefaultContentManager::getMines() const
+{
+	return m_mines;
 }
 
 const TownModel* DefaultContentManager::getTown(int8_t townId) const

src/externalized/DefaultContentManager.h
@@ -159,6 +159,9 @@ public:
 	virtual const std::vector<const BloodCatPlacementsModel*>& getBloodCatPlacements() const override;
 	virtual const std::vector<const BloodCatSpawnsModel*>& getBloodCatSpawns() const override;
 	virtual const BloodCatSpawnsModel* getBloodCatSpawnsOfSector(uint8_t sectorId) const override;
+	virtual const MineModel* getMineForSector(uint8_t sectorX, uint8_t sectorY, uint8_t sectorZ) const override;
+	virtual const MineModel* getMine(uint8_t mineId) const override;
+	virtual const std::vector<const MineModel*>& getMines() const override;
 	virtual const std::vector<const SamSiteModel*>& getSamSites() const override;
 	virtual const int8_t findSamIDBySector(uint8_t sectorId) const override;
 	virtual const SamSiteModel* findSamSiteBySector(uint8_t sectorId) const override;
@@ -223,6 +226,7 @@ protected:
 
 	std::vector<const BloodCatPlacementsModel*> m_bloodCatPlacements;
 	std::vector<const BloodCatSpawnsModel*> m_bloodCatSpawns;
+	std::vector<const MineModel*> m_mines;
 	std::vector<const SamSiteModel*> m_samSites;
 	std::map<int8_t, const TownModel*> m_towns;
 	std::vector<const ST::string*> m_townNames;

src/externalized/JsonObject.h
@@ -59,6 +59,11 @@ public:
 		return m_value[name].GetInt();
 	}
 
+	unsigned int GetUInt(const char* name) const
+	{
+		return m_value[name].GetUint();
+	}
+
 	bool GetBool(const char *name) const
 	{
 		return m_value[name].GetBool();

src/externalized/strategic/CMakeLists.txt
@@ -5,6 +5,7 @@ set(JA2_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/BloodCatPlacementsModel.cc
     ${CMAKE_CURRENT_SOURCE_DIR}/BloodCatSpawnsModel.cc
     ${CMAKE_CURRENT_SOURCE_DIR}/FactParamsModel.cc
+    ${CMAKE_CURRENT_SOURCE_DIR}/MineModel.cc
     ${CMAKE_CURRENT_SOURCE_DIR}/MovementCostsModel.cc
     ${CMAKE_CURRENT_SOURCE_DIR}/NpcPlacementModel.cc
     ${CMAKE_CURRENT_SOURCE_DIR}/SamSiteModel.cc

src/externalized/strategic/MineModel.cc
@@ -1,0 +1,92 @@
+#include "MineModel.h"
+
+#include "Campaign_Types.h"
+#include "Strategic_Mines.h"
+
+
+MineModel::MineModel(const uint8_t mineId_, const uint8_t entranceSector_, const uint8_t associatedTownId_, 
+	const uint8_t mineType_, const uint16_t minimumMineProduction_, const bool headMinerAssigned_,
+	const bool noDepletion_, const bool delayDepletion_, const bool isInfestible_,
+	std::vector<std::array<uint8_t, 2>> mineSectors_, const int16_t faceDisplayYOffset_) :
+		mineId(mineId_), entranceSector(entranceSector_), associatedTownId(associatedTownId_),
+		mineType(mineType_), minimumMineProduction(minimumMineProduction_), headMinerAssigned(headMinerAssigned_),
+		noDepletion(noDepletion_), delayDepletion(delayDepletion_), isInfestible(isInfestible_),
+		mineSectors(mineSectors_), faceDisplayYOffset(faceDisplayYOffset_) {}
+
+bool MineModel::isAbandoned() const
+{
+	return minimumMineProduction == 0;
+}
+
+uint8_t mineTypeFromString(std::string mineType)
+{
+	if (mineType == "GOLD_MINE") return GOLD_MINE;
+	if (mineType == "SILVER_MINE") return SILVER_MINE;
+	
+	SLOGE(ST::format("Unrecognized mine type: '{}'", mineType));
+	throw std::runtime_error("");
+}
+
+std::vector<std::array<uint8_t, 2>> readMineSectors(const rapidjson::Value& sectorsJson)
+{
+	std::vector<std::array<uint8_t, 2>> sectors;
+	for (auto& s: sectorsJson.GetArray())
+	{
+		auto sector = s.GetArray();
+		if (sector.Size() != 2) throw new std::runtime_error("Elements in mineSector must be arrays of 2");
+
+		auto sectorString = sector[0].GetString();
+		auto sectorLevel = sector[1].GetUint();
+		if (!IS_VALID_SECTOR_SHORT_STRING(sectorString))
+		{
+			throw new std::runtime_error("mineSector given is not a valid sector string");
+		}
+
+		sectors.push_back(std::array<uint8_t, 2> {
+			SECTOR_FROM_SECTOR_SHORT_STRING(sectorString),
+			static_cast<uint8_t>(sectorLevel)
+		});
+	}
+	return sectors;
+}
+
+MineModel* MineModel::deserialize(uint8_t index, const rapidjson::Value& json)
+{
+	auto obj = JsonObjectReader(json);
+	const char* entrance = obj.GetString("entranceSector");
+	if (!IS_VALID_SECTOR_SHORT_STRING(entrance))
+	{
+		SLOGE(ST::format("Invalid mine entrance sector: '{}'", entrance));
+		throw std::runtime_error("");
+	}
+
+	auto mineSectors = readMineSectors(json["mineSectors"]);
+
+	return new MineModel(
+		index,
+		SECTOR_FROM_SECTOR_SHORT_STRING(entrance),
+		obj.GetUInt("associatedTownId"),
+		mineTypeFromString(obj.GetString("mineType")),
+		obj.GetUInt("minimumMineProduction"),
+		obj.getOptionalBool("headMinerAssigned"),
+		obj.getOptionalBool("noDepletion"),
+		obj.getOptionalBool("delayDepletion"),
+		obj.getOptionalBool("isInfestible"),
+		mineSectors,
+		obj.getOptionalInt("faceDisplayYOffset")
+	);
+}
+
+void MineModel::validateData(std::vector<const MineModel*> models)
+{
+	if (models.size() != MAX_NUMBER_OF_MINES)
+	{
+		SLOGW(ST::format("There are {} mines defined. Expected {} mines. This breaks vanilla save compatability", models.size(), MAX_NUMBER_OF_MINES));
+	}
+	if (models.size() < MAX_NUMBER_OF_MINES)
+	{
+		// Mine-related quests use hard-coded enums and may run into problems.
+		SLOGE(ST::format("There are fewer than {} mines defined.", MAX_NUMBER_OF_MINES));
+		throw std::runtime_error("");
+	}
+}

src/externalized/strategic/MineModel.h
@@ -1,0 +1,35 @@
+#pragma once
+
+#include "JsonObject.h"
+#include <array>
+#include <rapidjson/document.h>
+#include <string>
+#include <vector>
+
+class MineModel
+{
+public:
+	MineModel(const uint8_t mineId, const uint8_t entranceSector_, const uint8_t associatedTownId_, const uint8_t mineType_, const uint16_t minimumMineProduction_,
+		const bool headMinerAssigned_, const bool noDepletion_, const bool delayDepletion_, const bool isInfestible_,
+		const std::vector<std::array<uint8_t, 2>> mineSectors_,
+		const int16_t faceDisplayYOffset);
+ 
+	bool isAbandoned() const;
+
+	const uint8_t mineId;
+	const uint8_t associatedTownId;
+	const uint8_t entranceSector;
+	const uint8_t mineType;
+	const uint16_t minimumMineProduction;
+	const bool headMinerAssigned;
+	const bool noDepletion;
+	const bool delayDepletion;
+	const bool isInfestible;
+
+	const std::vector<std::array<uint8_t, 2>> mineSectors;
+
+	const int16_t faceDisplayYOffset;
+	
+	static MineModel* deserialize(uint8_t index, const rapidjson::Value& json);
+	static void validateData(std::vector<const MineModel*> models);
+};

commit e0c071b1773830b16f47f0ffe6aa5a7b8d217418

Fixing head miner assignment
Bug introduced in #1098

src/game/Strategic/StrategicMap.cc
@@ -800,7 +800,7 @@ void HandleQuestCodeOnSectorEntry( INT16 sNewSectorX, INT16 sNewSectorY, INT8 bN
 					p.sSectorX = SECTORX(sector);
 					p.sSectorY = SECTORY(sector);
 					p.bSectorZ = 0;
-					p.bTown = thisMine->associatedTownId;
+					p.bTown = ubMine->associatedTownId;
 
 					// mark miner as placed
 					ubRandomMiner[ ubMiner ] = 0;