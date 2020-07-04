76be84ed0d7c032a3bd5a8926aab12e98125554f
Stefan Lau <github@stefanlau.com>
2020-07-04 10:45:30 +0200

    Replace absolute path at compile time for log statements

--- src/sgp/Logger.h
+++ src/sgp/Logger.h
@@ -8,19 +8,22 @@
 void LogMessage(bool isAssert, LogLevel level, const char* file, const ST::string& str);
 void LogMessage(bool isAssert, LogLevel level, const char *file, const char *format, ...);
 
+/** Get filename relative to src directory */
+#define __FILENAME__ (__FILE__ + SOURCE_PATH_SIZE)
+
 /** Print debug message macro. */
-#define SLOGD(FORMAT, ...) LogMessage(false, LogLevel::Debug, __FILE__, FORMAT, ##__VA_ARGS__)
+#define SLOGD(FORMAT, ...) LogMessage(false, LogLevel::Debug, __FILENAME__, FORMAT, ##__VA_ARGS__)
 
 /** Print info message macro. */
-#define SLOGI(FORMAT, ...) LogMessage(false, LogLevel::Info,  __FILE__, FORMAT, ##__VA_ARGS__)
+#define SLOGI(FORMAT, ...) LogMessage(false, LogLevel::Info,  __FILENAME__, FORMAT, ##__VA_ARGS__)
 
 /** Print warning message macro. */
-#define SLOGW(FORMAT, ...) LogMessage(false, LogLevel::Warn, __FILE__, FORMAT, ##__VA_ARGS__)
+#define SLOGW(FORMAT, ...) LogMessage(false, LogLevel::Warn, __FILENAME__, FORMAT, ##__VA_ARGS__)
 
 /** Print error message macro. */
-#define SLOGE(FORMAT, ...) LogMessage(false, LogLevel::Error, __FILE__, FORMAT, ##__VA_ARGS__)
+#define SLOGE(FORMAT, ...) LogMessage(false, LogLevel::Error, __FILENAME__, FORMAT, ##__VA_ARGS__)
 
 /** Print error message macro. */
-#define SLOGA(FORMAT, ...) LogMessage(true, LogLevel::Error, __FILE__, FORMAT, ##__VA_ARGS__)
+#define SLOGA(FORMAT, ...) LogMessage(true, LogLevel::Error, __FILENAME__, FORMAT, ##__VA_ARGS__)
 
 #endif//SGP_LOGGER_H_
