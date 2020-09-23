cc8722922eabc2a9643a5fa90bef10ef7110bfa2
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2020-09-24 07:59:50 +0800

    wip: more enums

--- assets/externalized/scripts/enums.lua
+++ assets/externalized/scripts/enums.lua
@@ -1,17 +1,116 @@
 Items = {
     SUNGOGGLE = 212,
-    CANTEEN   = 214
+    CANTEEN   = 214,
 }
 
 Visibility = {
 	BURIED               =  -3,
 	HIDDEN_IN_OBJECT     =  -2,
 	INVISIBLE            =  -1,
-	VISIBLE              =   1
+	VISIBLE              =   1,
 }
 
 SectorFlags = {
     SF_ALREADY_VISITED = 32,
     SF_USE_ALTERNATE_MAP = 64,
-    SF_HAS_ENTERED_TACTICAL = 512
+    SF_HAS_ENTERED_TACTICAL = 512,
+}
+
+CivGroups = {
+    NON_CIV_GROUP = 0,
+    REBEL_CIV_GROUP = 1,
+    KINGPIN_CIV_GROUP = 2,
+    SANMONA_ARMS_GROUP = 3,
+    ALMA_MILITARY_CIV_GROUP = 7,
+    DOCTORS_CIV_GROUP = 8,
+    HICKS_CIV_GROUP = 10,
+    WARDEN_CIV_GROUP = 11,
+}
+
+Teams = {
+    OUR_TEAM = 0,
+    ENEMY_TEAM = 1,
+    CREATURE_TEAM = 2,
+    MILITIA_TEAM = 3,
+    CIV_TEAM = 4,
+}
+
+EnemyRanks = {
+    ENEMY_RANK_ADMIN = 0,
+    ENEMY_RANK_TROOP = 1,
+    ENEMY_RANK_ELITE = 2,
+}
+
+MilitiaRanks = {
+    GREEN_MILITIA = 0,
+    REGULAR_MILITIA = 1,
+    ELITE_MILITIA = 2,
+}
+
+SoldierBodyTypes = {
+    BODY_RANDOM = -1,
+    REGMALE = 0,
+    BIGMALE = 1,
+    STOCKYMALE = 2,
+    REGFEMALE = 3,
+    ADULTFEMALEMONSTER = 4,
+    AM_MONSTER = 5,
+    YAF_MONSTER = 6,
+    YAM_MONSTER = 7,
+    LARVAE_MONSTER = 8,
+    INFANT_MONSTER = 9,
+    QUEENMONSTER = 10,
+    FATCIV = 11,
+    MANCIV = 12,
+    MINICIV = 13,
+    DRESSCIV = 14,
+    HATKIDCIV = 15,
+    KIDCIV = 16,
+    CRIPPLECIV = 17,
+    COW = 18,
+    CROW = 19,
+    BLOODCAT = 20,
+    ROBOTNOWEAPON = 21,
+    HUMVEE = 22,
+    TANK_NW = 23,
+    TANK_NE = 24,
+    ELDORADO = 25,
+    ICECREAMTRUCK = 26,
+    JEEP = 27,
+}
+
+MercProfiles = {
+    MIGUEL = 57,
+    CARLOS = 58,
+    IRA = 59,
+    DIMITRI = 60,
+
+    QUEEN = 75,
+
+    SKYRIDER = 97,
+    PABLO = 98,
+    FATIMA = 101,
+
+    ELLIOT = 135,
+
+    NO_PROFILE = 200,
+}
+
+SkillTraits =  {
+    NO_SKILLTRAIT = 0,
+    LOCKPICKING = 1,
+    HANDTOHAND = 2,
+    ELECTRONICS = 3,
+    NIGHTOPS = 4,
+    THROWING = 5,
+    TEACHING = 6,
+    HEAVY_WEAPS = 7,
+    AUTO_WEAPS = 8,
+    STEALTHY = 9,
+    AMBIDEXT = 10,
+    THIEF = 11,
+    MARTIALARTS = 12,
+    KNIFING = 13,
+    ONROOF = 14,
+    CAMOUFLAGED = 15,
 }
