c454c98a486e994cd07f3805d3b2518c235f9444
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2020-09-24 21:58:17 +0800

    Disable vanilla Strategic AI if no groups configured

--- src/game/Strategic/Strategic_AI.cc
+++ src/game/Strategic/Strategic_AI.cc
@@ -144,6 +144,9 @@ UINT8 *gubPatrolReinforcementsDenied = NULL;
 //Unsaved vars
 BOOLEAN gfDisplayStrategicAILogs = FALSE;
 
+
+BOOLEAN gfIsStrategicAIActive = FALSE;
+
 extern INT16 sWorldSectorLocationOfFirstBattle;
 static const SGPSector meduna(3, 16);
 
@@ -316,6 +319,26 @@ static void ValidateLargeGroup(GROUP* pGroup)
 
 void InitStrategicAI()
 {
+	if (GCM->getGarrisonGroups().empty() && GCM->getPatrolGroups().empty())
+	{
+		SLOGI("Strategic AI disabled");
+		return;
+	}
+
+	//Initialize the sectorinfo structure so all sectors don't point to a garrisonID.
+	FOR_EACH(SECTORINFO, i, SectorInfo)
+	{
+		i->ubGarrisonID = NO_GARRISON;
+	}
+
+	if (GCM->getGarrisonGroups().empty() && GCM->getPatrolGroups().empty())
+	{
+		SLOGI("Strategic AI disabled");
+		gfIsStrategicAIActive = FALSE;
+		return;
+	}
+
+	gfIsStrategicAIActive = TRUE;
 	gfExtraElites                      = FALSE;
 	gubNumAwareBattles                 = 0;
 	gfQueenAIAwake                     = FALSE;
@@ -342,11 +365,6 @@ void InitStrategicAI()
 
 	AddStrategicEvent(EVENT_EVALUATE_QUEEN_SITUATION, evaluate_time, 0);
 
-	//Initialize the sectorinfo structure so all sectors don't point to a garrisonID.
-	FOR_EACH(SECTORINFO, i, SectorInfo)
-	{
-		i->ubGarrisonID = NO_GARRISON;
-	}
 
 	/* Copy over the original army composition as it does get modified during the
 	 * campaign. This bulletproofs starting the game over again. */
@@ -566,6 +584,8 @@ void InitStrategicAI()
 
 void KillStrategicAI()
 {
+	gfIsStrategicAIActive = FALSE;
+
 	gPatrolGroup.clear();
 	gGarrisonGroup.clear();
 
@@ -2866,6 +2886,8 @@ static UINT8 RedirectEnemyGroupsMovingThroughSector(const SGPSector& sSector);
 
 void StrategicHandleQueenLosingControlOfSector(const SGPSector& sSector)
 {
+	if (!gfIsStrategicAIActive) return;
+
 	SECTORINFO *pSector;
 	UINT8 ubSectorID;
 	if (sSector.z)
