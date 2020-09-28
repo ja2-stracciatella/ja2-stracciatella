296825339f941f26c7668590eaaee5c581cedf0d
Stefan Lau <github@stefanlau.com>
2020-09-28 15:17:47 +0200

    Enable fullscreen mode for StracciatellaActivity

--- android/app/src/main/java/io/github/ja2stracciatella/StracciatellaActivity.kt
+++ android/app/src/main/java/io/github/ja2stracciatella/StracciatellaActivity.kt
@@ -1,5 +1,6 @@
 package io.github.ja2stracciatella
 
+import android.view.View
 import org.libsdl.app.SDLActivity
 
 open class StracciatellaActivity : SDLActivity() {
@@ -9,5 +10,19 @@ open class StracciatellaActivity : SDLActivity() {
             "SDL2",
             "ja2"
         );
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        // Set app to fullscreen mode
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+        }
     }
 }
