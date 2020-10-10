d30ea777ec873930eafc83da196d07b9c6009eee
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2020-10-11 00:19:18 +0800

    Externalizing the I.M.P. merc initial exp level

--- src/externalized/policy/IMPPolicy.h
+++ src/externalized/policy/IMPPolicy.h
@@ -9,6 +9,7 @@ class ItemSystem;
 class IMPPolicy
 {
 public:
+	virtual uint8_t getStartingLevel() const = 0;
 	virtual const std::vector<const ItemModel *> & getInventory() const = 0;
 	virtual const std::vector<const ItemModel *> & getGoodShooterItems() const = 0;
 	virtual const std::vector<const ItemModel *> & getNormalShooterItems() const = 0;
