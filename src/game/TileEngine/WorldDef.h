d9f26668f8f1fffd70522a0aa128c6864e20c0a4
Stefan Lau <github@stefanlau.com>
2020-11-05 23:03:48 +0100

    Fix tileset defaulting logic

--- src/game/TileEngine/WorldDef.h
+++ src/game/TileEngine/WorldDef.h
@@ -277,6 +277,7 @@ void CompileWorldMovementCosts(void);
 void RecompileLocalMovementCosts( INT16 sCentreGridNo );
 void RecompileLocalMovementCostsFromRadius( INT16 sCentreGridNo, INT8 bRadius );
 
+TileSetID GetDefaultTileset();
 // Tilesets may not provide all tile types.If a tile type is not available in a tileset, we fall back and use the surface in the default tileset.
 TILE_SURFACE_RESOURCE GetAdjustedTilesetResource(TileSetID tilesetID, UINT32 uiTileType, const ST::string filePrefix = "");
 
