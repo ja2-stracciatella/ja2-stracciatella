8d3ce50dd2735a8212f89d30faa57f375315bcc8
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2021-04-21 10:30:44 +0800

    Create ja2-launcher.log in temp directory
    See #1257

--- src/launcher/main.cc
+++ src/launcher/main.cc
@@ -2,11 +2,13 @@
 #include "RustInterface.h"
 
 #include <FL/Fl.H>
+#include <filesystem>
 
 
 int main(int argc, char* argv[]) 
 {
-	Logger_initialize("ja2-launcher.log");
+	auto logPath = std::filesystem::temp_directory_path().append("ja2-launcher.log");
+	Logger_initialize(logPath.c_str());
 
 #ifdef _WIN32
 	// Ensure quick-edit mode is off, or else it will block execution
