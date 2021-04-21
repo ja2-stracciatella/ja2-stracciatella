a2b7923a4258271397adafe465b3529c1e680056
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2021-04-21 20:11:03 +0800

    fall back to std::experimental::filesystem on older GCC
    Our MingW build still uses GCC 6 which does not yet have std::filesystem

--- src/launcher/main.cc
+++ src/launcher/main.cc
@@ -1,13 +1,19 @@
-#include <Launcher.h>
+#include "Launcher.h"
 #include "RustInterface.h"
-
 #include <FL/Fl.H>
-#include <filesystem>
+#include <string_theory/string>
 
+#if defined(__GNUC__) && __GNUC__ < 8
+	#include <experimental/filesystem>
+	namespace fs = std::experimental::filesystem;
+#else
+	#include <filesystem>
+	namespace fs = std::filesystem;
+#endif
 
 int main(int argc, char* argv[]) 
 {
-	auto logPath = std::filesystem::temp_directory_path().append("ja2-launcher.log");
+	ST::string logPath { fs::temp_directory_path().append("ja2-launcher.log") };
 	Logger_initialize(logPath.c_str());
 
 #ifdef _WIN32
