e9b75a0a15988acbec1512dd068b643684b54aef
FluffyQuack <git@fluffyquack.com>
2021-05-30 11:42:39 +0200

    Comment tweak.

--- src/game/Tactical/UI_Cursors.cc
+++ src/game/Tactical/UI_Cursors.cc
@@ -52,7 +52,7 @@ static UICursorID HandleTinCanCursor(            SOLDIERTYPE*, GridNo map_pos, M
 
 static BOOLEAN gfCannotGetThrough = FALSE;
 static BOOLEAN gfDisplayFullCountRing = FALSE;
-static int giHitChance = 0; // If value is -1, then we skip showing hit chance
+static int giHitChance = 0; // If value is -1 then we skip showing hit chance
 static int giLastBodyLocationTargeted = 0;
 static int giLastAimTime = 0;
 
