d4761a24dbdee6e58c6ce82d6bfa1e947032d4f3
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2021-09-04 17:26:58 +0800

    #1392 : correct the armour value comparison
    To compare to the value of the correct armour slot, when searching for equipment.
    
    Also added a check to avoid division by zero, in case the current armour slot is invalid or has zero protection value.

--- src/game/TacticalAI/FindLocations.cc
+++ src/game/TacticalAI/FindLocations.cc
@@ -1620,8 +1620,11 @@ INT8 SearchForItems(SOLDIERTYPE& s, ItemSearchReason const reason, UINT16 const 
 							else
 							{
 								INT8 const new_rating = EffectiveArmour(&o);
-								if (EffectiveArmour(&s.inv[HELMETPOS]) <= new_rating) continue; // XXX makes no sense: always compare with helmet and only consider when worse
-								temp_value = 100 * new_rating / EffectiveArmour(&cur_armour);
+								INT8 current_rating = EffectiveArmour(&cur_armour);
+								if (current_rating >= new_rating) continue; // the new armour isn't better, skip
+								if (current_rating == 0) current_rating = 1; // avoid division by zero
+
+								temp_value = 100 * new_rating / current_rating;
 							}
 						}
 						/* FALLTHROUGH */
