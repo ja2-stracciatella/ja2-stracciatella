435d2efb08188483f69d9aecdfe172a7f9fd428d
ja2-derek <63151803+ja2-derek@users.noreply.github.com>
2021-09-08 22:49:15 +0800

    Handle push/get of ST::string pointers

--- src/externalized/scripting/STStringHandler.h
+++ src/externalized/scripting/STStringHandler.h
@@ -34,3 +34,33 @@ int sol_lua_push(sol::types<ST::string>, lua_State* L, const ST::string& st)
 {
 	return sol::stack::push(L, st.to_std_string());
 }
+
+
+// Now ST::string*
+template <typename Handler>
+bool sol_lua_check(sol::types<ST::string*>, lua_State* L, int index, Handler&& handler, sol::stack::record& tracking)
+{
+	// check_usertype is a backdoor for directly checking sol3 usertypes
+	if (!sol::stack::check_usertype<ST::string*>(L, index)
+	    && !sol::stack::check<std::string>(L, index)) {
+		handler(L, index, sol::type_of(L, index), sol::type::userdata, "expected a ST::string* ot std::string");
+		return false;
+	}
+	tracking.use(1);
+	return true;
+}
+
+ST::string* sol_lua_get(sol::types<ST::string*>, lua_State* L, int index, sol::stack::record& tracking)
+{
+	if (sol::stack::check_usertype<ST::string*>(L, index)) {
+		ST::string* ns = sol::stack::get_usertype<ST::string*>(L, index, tracking);
+		return ns;
+	}
+	std::string str = sol::stack::get<std::string>(L, index, tracking);
+	return new ST::string(str); // C++ to own the pointer
+}
+
+int sol_lua_push(sol::types<ST::string*>, lua_State* L, const ST::string* st)
+{
+	return sol::stack::push(L, st->to_std_string());
+}
