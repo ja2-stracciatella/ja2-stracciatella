f47ae57a235366dcbeb05977c4091d9bc7aa3d51
Jaka Kranjc <smgl@lynxlynx.info>
2022-04-24 19:01:40 +0200

    Debug: converted Assert to ST

--- src/sgp/Debug.h
+++ src/sgp/Debug.h
@@ -7,7 +7,7 @@
 #define DEBUG_PRINT_FPS                         (0)             /**< Flag telling to print FPS (Frames per second) counter. */
 #define DEBUG_PRINT_GAME_CYCLE_TIME             (0)             /**< Flag telling to print how much time every game cycle takes. */
 
-#define Assert(a)       (a) ? (void)0 : SLOGA("Assertion failed in %s, line %d", __FILE__, __LINE__)
+#define Assert(a)       (a) ? (void)0 : STLOGA("Assertion failed in {}, line {}", __FILE__, __LINE__)
 #define AssertMsg(a, b) (a) ? (void)0 : STLOGA("Assertion failed in {}, line {}:\n{}", __FILE__, __LINE__, b)
 
 #endif
