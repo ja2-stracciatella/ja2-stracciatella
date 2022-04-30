208d4ac49b698b3fd6f9d7c5268888ca07460861
Trenton Schulz <trenton@norwegianrockcat.com>
2022-04-30 10:19:32 +0200

    Handle the EXDEV error on all OS's where it can happen
    This EXDEV error can happen on all Unix-like OS's that have a rename(2) that
    conform to POSIX.1. So, go with the more inclusive "target_family"
    instead of target_os.
    
    Makes JA2 work with FreeBSD (and likely other OS's that may have their
    tmp on another filesystem).

--- rust/stracciatella/src/fs.rs
+++ rust/stracciatella/src/fs.rs
@@ -59,7 +59,7 @@ pub fn remove_file<P: AsRef<Path>>(path: P) -> Result<(), io::Error> {
 pub fn rename<P: AsRef<Path>>(from: P, to: P) -> Result<(), io::Error> {
     match std::fs::rename(&from, &to) {
         Ok(()) => Ok(()),
-        #[cfg(target_os = "linux")]
+        #[cfg(target_family = "unix")]
         Err(e) if e.raw_os_error() == Some(libc::EXDEV) => {
             std::fs::copy(&from, &to).and_then(|_| remove_file(&from))
         }
