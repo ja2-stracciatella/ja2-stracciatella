096629883f71a251c589cdaa3bcac0a66d5f8366
Momoko Hanasaki <hanasaki.momoko@gmail.com>
2022-06-25 12:09:42 +0200

    Do not call AllTeamsLookForAll while trashing the world

--- src/game/TileEngine/SmokeEffects.h
+++ src/game/TileEngine/SmokeEffects.h
@@ -38,7 +38,7 @@ struct SMOKEEFFECT
 SmokeEffectKind GetSmokeEffectOnTile(INT16 sGridNo, INT8 bLevel);
 
 // Decays all smoke effects...
-void DecaySmokeEffects( UINT32 uiTime );
+void DecaySmokeEffects(UINT32 uiTime, bool trashingWorld = false);
 
 // Add smoke to gridno
 // ( Replacement algorithm uses distance away )
