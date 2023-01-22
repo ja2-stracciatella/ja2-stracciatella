22f2a944b58a4d2d33e15cc1f7a0e9b04210e25a
Momoko Hanasaki <hanasaki.momoko@gmail.com>
2023-01-22 07:21:29 +0100

    Reimplement SGP::Buffer using unique_ptr and add a deprecation warning
    This class predates C++11 and it shows. This commit adds a new method
    MoveToUnique to ease the transition away from it.

--- src/sgp/Buffer.h
+++ src/sgp/Buffer.h
@@ -2,42 +2,48 @@
 #define BUFFER_H
 
 #include <cstddef>
+#include <memory>
+#include <utility>
 
+/*
+ * Please do not use this class in new code, there is nothing it can do
+ * that unique_ptr or vector cannot do better. The only reason the
+ * deprecated attribute is commented out is for the sake of the existing
+ * Buffer users.
+ */
 namespace SGP
 {
-	template<typename T> class Buffer
+	/* [[deprecated]] */ template<typename T> class Buffer
 	{
+		using UniqueT = std::unique_ptr<T []>;
+
 		public:
-			explicit Buffer(T* const buf = 0) : buf_(buf) {}
+			explicit Buffer(T* const buf = nullptr) : ptr_{ buf } {}
 
-			explicit Buffer(size_t const n) : buf_(new T[n]{}) {}
+			explicit Buffer(std::size_t const n) { Allocate(n); }
 
-			~Buffer() { if (buf_) delete[] buf_; }
+			Buffer<T> & Allocate(std::size_t const n) { ptr_ = std::make_unique<T []>(n); return *this; }
 
-			Buffer& Allocate(size_t const n) { return *this = new T[n]{}; }
+			T* Release() noexcept { return ptr_.release(); }
 
-			T* Release()
-			{
-				T* const buf = buf_;
-				buf_ = 0;
-				return buf;
-			}
+			Buffer<T> & operator=(T * const buf) noexcept { ptr_.reset(buf); return *this; }
 
-			Buffer& operator =(T* const buf)
-			{
-				if (buf_) delete[] buf_;
-				buf_ = buf;
-				return *this;
-			}
+			// Small utility function to help ease the transition away from Buffer.
+			UniqueT moveToUnique() noexcept { return std::move(ptr_); }
 
-			operator T*()             { return buf_; }
-			operator T const*() const { return buf_; }
+			operator T*()             noexcept { return ptr_.get(); }
+			operator T const*() const noexcept { return ptr_.get(); }
 
 		private:
-			T* buf_;
+			UniqueT ptr_;
 
-			Buffer(const Buffer&);          /* no copy */
-			void operator =(const Buffer&); /* no assignment */
+			Buffer(const Buffer&) = delete;          /* no copy */
+			void operator=(const Buffer&) = delete; /* no assignment */
+
+			// Moving a buffer would actually work, but to discourage use
+			// of this class, these two are deleted as well.
+			Buffer(Buffer &&) = delete;          /* no move */
+			void operator=(Buffer &&) = delete; /* no move assignment */
 	};
 }
 
