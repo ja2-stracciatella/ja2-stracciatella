83f6ca86e7f8d0997e1a286c330981614470138d
Momoko Hanasaki <hanasaki.momoko@gmail.com>
2023-04-26 09:38:42 +0200

    Fix return value of CalcPercentBetter
    As the name suggests, this function is supposed to calculate how much
    better a new cover position is compared to the old one, so returning
    NOWHERE would indicate the new cover is 25,600% better.

--- src/game/TacticalAI/FindLocations.cc
+++ src/game/TacticalAI/FindLocations.cc
@@ -50,25 +50,24 @@ INT8 gubAIPathCosts[19][19];
 
 static INT32 CalcPercentBetter(INT32 iOldValue, INT32 iNewValue, INT32 iOldScale, INT32 iNewScale)
 {
-	INT32 iValueChange,iScaleSum,iPercentBetter;//,loopCnt,tempInt;
-
 	// calcalate how much better the new cover would be than the current cover
-	iValueChange = iNewValue - iOldValue;
+	INT32 const iValueChange = iNewValue - iOldValue;
 
 	// here, the change in cover HAS to be an improvement over current cover
 	if (iValueChange <= 0)
 	{
-		return(NOWHERE);
+		return 0; // not any better
 	}
 
-	iScaleSum = iOldScale + iNewScale;
+	INT32 const iScaleSum = iOldScale + iNewScale;
 
 	// here, the change in cover HAS to be an improvement over current cover
 	if (iScaleSum <= 0)
 	{
-		return(NOWHERE);
+		return 0; // not any better
 	}
-	iPercentBetter = (iValueChange * 100) / iScaleSum;
+
+	INT32 const iPercentBetter = (iValueChange * 100) / iScaleSum;
 	SLOGD("CalcPercentBetter: %Better {}, old {}, new {}, change {}\noldScale {}, newScale {}, scaleSum {}",
 		iPercentBetter, iOldValue, iNewValue, iValueChange, iOldScale, iNewScale, iScaleSum);
 	return(iPercentBetter);
@@ -977,7 +976,7 @@ INT16 FindBestNearbyCover(SOLDIERTYPE *pSoldier, INT32 morale, INT32 *piPercentB
 		{
 			SLOGD("Found Cover: current {}, best {}, %Better {}",
 				iCurrentCoverValue, iBestCoverValue, *piPercentBetter);
-			return((INT16)sBestCover);       // return the gridno of that cover
+			return sBestCover; // return the gridno of that cover
 		}
 	}
 	return(NOWHERE);       // return that no suitable cover was found
