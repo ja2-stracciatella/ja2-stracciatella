f4d578ceefbf22699593a6604ecfeea09016669d
Momoko Hanasaki <hanasaki.momoko@gmail.com>
2023-05-18 11:37:06 +0200

    CMakeLists.txt maintenance
    • remove unused define WITH_SOUND_DEBUG
    • fix WITH_ASAN option
    • only add compiler warnings flags when compiling, not when linking
    • disable some MSVC warnings here instead of in Platform.h

--- src/sgp/Platform.h
+++ src/sgp/Platform.h
@@ -23,22 +23,6 @@
 #define SOURCE_ROOT ("src/")
 #endif
 
-/* #if CASE_SENSITIVE_FS */
-/* #include <dirent.h> */
-/* #endif */
-
-/**************************************************************
- * Warnings
- *************************************************************/
-
-/* Disable some warnings on Visual Studio */
-#if defined(_MSC_VER)
-#pragma warning( disable : 4018 )                       /* example: '>' : signed/unsigned mismatch */
-#pragma warning( disable : 4200 )                       /* example: nonstandard extension used : zero-sized array in struct/union */
-#pragma warning( disable : 4244 )                       /* example: conversion from 'int' to 'real', possible loss of data */
-#pragma warning( disable : 4800 )                       /* example: 'const UINT32' : forcing value to bool 'true' or 'false' (performance warning) */
-#pragma warning( disable : 4996 )                       /* example: 'wcscpy': This function or variable may be unsafe. Consider using wcscpy_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details. */
-#endif
 
 /**************************************************************
  * Misc
