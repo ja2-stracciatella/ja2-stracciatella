bfa7bca2a6636718cc8bf9bb10ee2949c7ff4ac1
Stefan Lau <github@stefanlau.com>
2023-10-06 16:30:33 +0200

    Remove remove_dir_all rust dependency

--- rust/stracciatella_c_api/src/c/fs/mod.rs
+++ rust/stracciatella_c_api/src/c/fs/mod.rs
@@ -142,19 +142,6 @@ pub extern "C" fn Fs_modifiedSecs(path: *const c_char, modified_secs: *mut f64) 
     no_rust_error()
 }
 
-/// Removes a directory and all it's contents.
-/// Sets the rust error.
-/// @see https://doc.rust-lang.org/std/fs/fn.remove_dir_all.html
-#[no_mangle]
-pub extern "C" fn Fs_removeDirAll(dir: *const c_char) -> bool {
-    forget_rust_error();
-    let dir = path_buf_from_c_str_or_panic(unsafe_c_str(dir));
-    if let Err(err) = fs::remove_dir_all(&dir) {
-        remember_rust_error(format!("Fs_removeDirAll {:?}: {}", dir, err));
-    }
-    no_rust_error()
-}
-
 /// Removes a file.
 /// Sets the rust error.
 /// @see https://doc.rust-lang.org/std/fs/fn.remove_file.html
