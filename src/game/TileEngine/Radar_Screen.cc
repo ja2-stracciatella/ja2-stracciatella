5481f45e90cff6c4781f0b49b4ceb16a7b460c73
Mladen Milinkovic <maxrd2@smoothware.net>
2023-10-27 12:19:14 +0200

    Revert "Make four render variables const"
    This reverts commit 6748af147ee20ed62378f7034d2cd7be6649bcaa.

--- src/game/TileEngine/Radar_Screen.cc
+++ src/game/TileEngine/Radar_Screen.cc
@@ -315,6 +315,7 @@ static void AdjustWorldCenterFromRadarCoords(INT16 sRadarX, INT16 sRadarY)
 
 	INT16 sScreenX, sScreenY;
 	INT16	sTempX_W, sTempY_W;
+	INT16 sNewCenterWorldX, sNewCenterWorldY;
 	INT16 sNumXSteps, sNumYSteps;
 
 	// Use radar scale values to get screen values, then convert ot map values, rounding to nearest middle tile
@@ -344,7 +345,10 @@ static void AdjustWorldCenterFromRadarCoords(INT16 sRadarX, INT16 sRadarY)
 	FromScreenToCellCoordinates( sScreenX, sScreenY, &sTempX_W, &sTempY_W );
 
 	// Adjust these to world center
-	SetRenderCenter( gCenterWorldX + sTempX_W, gCenterWorldY + sTempY_W);
+	sNewCenterWorldX = (INT16)(gCenterWorldX + sTempX_W);
+	sNewCenterWorldY = (INT16)(gCenterWorldY + sTempY_W);
+
+	SetRenderCenter( sNewCenterWorldX, sNewCenterWorldY );
 }
 
 
