ja2-stracciatella/ja2-stracciatella

commit 1ae407c066d42ae64a8809bae600ccef50ee4996

Externalize armours

src/game/Tactical/Items.cc
@@ -1,3 +1,4 @@
+#include "ArmourModel.h"
 #include "Font_Control.h"
 #include "Handle_Items.h"
 #include "Items.h"
@@ -1752,6 +1753,7 @@ BOOLEAN CanItemFitInPosition(SOLDIERTYPE* pSoldier, OBJECTTYPE* pObj, INT8 bPos,
 	UINT8 ubSlotLimit;
 	INT8  bNewPos;
 
+	auto item = GCM->getItem(pObj->usItem);
 	switch( bPos )
 	{
 		case SECONDHANDPOS:
@@ -1761,7 +1763,7 @@ BOOLEAN CanItemFitInPosition(SOLDIERTYPE* pSoldier, OBJECTTYPE* pObj, INT8 bPos,
 			}
 			break;
 		case HANDPOS:
-			if (GCM->getItem(pObj->usItem)->isTwoHanded())
+			if (item->isTwoHanded())
 			{
 				if (pSoldier->inv[HANDPOS].usItem != NOTHING && pSoldier->inv[SECONDHANDPOS].usItem != NOTHING)
 				{
@@ -1792,26 +1794,26 @@ BOOLEAN CanItemFitInPosition(SOLDIERTYPE* pSoldier, OBJECTTYPE* pObj, INT8 bPos,
 		case VESTPOS:
 		case HELMETPOS:
 		case LEGPOS:
-			if (GCM->getItem(pObj->usItem)->getItemClass() != IC_ARMOUR)
+			if (item->getItemClass() != IC_ARMOUR)
 			{
 				return( FALSE );
 			}
 			switch (bPos)
 			{
 				case VESTPOS:
-					if (Armour[GCM->getItem(pObj->usItem)->getClassIndex()].ubArmourClass != ARMOURCLASS_VEST)
+					if (item->asArmour()->getArmourClass() != ARMOURCLASS_VEST)
 					{
 						return( FALSE );
 					}
 					break;
 				case HELMETPOS:
-					if (Armour[GCM->getItem(pObj->usItem)->getClassIndex()].ubArmourClass != ARMOURCLASS_HELMET)
+					if (item->asArmour()->getArmourClass() != ARMOURCLASS_HELMET)
 					{
 						return( FALSE );
 					}
 					break;
 				case LEGPOS:
-					if (Armour[GCM->getItem(pObj->usItem)->getClassIndex()].ubArmourClass != ARMOURCLASS_LEGGINGS)
+					if (item->asArmour()->getArmourClass() != ARMOURCLASS_LEGGINGS)
 					{
 						return( FALSE );
 					}
@@ -1822,7 +1824,7 @@ BOOLEAN CanItemFitInPosition(SOLDIERTYPE* pSoldier, OBJECTTYPE* pObj, INT8 bPos,
 			break;
 		case HEAD1POS:
 		case HEAD2POS:
-			if (GCM->getItem(pObj->usItem)->getItemClass() != IC_FACE)
+			if (item->getItemClass() != IC_FACE)
 			{
 				return( FALSE );
 			}
@@ -2125,7 +2127,7 @@ static BOOLEAN InternalAutoPlaceObject(SOLDIERTYPE* pSoldier, OBJECTTYPE* pObj, 
 			break;
 
 		case IC_ARMOUR:
-			switch (Armour[GCM->getItem(pObj->usItem)->getClassIndex()].ubArmourClass)
+			switch (pItem->asArmour()->getItemClass())
 			{
 				case ARMOURCLASS_VEST:
 					if (pSoldier->inv[VESTPOS].usItem == NONE)
@@ -3060,14 +3062,15 @@ static INT8 CheckItemForDamage(UINT16 usItem, INT32 iMaxDamage)
 		iMaxDamage = 2;
 	}
 
+	auto item = GCM->getItem(usItem);
 	// if the item is protective armour, reduce the amount of damage
 	// by its armour value
-	if (GCM->getItem(usItem)->getItemClass() == IC_ARMOUR)
+	if (item->asArmour())
 	{
-		iMaxDamage -= (iMaxDamage * Armour[GCM->getItem(usItem)->getClassIndex()].ubProtection) / 100;
+		iMaxDamage -= (iMaxDamage * item->asArmour()->getProtection()) / 100;
 	}
 	// metal items are tough and will be damaged less
-	if (GCM->getItem(usItem)->getFlags() & ITEM_METAL)
+	if (item->getFlags() & ITEM_METAL)
 	{
 		iMaxDamage /= 2;
 	}
@@ -3520,9 +3523,9 @@ bool ItemIsCool(OBJECTTYPE const& o)
 	{
 		if (GCM->getWeapon(o.usItem)->ubDeadliness >= 30) return true;
 	}
-	else if (item->isArmour())
+	else if (item->asArmour())
 	{
-		if (Armour[item->getClassIndex()].ubProtection >= 20) return true;
+		if (item->asArmour()->getProtection() >= 20) return true;
 	}
 
 	return false;

rust/stracciatella/src/schemas/yaml/armours.schema.yaml
@@ -1,0 +1,36 @@
+$schema: http://json-schema.org/schema#
+type: array
+title: Armours
+description: |
+  Defines armour items that can be used in game.
+items:
+  allOf:
+    - title: Base Item
+      $ref: types/base-item.schema.yaml
+    - title: Armour
+      description: |
+        An armour item that can be used in game.
+      type: object
+      properties:
+        armourClass:
+          title: Armour class
+          description: The type of armour
+          type: string
+          enum:
+            - VEST
+            - LEGGINGS
+            - HELMET
+            - PLATE
+            - CREATURE
+        protection:
+          title: Protection
+          description: The protection level of the armour
+          $ref: types/uint8.schema.yaml
+        degradePercentage:
+          title: Degrade Percentage
+          description: The percentage of degradation for the armour
+          $ref: types/uint8.schema.yaml
+      required:
+        - armourClass
+        - protection
+        - degradePercentage

rust/stracciatella/src/schemas/yaml/explosives.schema.yaml
@@ -7,9 +7,9 @@ items:
   allOf:
     - title: Base Item
       $ref: types/base-item.schema.yaml
-    - title: Item
+    - title: Explosive
       description: |
-        An explosive item defines that can be used in game.
+        An explosive item that can be used in game.
       type: object
       properties:
         animation:

src/externalized/ArmourModel.cc
@@ -1,0 +1,87 @@
+#include "ArmourModel.h"
+#include "Exceptions.h"
+#include "Item_Types.h"
+#include "Weapons.h"
+#include <cstdint>
+
+uint8_t deserializeArmourClass(const ST::string& armourClass) {
+	if (armourClass == "HELMET") return ARMOURCLASS_HELMET;
+	if (armourClass == "VEST") return ARMOURCLASS_VEST;
+	if (armourClass == "LEGGINGS") return ARMOURCLASS_LEGGINGS;
+	if (armourClass == "PLATE") return ARMOURCLASS_PLATE;
+	if (armourClass == "CREATURE") return ARMOURCLASS_MONST;
+	throw DataError(ST::format("Unknown armour class '{}'", armourClass));
+}
+
+ArmourModel::ArmourModel(
+			uint16_t itemIndex,
+			ST::string&& internalName,
+			ST::string&& shortName,
+			ST::string&& name,
+			ST::string&& description,
+			InventoryGraphicsModel&& inventoryGraphics,
+			TilesetTileIndexModel&& tileGraphic,
+			uint8_t weight,
+			uint8_t perPocket,
+			uint16_t price,
+			uint8_t coolness,
+			int8_t reliability,
+			int8_t repairEase,
+			uint32_t flags,
+			uint8_t armourClass,
+			uint8_t protection,
+			uint8_t degradePercentage
+	) : ItemModel(itemIndex, std::move(internalName), std::move(shortName), std::move(name), std::move(description), IC_ARMOUR, 0, INVALIDCURS, std::move(inventoryGraphics), std::move(tileGraphic), weight, perPocket, price, coolness, reliability, repairEase, flags) {
+	this->armourClass = armourClass;
+	this->protection = protection;
+	this->degradePercentage = degradePercentage;
+}
+
+ArmourModel* ArmourModel::deserialize(const JsonValue &json, const BinaryData& vanillaItemStrings) {
+	auto obj = json.toObject();
+	ItemModel::InitData const initData{ obj, vanillaItemStrings };
+
+	int itemIndex = obj.GetInt("itemIndex");
+	ST::string internalName = obj.GetString("internalName");
+	auto shortName = ItemModel::deserializeShortName(initData);
+	auto name = ItemModel::deserializeName(initData);
+	auto description = ItemModel::deserializeDescription(initData);
+	auto flags = ItemModel::deserializeFlags(obj);
+	auto inventoryGraphics = InventoryGraphicsModel::deserialize(obj["inventoryGraphics"]);
+	auto tileGraphic = TilesetTileIndexModel::deserialize(obj["tileGraphic"]);
+	auto armourClass = deserializeArmourClass(obj.GetString("armourClass"));
+	auto protection = obj.GetUInt("protection");
+	auto degradePercentage = obj.GetUInt("degradePercentage");
+
+	return new ArmourModel(
+		itemIndex,
+		std::move(internalName),
+		std::move(shortName),
+		std::move(name),
+		std::move(description),
+		std::move(inventoryGraphics),
+		std::move(tileGraphic),
+		obj.GetUInt("ubWeight"),
+		obj.GetUInt("ubPerPocket"),
+		obj.GetUInt("usPrice"),
+		obj.GetUInt("ubCoolness"),
+		obj.GetInt("bReliability"),
+		obj.GetInt("bRepairEase"),
+		flags,
+		armourClass,
+		protection,
+		degradePercentage
+	);
+}
+
+uint8_t ArmourModel::getArmourClass() const {
+	return armourClass;
+}
+
+uint8_t ArmourModel::getProtection() const {
+	return protection;
+}
+
+uint8_t ArmourModel::getDegradePercentage() const {
+	return degradePercentage;
+}

src/externalized/ArmourModel.h
@@ -1,0 +1,39 @@
+#pragma once
+
+#include "ItemModel.h"
+#include <cstdint>
+
+struct ArmourModel : ItemModel
+{
+	ArmourModel(
+		uint16_t itemIndex,
+		ST::string&& internalName,
+		ST::string&& shortName,
+		ST::string&& name,
+		ST::string&& description,
+		InventoryGraphicsModel&& inventoryGraphics,
+		TilesetTileIndexModel&& tileGraphic,
+		uint8_t weight,
+		uint8_t perPocket,
+		uint16_t price,
+		uint8_t coolness,
+		int8_t reliability,
+		int8_t repairEase,
+		uint32_t flags,
+		uint8_t armourClass,
+		uint8_t protection,
+		uint8_t degradePercentage
+	);
+
+	virtual const ArmourModel* asArmour() const { return this; }
+
+	static ArmourModel* deserialize(const JsonValue &json, const BinaryData& vanillaItemStrings);
+
+	uint8_t getArmourClass() const;
+	uint8_t getProtection() const;
+	uint8_t getDegradePercentage() const;
+	protected:
+		uint8_t armourClass;
+		uint8_t protection;
+		uint8_t degradePercentage;
+};

src/externalized/CMakeLists.txt
@@ -14,6 +14,7 @@ add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/ui")
 file(GLOB LOCAL_JA2_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/*.h)
 set(LOCAL_JA2_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/AmmoTypeModel.cc
+    ${CMAKE_CURRENT_SOURCE_DIR}/ArmourModel.cc
     ${CMAKE_CURRENT_SOURCE_DIR}/CalibreModel.cc
     ${CMAKE_CURRENT_SOURCE_DIR}/DealerModel.cc
     ${CMAKE_CURRENT_SOURCE_DIR}/DealerInventory.cc

src/externalized/ContentManager.h
@@ -135,6 +135,9 @@ public:
 	virtual const ExplosiveModel* getExplosive(uint16_t index) = 0;
 	virtual const ExplosiveModel* getExplosiveByName(const ST::string &name) = 0;
 
+	virtual const ArmourModel* getArmour(uint16_t index) = 0;
+	virtual const ArmourModel* getArmourByName(const ST::string &name) = 0;
+
 	virtual const std::vector<std::vector<const WeaponModel*> > & getNormalGunChoice() const = 0;
 	virtual const std::vector<std::vector<const WeaponModel*> > & getExtendedGunChoice() const = 0;
 	virtual const std::vector<GARRISON_GROUP>& getGarrisonGroups() const = 0;

src/externalized/DefaultContentManager.cc
@@ -10,6 +10,7 @@
 #include "FileMan.h"
 
 #include "AmmoTypeModel.h"
+#include "ArmourModel.h"
 #include "CacheSectorsModel.h"
 #include "CalibreModel.h"
 #include "ContentMusic.h"
@@ -454,6 +455,15 @@ const WeaponModel* DefaultContentManager::getWeapon(uint16_t itemIndex)
 	return getItem(itemIndex)->asWeapon();
 }
 
+/** Get the armour with the given index.
+ * Returns nullptr if the item is not an armour
+ * Throws if the armour does not exist
+*/
+const ArmourModel* DefaultContentManager::getArmour(uint16_t itemIndex)
+{
+	return getItem(itemIndex)->asArmour();
+}
+
 /** Get the weapon with the given name.
  * Throws if the weapon does not exist
  */
@@ -466,6 +476,20 @@ const WeaponModel* DefaultContentManager::getWeaponByName(const ST::string &inte
 		throw std::runtime_error(ST::format("weapon '{}' was not found", internalName).to_std_string());
 	}
 	return it->second;//m_weaponMap[internalName];
+}
+
+/** Get the armour with the given name.
+ * Throws if the armour does not exist
+ */
+const ArmourModel* DefaultContentManager::getArmourByName(const ST::string &internalName)
+{
+	std::map<ST::string, const ArmourModel*>::const_iterator it = m_armourMap.find(internalName);
+	if(it == m_armourMap.end())
+	{
+		SLOGE("armour '{}' was not found", internalName);
+		throw std::runtime_error(ST::format("armour '{}' was not found", internalName).to_std_string());
+	}
+	return it->second;
 }
 
 const MagazineModel* DefaultContentManager::getMagazineByName(const ST::string &internalName)
@@ -544,6 +568,26 @@ bool DefaultContentManager::loadWeapons(const BinaryData& vanillaItemStrings)
 
 		m_items[w->getItemIndex()] = w;
 		m_weaponMap.try_emplace(w->getInternalName(), w);
+	}
+
+	return true;
+}
+
+bool DefaultContentManager::loadArmours(const BinaryData& vanillaItemStrings)
+{
+	auto json = readJsonDataFileWithSchema("armours.json");
+	for (auto& element : json.toVec()) {
+		ArmourModel *a = ArmourModel::deserialize(element, vanillaItemStrings);
+		SLOGD("Loaded armour {} {}", a->getItemIndex(), a->getInternalName());
+
+		if (a->getItemIndex() >= m_items.size())
+		{
+			SLOGE("Armour index must be in the interval 0 - {}", m_items.size() - 1);
+			return false;
+		}
+
+		m_items[a->getItemIndex()] = a;
+		m_armourMap.try_emplace(a->getInternalName(), a);
 	}
 
 	return true;
@@ -848,6 +892,7 @@ bool DefaultContentManager::loadGameData(BinaryData const& binaryData)
 		&& loadSmokeEffects()
 		&& loadExplosionAnimations()
 		&& loadExplosives(binaryData, m_explosionAnimations)
+		&& loadArmours(binaryData)
 		&& loadArmyData()
 		&& loadMusic();
 

commit 20c558ff05a5991ffec76a20181b4e4e4d20b9d5

InternalAutoPlaceObject: fixed comparison against the wrong type, fixes #2207
regression from 1ae407c066

src/game/Tactical/Items.cc
@@ -2108,7 +2108,7 @@ static BOOLEAN InternalAutoPlaceObject(SOLDIERTYPE* pSoldier, OBJECTTYPE* pObj, 
 			break;
 
 		case IC_ARMOUR:
-			switch (pItem->asArmour()->getItemClass())
+			switch (pItem->asArmour()->getArmourClass())
 			{
 				case ARMOURCLASS_VEST:
 					if (pSoldier->inv[VESTPOS].usItem == NONE)